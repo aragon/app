bd329829304d067de91b81e00b5248fe7f7645a2
Ruggero Cino <ruggero.cino@gmail.com>
2023-07-05 14:53:46 +0200

    Implement tag component, setup test environment

---
+++ packages/gov-ui-kit/jest.config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  testEnvironment: "jsdom",
+  collectCoverageFrom: ["./src/**/*.{ts,tsx}"],
+  coveragePathIgnorePatterns: [".d.ts", ".api.ts", "index.ts", ".stories.tsx"],
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
+};
+
+module.exports = config;
---
+++ packages/gov-ui-kit/src/components/tag/tag.spec.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { Tag } from "./tag";
+import { TagProps } from "./tag.api";
+
+describe("Tag", () => {
+  const createTestComponent = (props?: Partial<TagProps>) => {
+    const completeProps = {
+      children: "my-label",
+      ...props,
+    };
+
+    return <Tag {...completeProps} />;
+  };
+
+  it("renders the specified label", () => {
+    const children = "label-test";
+    render(createTestComponent({ children }));
+    expect(screen.getByText(children)).toBeInTheDocument();
+  });
+});
