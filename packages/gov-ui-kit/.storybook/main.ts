574c3c4b55dc5ac4b4e4e60c62d8c3846a8e1333
Ruggero Cino <ruggero.cino@gmail.com>
2023-07-06 13:05:25 +0200

    Setup tailwindcss

--- packages/gov-ui-kit/.storybook/main.ts
+++ packages/gov-ui-kit/.storybook/main.ts
@@ -2,7 +2,15 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
     stories: ['../docs/**/*.@(md|mdx)', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        {
+            name: '@storybook/addon-styling',
+            options: { postCss: true },
+        },
+    ],
     framework: {
         name: '@storybook/react-webpack5',
         options: {},
