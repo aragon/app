6b1f3e80ae63320abaccfad431611bc691c2b18b
Ruggero <ruggero.cino@gmail.com>
2023-11-23 09:45:21 +0100

    feat: APP-2612 - Implement Tag component (#46)

---
+++ packages/gov-ui-kit/src/components/tag/index.ts
@@ -0,0 +1,1 @@
+export { Tag, type ITagProps } from './tag';
---
+++ packages/gov-ui-kit/src/components/tag/tag.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Tag } from './tag';
+
+const meta: Meta<typeof Tag> = {
+    title: 'components/Tag',
+    component: Tag,
+    tags: ['autodocs'],
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/jfKRr1V9evJUp1uBeyP3Zz/v1.0.0?type=design&node-id=21-200&mode=design&t=9F9GGTyrap4TVsvP-4',
+        },
+    },
+};
+
+type Story = StoryObj<typeof Tag>;
+
+/**
+ * Default usage example of the Tag component.
+ */
+export const Default: Story = {
+    args: {
+        label: 'Label',
+    },
+};
+
+export default meta;
