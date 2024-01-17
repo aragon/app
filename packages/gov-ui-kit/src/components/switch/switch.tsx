720d889b9ae1b050d8b6d44006d58dd9f6577f30
Ruggero <ruggero.cino@gmail.com>
2024-01-17 11:11:18 +0100

    feat: Update dependencies, fix label size of Switch component (#61)

--- packages/gov-ui-kit/src/components/switch/switch.tsx
+++ packages/gov-ui-kit/src/components/switch/switch.tsx
@@ -10,14 +10,14 @@ const rootClassNames = classNames(
 );
 
 const thumbClassNames = classNames(
-    'block h-4 w-4 rounded-full bg-neutral-300 transition-transform duration-100 will-change-transform', // Default
+    'block size-4 rounded-full bg-neutral-300 transition-transform duration-100 will-change-transform', // Default
     'data-[state=checked]:translate-x-[14px] data-[state=checked]:bg-primary-400', // State is checked
     'group-disabled:bg-neutral-200 group-disabled:data-[state=checked]:bg-neutral-300', // Disabled
 );
 
 // using `peer` since the parent div is not focusable nor able to be disabled
 const labelClassNames = classNames(
-    'text-sm/tight font-semibold text-neutral-600', // Default
+    'text-sm font-semibold leading-tight text-neutral-600 md:text-base', // Default
     'peer-disabled:text-neutral-300 peer-disabled:peer-data-[state=checked]:text-neutral-600', // Disabled
 );
 
