142d020da04389c0c081738618299f1a7c9f306c
Fabrice Francois <fabricevladimir@gmail.com>
2024-01-23 09:49:03 -0500

    Fix: Toggle hover shadow styling (#66)

--- packages/gov-ui-kit/src/components/toggles/toggle/toggle.tsx
+++ packages/gov-ui-kit/src/components/toggles/toggle/toggle.tsx
@@ -24,7 +24,7 @@ export const Toggle: React.FC<IToggleProps> = (props) => {
     const toggleClasses = classNames(
         'flex h-10 items-center rounded-[40px] border border-neutral-100 px-4', // Default
         'focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset', // Focus state
-        'hover:enabled:border-neutral-200 hover:enabled:shadow-primary-md', // Hover state
+        'hover:enabled:border-neutral-200 hover:enabled:shadow-neutral', // Hover state
         'data-[state=off]:enabled:bg-neutral-0 data-[state=off]:enabled:text-neutral-600', // Default state
         'data-[state=on]:enabled:bg-neutral-100 data-[state=on]:enabled:text-neutral-800', // Active state
         'disabled:bg-neutral-100 disabled:text-neutral-300', // Disabled state
