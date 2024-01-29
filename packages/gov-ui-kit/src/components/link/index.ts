803b6ddcb87abeb3d6fef06eb7463d08a4248281
Kevin Davis <65736142+thekidnamedkd@users.noreply.github.com>
2024-01-29 13:32:04 +0100

    feat: APP-2733 - Implement Link component (#68)

---
+++ packages/gov-ui-kit/src/components/link/index.ts
@@ -0,0 +1,2 @@
+export { Link } from './link';
+export { ILinkProps, LinkVariant } from './link.api';
---
+++ packages/gov-ui-kit/src/components/link/link.test.tsx
@@ -0,0 +1,36 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Link, type ILinkProps } from '.';
+
+describe('<Link /> component', () => {
+    const createTestComponent = (props?: Partial<ILinkProps>) => {
+        const completeProps: ILinkProps = { children: 'Default children', href: 'http://default.com', ...props };
+
+        return <Link {...completeProps} />;
+    };
+
+    it('renders correctly with minimum props', () => {
+        render(createTestComponent({ children: 'Example', href: 'http://example.com' }));
+        const linkElement = screen.getByRole('link', { name: 'Example' });
+        expect(linkElement).toBeInTheDocument();
+        expect(linkElement).toHaveAttribute('href', 'http://example.com');
+    });
+
+    it('applies correct classes based on disabled prop', () => {
+        const handleClick = jest.fn();
+        render(createTestComponent({ children: 'TEST', disabled: true, onClick: handleClick }));
+        // eslint-disable-next-line testing-library/no-node-access
+        const linkElement = screen.getByText('TEST').closest('a')!;
+        fireEvent.click(linkElement);
+        expect(linkElement).toHaveClass('truncate text-neutral-300 cursor-not-allowed');
+        expect(linkElement).toHaveAttribute('aria-disabled', 'true');
+        expect(linkElement).not.toHaveAttribute('href');
+        expect(handleClick).not.toHaveBeenCalled();
+    });
+
+    it('renders correctly with no icon', () => {
+        const children = 'Link without icon';
+        render(createTestComponent({ children, href: 'http://example.com' }));
+        const linkElement = screen.getByRole('link', { name: 'Link without icon' });
+        expect(linkElement).toHaveTextContent(children);
+    });
+});
