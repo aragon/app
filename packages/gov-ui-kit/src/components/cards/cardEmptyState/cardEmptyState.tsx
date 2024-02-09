f88c34929992f144975e8b819f0a467c82d87114
Kevin Davis <65736142+thekidnamedkd@users.noreply.github.com>
2024-02-09 17:55:59 +0100

    feat: APP-2740 - Implement EmptyStateCard Component (#85)

---
+++ packages/gov-ui-kit/src/components/cards/cardEmptyState/cardEmptyState.tsx
@@ -0,0 +1,10 @@
+import { EmptyState, type IEmptyStateProps } from '../../states/emptyState';
+import { Card } from '../card';
+
+export const CardEmptyState: React.FC<IEmptyStateProps> = (props) => {
+    return (
+        <Card>
+            <EmptyState {...props} />
+        </Card>
+    );
+};
