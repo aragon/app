5ece9eb5dd4c1455239061a0a36284d239247211
Fabrice Francois <fabricevladimir@gmail.com>
2024-03-06 04:33:07 -0500

    feat: APP-2939 - Implement DaoAvatar module component (#117)

--- packages/gov-ui-kit/CHANGELOG.md
+++ packages/gov-ui-kit/CHANGELOG.md
@@ -10,6 +10,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 ### Added
 
 -   Implement animations for `Dialog` and `DialogAlert` components
+-   Implement `DaoAvatar` module component
 
 ### Changed
 
---
+++ packages/gov-ui-kit/src/modules/components/dao/daoAvatar/daoAvatar.tsx
@@ -0,0 +1,57 @@
+import classNames from 'classnames';
+import type React from 'react';
+import { Avatar, type IAvatarProps } from '../../../../core';
+
+export interface IDaoAvatarProps extends Omit<IAvatarProps, 'fallback'> {
+    /**
+     * Name of the DAO
+     */
+    name?: string;
+}
+
+export const DaoAvatar: React.FC<IDaoAvatarProps> = (props) => {
+    const { name, size = 'lg', className, ...otherProps } = props;
+    const daoInitials = getDaoInitials(name).toUpperCase();
+
+    return (
+        <Avatar
+            size={size}
+            fallback={
+                <span
+                    className={classNames(
+                        'flex size-full shrink-0 items-center justify-center rounded-full bg-primary-400',
+                        'text-xs leading-tight text-neutral-0 md:text-sm lg:text-lg',
+                        className,
+                    )}
+                >
+                    {daoInitials}
+                </span>
+            }
+            {...otherProps}
+        />
+    );
+};
+
+/**
+ * Get the initials for a DAO
+ * @param name name of the DAO
+ * @returns the DAO initials
+ */
+function getDaoInitials(name: IDaoAvatarProps['name']): string {
+    if (!name) {
+        return '';
+    }
+
+    const trimmedName = name.trim();
+
+    if (trimmedName.length <= 2) {
+        return trimmedName;
+    }
+
+    const words = trimmedName.split(' ');
+    if (words.length < 2) {
+        return words[0][0] + words[0][1];
+    } else {
+        return words[0][0] + words[1][0];
+    }
+}
