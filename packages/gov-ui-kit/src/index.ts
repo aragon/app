b8da858cde1556368ccbece937af4aad269a276f
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-04 10:54:22 -0400

    chore: Export module components (#136)

--- packages/gov-ui-kit/src/index.ts
+++ packages/gov-ui-kit/src/index.ts
@@ -1,5 +1,2 @@
 export * from './core';
-// Temporarily disable modules component export as Aragon App will throw error when trying to import components from
-// the ODS library. The error is caused by the ODS build because it imports the WagmiProvider that is not implemented
-// on Wagmi v1. Enable modules component export when Aragon App migrates to Wagmi v2 (https://aragonassociation.atlassian.net/browse/APP-2949)
-// export * from './modules';
+export * from './modules';
