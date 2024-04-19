87e82f45c2df5fd8ddd823824929a3fcd3482af3
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-19 05:12:59 -0400

    feat: APP-2990 - Implement ProposalDataListItemSkeleton module component (#144)

---
+++ packages/gov-ui-kit/src/modules/components/proposal/proposalDataListItem/proposalDataListItemSkeleton/proposalDataListItemSkeleton.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { DataList } from '../../../../../core';
+import { ProposalDataListItemSkeleton, type IProposalDataListItemSkeletonProps } from './proposalDataListItemSkeleton';
+
+describe('<ProposalDataListItemSkeleton /> component', () => {
+    const createTestComponent = (props?: Partial<IProposalDataListItemSkeletonProps>) => {
+        const completeProps: IProposalDataListItemSkeletonProps = { ...props };
+
+        return (
+            <DataList.Root entityLabel="Proposal">
+                <ProposalDataListItemSkeleton {...completeProps} />
+            </DataList.Root>
+        );
+    };
+    it('has correct accessibility attributes', () => {
+        render(createTestComponent());
+        const listItem = screen.getByLabelText('loading');
+        expect(listItem).toHaveAttribute('aria-busy', 'true');
+        expect(listItem).toHaveAttribute('tabIndex', '0');
+    });
+});
