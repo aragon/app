444c6d76591bd07a54a895f7ad039e4e7405ebb6
Ruggero Cino <ruggero.cino@gmail.com>
2024-05-28 10:09:26 +0200

    Update default metadata

--- src/modules/application/constants/defaultMetadata.ts
+++ src/modules/application/constants/defaultMetadata.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const defaultMetadata: Metadata = {
-    title: 'Aragon App - The DAO platform that grows with you',
+    title: 'Aragon App, DAO creation and management platform',
     description:
         'Aragon App is the long awaited human-centered approach to DAOs. A huge evolution when it comes to user experience in web3, backed by a strong and modular core.',
 };
