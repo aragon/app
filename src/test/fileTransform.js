8109c4a6270472923f52f3ee2ffca8ed5beb823a
Ruggero Cino <ruggero.cino@gmail.com>
2024-05-29 16:37:43 +0200

    Fix tests

--- src/test/fileTransform.js
+++ src/test/fileTransform.js
@@ -1,6 +1,20 @@
 const path = require('path');
 
-const transform = (src, filePath) => ({ code: `module.exports = ${JSON.stringify(path.basename(filePath))};` });
+const transform = (src, filePath) => {
+    const fileName = path.basename(filePath);
+
+    // Simply exports unsupported Jest assets as a string containining their file name.
+    // (see https://jestjs.io/docs/code-transformation#transforming-images-to-their-path)
+    if (path.extname(filePath) !== '.svg') {
+        return { code: `module.exports = "${fileName}";` };
+    }
+
+    // Mock NextJs behaviour of determining the width and height property of local imported images.
+    // (see https://nextjs.org/docs/app/building-your-application/optimizing/images#local-images)
+    const code = `module.exports = { src: "${filePath}", height: 10, width: 10 };`;
+
+    return { code };
+};
 
 module.exports = {
     process: transform,
