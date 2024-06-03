781d1ce97483661ded907fadc07033a6e2b2e254
Ruggero Cino <ruggero.cino@gmail.com>
2024-06-03 10:17:19 +0200

    Correctly render DAO avatars

--- src/modules/application/components/navigations/navigationDao/navigationDao.tsx
+++ src/modules/application/components/navigations/navigationDao/navigationDao.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useDao } from '@/shared/api/daoService';
+import { ipfsUtils } from '@/shared/utils/ipfsUtils';
 import { Button, DaoAvatar, IconType } from '@aragon/ods';
 import classNames from 'classnames';
 import { useState } from 'react';
@@ -22,6 +23,7 @@ export const NavigationDao: React.FC<INavigationDaoProps> = (props) => {
     const urlParams = { slug };
     const { data: dao } = useDao({ urlParams });
 
+    const daoAvatar = ipfsUtils.cidToSrc(dao?.avatar);
     const links = navigationDaoLinks(dao?.permalink);
 
     return (
@@ -31,19 +33,16 @@ export const NavigationDao: React.FC<INavigationDaoProps> = (props) => {
         >
             <div className="flex flex-row justify-between">
                 <button className="flex flex-row items-center gap-3 p-1">
-                    <DaoAvatar src={dao?.avatar ?? undefined} name={dao?.name} size="md" />
-                    <p className="text-base leading-tight text-neutral-800">{dao?.name}</p>
+                    <DaoAvatar src={daoAvatar} name={dao?.name} size="md" />
+                    <p className="text-base font-normal leading-tight text-neutral-800">{dao?.name}</p>
                 </button>
-                <div className="flex flex-row gap-2">
-                    {/* TODO: render wallet component */}
-                    <Navigation.Trigger onClick={() => setIsDialogOpen(true)} />
-                </div>
+                <Navigation.Trigger onClick={() => setIsDialogOpen(true)} />
             </div>
             <Navigation.Links className="hidden md:flex lg:pl-[56px]" links={links} variant="columns" />
             <Navigation.Dialog links={links} open={isDialogOpen} onOpenChange={setIsDialogOpen}>
                 <div className="flex flex-col gap-4 px-4">
                     <div className="flex grow flex-row justify-between">
-                        <DaoAvatar src={dao?.avatar ?? undefined} name={dao?.name} size="md" />
+                        <DaoAvatar src={daoAvatar} name={dao?.name} size="md" />
                         <div className="flex flex-row gap-3">
                             <Button variant="tertiary" size="md" iconLeft={IconType.COPY} />
                             <Button variant="tertiary" size="md" iconLeft={IconType.APP_EXPLORE} />
