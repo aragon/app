7142b28318bdc242270a108557fd722811b9bd3c
Ruggero Cino <ruggero.cino@gmail.com>
2024-06-03 11:56:56 +0200

    Add tests for NavigationDialog component

--- src/modules/application/components/navigations/navigation/navigationDialog/index.ts
+++ src/modules/application/components/navigations/navigation/navigationDialog/index.ts
@@ -1,4 +1,1 @@
-import dynamic from 'next/dynamic';
-
-export const NavigationDialog = dynamic(() => import('./navigationDialog').then((module) => module.NavigationDialog));
-export { type INavigationDialogProps } from './navigationDialog';
+export { NavigationDialog, type INavigationDialogProps } from './navigationDialog';
