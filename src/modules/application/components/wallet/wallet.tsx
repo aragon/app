fe6d07577864dea3442eafcba5818a540c6a79eb
Ruggero Cino <ruggero.cino@gmail.com>
2024-06-04 16:09:20 +0200

    Remove unused wallet component

--- src/modules/application/components/wallet/wallet.tsx
+++
@@ -1,9 +0,0 @@
-export interface IWalletProps {}
-
-export const Wallet: React.FC<IWalletProps> = () => {
-    return (
-        <button className="rounded-full border border-neutral-100 bg-neutral-0 px-4 py-3.5 text-neutral-500">
-            Connect
-        </button>
-    );
-};
