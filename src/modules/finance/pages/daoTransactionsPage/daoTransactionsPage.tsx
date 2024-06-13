f50ad0444533d0d854fed5b02cfae28df7966d61
Kevin Davis <whomst@kevind.xyz>
2024-06-13 16:04:37 +0200

    feat: write daoTransactionsPage to hold thecontent

--- src/modules/finance/pages/daoTransactionsPage/daoTransactionsPage.tsx
+++ src/modules/finance/pages/daoTransactionsPage/daoTransactionsPage.tsx
@@ -1,5 +1,16 @@
+import { transactionListOptions } from '@/modules/finance/api/financeService/queries/useTransactionList';
+import { Page } from '@/shared/components/page';
+import { QueryClient } from '@tanstack/react-query';
+import { DaoTransactionsPageContent } from './daoTransactionsPageContent';
 export interface IDaoTransactionsPageProps {}
 
-export const DaoTransactionsPage: React.FC<IDaoTransactionsPageProps> = () => {
-    return <div>DAO Transactions</div>;
+export const DaoTransactionsPage: React.FC<IDaoTransactionsPageProps> = async () => {
+    const queryClient = new QueryClient();
+    await queryClient.prefetchInfiniteQuery(transactionListOptions({ queryParams: {} }));
+
+    return (
+        <Page.Container queryClient={queryClient}>
+            <DaoTransactionsPageContent />
+        </Page.Container>
+    );
 };
