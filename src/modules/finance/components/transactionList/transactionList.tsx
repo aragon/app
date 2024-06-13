0306fa974349723aaa7106229265275990f0b622
Kevin Davis <whomst@kevind.xyz>
2024-06-13 16:02:57 +0200

    feat: write txlist component to use ODS impl

---
+++ src/modules/finance/components/transactionList/transactionList.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useTransactionList } from '@/modules/finance/api/financeService/queries/useTransactionList';
+import { useTranslations } from '@/shared/components/translationsProvider';
+import { DataListContainer, DataListPagination, DataListRoot, TransactionDataListItemStructure } from '@aragon/ods';
+
+export interface ITransactionList {}
+
+export const TransactionList: React.FC<ITransactionList> = () => {
+    const { t } = useTranslations();
+
+    const { data: transactionListData, fetchNextPage, isLoading } = useTransactionList({ queryParams: {} });
+
+    const transactionList = transactionListData?.pages.flatMap((page) => page.data);
+
+    return (
+        <DataListRoot
+            entityLabel={t('app.finance.transactionList.entity')}
+            onLoadMore={fetchNextPage}
+            state={isLoading ? 'fetchingNextPage' : 'idle'}
+            pageSize={transactionListData?.pages[0].metadata.limit}
+            itemsCount={transactionListData?.pages[0].metadata.totRecords}
+        >
+            <DataListContainer>
+                {transactionList?.map((transaction) => (
+                    <TransactionDataListItemStructure
+                        chainId={transaction.chainId}
+                        hash={transaction.hash}
+                        key={transaction.hash}
+                        date={transaction.date}
+                        type={transaction.type}
+                        status={transaction.status}
+                        tokenSymbol={transaction.tokenSymbol}
+                        tokenAmount={transaction.tokenAmount}
+                        tokenPrice={transaction.tokenPrice}
+                        tokenAddress={transaction.tokenAddress}
+                    />
+                ))}
+            </DataListContainer>
+            <DataListPagination />
+        </DataListRoot>
+    );
+};
