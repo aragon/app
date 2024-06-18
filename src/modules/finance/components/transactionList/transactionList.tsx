1154d5284b357b31ff1cd9812a10011a915d98db
Kevin Davis <whomst@kevind.xyz>
2024-06-18 20:05:08 +0200

    feat: include empty state and better pagination handling for transactions list

--- src/modules/finance/components/transactionList/transactionList.tsx
+++ src/modules/finance/components/transactionList/transactionList.tsx
@@ -2,11 +2,24 @@
 
 import { useTransactionList } from '@/modules/finance/api/financeService/queries/useTransactionList';
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { DataListContainer, DataListPagination, DataListRoot, TransactionDataListItemStructure } from '@aragon/ods';
+import {
+    DataListContainer,
+    DataListPagination,
+    DataListRoot,
+    EmptyState,
+    TransactionDataListItemStructure,
+} from '@aragon/ods';
+import { type ComponentProps } from 'react';
 
-export interface ITransactionListProps {}
+export interface ITransactionListProps extends ComponentProps<'div'> {
+    /**
+     * Hides the pagination component when set to true.
+     */
+    hidePagination?: boolean;
+}
 
-export const TransactionList: React.FC<ITransactionListProps> = () => {
+export const TransactionList: React.FC<ITransactionListProps> = (props) => {
+    const { hidePagination, children, ...otherProps } = props;
     const { t } = useTranslations();
 
     const { data: transactionListData, fetchNextPage, isLoading } = useTransactionList({ queryParams: {} });
@@ -20,24 +33,34 @@ export const TransactionList: React.FC<ITransactionListProps> = () => {
             state={isLoading ? 'fetchingNextPage' : 'idle'}
             pageSize={transactionListData?.pages[0].metadata.pageSize}
             itemsCount={transactionListData?.pages[0].metadata.totalRecords}
+            {...otherProps}
         >
             <DataListContainer>
-                {transactionList?.map((transaction) => (
-                    <TransactionDataListItemStructure
-                        chainId={transaction.chainId}
-                        hash={transaction.hash}
-                        key={transaction.hash}
-                        date={transaction.date}
-                        type={transaction.type}
-                        status={transaction.status}
-                        tokenSymbol={transaction.tokenSymbol}
-                        tokenAmount={transaction.tokenAmount}
-                        tokenPrice={transaction.tokenPrice}
-                        tokenAddress={transaction.tokenAddress}
+                {transactionList != null ? (
+                    transactionList?.map((transaction) => (
+                        <TransactionDataListItemStructure
+                            chainId={transaction.chainId}
+                            hash={transaction.hash}
+                            key={transaction.hash}
+                            date={transaction.date}
+                            type={transaction.type}
+                            status={transaction.status}
+                            tokenSymbol={transaction.tokenSymbol}
+                            tokenAmount={transaction.tokenAmount}
+                            tokenPrice={transaction.tokenPrice}
+                            tokenAddress={transaction.tokenAddress}
+                        />
+                    ))
+                ) : (
+                    <EmptyState
+                        heading="No transactions found"
+                        description="This will show the DAO's onchain actions when available."
+                        humanIllustration={{ body: 'BLOCKS', hairs: 'MIDDLE', expression: 'SURPRISED' }}
                     />
-                ))}
+                )}
             </DataListContainer>
-            <DataListPagination />
+            {transactionList?.length && !hidePagination && <DataListPagination />}
+            {children}
         </DataListRoot>
     );
 };
