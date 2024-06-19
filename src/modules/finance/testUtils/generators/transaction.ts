07328e2f661ab3f9919dec1e9746ea80ad62ae05
Kevin Davis <whomst@kevind.xyz>
2024-06-19 13:24:12 +0200

    chore: minimize to min viable props for mock of tx

--- src/modules/finance/testUtils/generators/transaction.ts
+++ src/modules/finance/testUtils/generators/transaction.ts
@@ -2,12 +2,6 @@ import { type ITransaction } from '@/modules/finance/api/financeService/domain/t
 
 export const generateTransaction = (transaction?: Partial<ITransaction>): ITransaction => ({
       chainId: 1,
-      tokenAddress: undefined ,
-      tokenSymbol: undefined ,
-      tokenAmount: undefined ,
-      tokenPrice: undefined,
-      type: undefined,
-      status: undefined,
       date: 'Unknown',
       hash: '0x0000000000000000000000000000000000000000',
     ...transaction,
