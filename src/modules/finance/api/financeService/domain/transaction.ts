8d60580a74184f13e7a3d1eff720c0fed843e771
Kevin Davis <whomst@kevind.xyz>
2024-06-21 11:59:11 +0200

    chore: update Tx types

--- src/modules/finance/api/financeService/domain/transaction.ts
+++ src/modules/finance/api/financeService/domain/transaction.ts
@@ -1,52 +1,40 @@
+import { TransactionType as DataListTransactionType } from '@aragon/ods'
+
 export enum TransactionStatus {
-  PENDING = 'PENDING',
   SUCCESS = 'SUCCESS',
   FAILED = 'FAILED',
 }
 
 export enum TransactionType {
-  DEPOSIT = 'DEPOSIT',
-  WITHDRAW = 'WITHDRAW',
-  ACTION = 'ACTION',
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+export const transactionTypeToDataListType: Record<TransactionType, DataListTransactionType> = {
+  withdraw: DataListTransactionType.WITHDRAW,
+  deposit: DataListTransactionType.DEPOSIT,
+};
+
+interface Token {
+  address: string;
+  symbol: string;
+  name: string;
+  type: string;
+  logo: string | null;
 }
 
 export interface ITransaction {
-  /**
-   * The chain ID of the transaction.
-   */
-  chainId: number;
-  /**
-   * The address of the token.
-   */
+  // awaiting chainId from backend to include type, required
+  blockNumber?: number;
+  blockTimestamp?: number;
+  category?: string;
+  daoAddress?: string;
+  fromAddress?: string;
+  network?: string;
+  toAddress?: string;
+  token?: Token;
   tokenAddress?: string;
-  /**
-   * The symbol of the token, e.g. 'ETH' as a string
-   */
-  tokenSymbol?: string;
-  /**
-   * The token value in the transaction.
-   */
-  tokenAmount?: number | string;
-  /**
-   * The estimated fiat value of the transaction.
-   */
-  tokenPrice?: number | string;
-  /**
-   * The type of transaction.
-   * @default TransactionType.ACTION
-   */
-  type?: TransactionType;
-  /**
-   * The current status of a blockchain transaction on the network.
-   * @default TransactionStatus.PENDING
-   */
-  status?: TransactionStatus;
-  /**
-   * The Unix timestamp of the transaction.
-   */
-  date: string;
-  /**
-   * The transaction hash.
-   */
-  hash: `0x${string}`;
+  transactionHash: `0x${string}`;
+  type: keyof typeof transactionTypeToDataListType;
+  value?: string;
 }
