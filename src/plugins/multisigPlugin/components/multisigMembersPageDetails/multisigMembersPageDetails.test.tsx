8d5bd1141216de145df81121073298f7f03ddd66
Ruggero Cino <ruggero.cino@gmail.com>
2024-06-24 12:57:45 +0200

    Implement tests for the members-page-details components

--- src/plugins/multisigPlugin/components/multisigMembersPageDetails/multisigMembersPageDetails.test.tsx
+++ src/plugins/multisigPlugin/components/multisigMembersPageDetails/multisigMembersPageDetails.test.tsx
@@ -1,5 +1,16 @@
-describe('<MultisigMemberPageDetails /> component', () => {
-    it('', () => {
-        //
+import { render, screen } from '@testing-library/react';
+import { MultisigMemberPageDetails, type IMultisigMembersPageDetailsProps } from './multisigMembersPageDetails';
+
+describe('<MultisigMembersPageDetails /> component', () => {
+    const createTestComponent = (props?: Partial<IMultisigMembersPageDetailsProps>) => {
+        const completeProps: IMultisigMembersPageDetailsProps = { ...props };
+
+        return <MultisigMemberPageDetails {...completeProps} />;
+    };
+
+    it('renders info regarding the eligible members', () => {
+        render(createTestComponent());
+        expect(screen.getByText(/multisigMembersPageDetails.eligibleMembers/)).toBeInTheDocument();
+        expect(screen.getByText(/multisigMembersPageDetails.members/)).toBeInTheDocument();
     });
 });
--- src/plugins/tokenPlugin/components/tokenMembersPageDetails/tokenMembersPageDetails.test.tsx
+++ src/plugins/tokenPlugin/components/tokenMembersPageDetails/tokenMembersPageDetails.test.tsx
@@ -1,5 +1,16 @@
-describe('<TokenMemberPageDetails /> component', () => {
-    it('', () => {
-        //
+import { render, screen } from '@testing-library/react';
+import { TokenMembersPageDetails, type ITokenMembersPageDetailsProps } from './tokenMembersPageDetails';
+
+describe('<TokenMembersPageDetails /> component', () => {
+    const createTestComponent = (props?: Partial<ITokenMembersPageDetailsProps>) => {
+        const completeProps: ITokenMembersPageDetailsProps = { ...props };
+
+        return <TokenMembersPageDetails {...completeProps} />;
+    };
+
+    it('renders info regarding the eligible members', () => {
+        render(createTestComponent());
+        expect(screen.getByText(/tokenMembersPageDetails.eligibleMembers/)).toBeInTheDocument();
+        expect(screen.getByText(/tokenMembersPageDetails.members/)).toBeInTheDocument();
     });
 });
