2d67ef81af8b6179e0d21602213e41d6aed51d1f
Ruggero Cino <ruggero.cino@gmail.com>
2024-07-08 10:42:11 +0200

    fix(APP-3323): Use DAO token decimals to format voting power of members

---
+++ src/plugins/tokenPlugin/testUtils/generators/daoTokenSettings.ts
@@ -0,0 +1,15 @@
+import { generateToken } from '@/modules/finance/testUtils';
+import { generateDaoSettings } from '@/shared/testUtils';
+import type { IDaoTokenSettings } from '../../types';
+
+export const generateDaoTokenSettings = (settings?: Partial<IDaoTokenSettings>): IDaoTokenSettings => ({
+    ...generateDaoSettings(),
+    token: generateToken(),
+    settings: {
+        supportThreshold: 0,
+        minDuration: 0,
+        minParticipation: 0,
+        minProposerVotingPower: '0',
+    },
+    ...settings,
+});
--- src/plugins/tokenPlugin/types/daoTokenSettings.ts
+++ src/plugins/tokenPlugin/types/daoTokenSettings.ts
@@ -1,6 +1,14 @@
+import type { IToken } from '@/modules/finance/api/financeService';
 import type { IDaoSettings } from '@/shared/api/daoService';
 
 export interface IDaoTokenSettings extends IDaoSettings {
+    /**
+     * Governance token of the DAO.
+     */
+    token: IToken;
+    /**
+     * Settings of the token-voting DAO.
+     */
     settings: {
         /**
          * Amount of tokens that need to vote "Yes" for a proposal to pass.
