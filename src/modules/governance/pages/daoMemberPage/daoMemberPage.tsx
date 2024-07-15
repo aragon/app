0aa759d5aeb0e21476e2a4068ee5a4be6e4dc3ec
rcaragon <radzionc@aragon.org>
2024-07-15 13:33:40 +0400

    feat: Add lazy loading for images in daoMemberPage

--- src/modules/governance/pages/daoMemberPage/daoMemberPage.tsx
+++ src/modules/governance/pages/daoMemberPage/daoMemberPage.tsx
@@ -1,4 +1,6 @@
 import { Page } from '@/shared/components/page';
+import { QueryClient } from '@tanstack/react-query';
+import { memberOptions } from '../../api/governanceService';
 import { type IDaoMemberPageParams } from '../../types';
 import { DaoMemberPageClient } from './daoMemberPageClient';
 
@@ -11,10 +13,21 @@ export interface IDaoMemberPageProps {
 
 export const DaoMemberPage: React.FC<IDaoMemberPageProps> = async (props) => {
     const { params } = props;
+    const { address, id: daoId } = params;
+
+    const queryClient = new QueryClient();
+
+    const memberUrlParams = { address };
+    const memberQueryParams = { daoId };
+    const memberParams = {
+        urlParams: memberUrlParams,
+        queryParams: memberQueryParams,
+    };
+    await queryClient.prefetchQuery(memberOptions(memberParams));
 
     return (
         <Page.Container>
-            <DaoMemberPageClient address={params.address} daoId={params.id} />
+            <DaoMemberPageClient address={address} daoId={daoId} />
         </Page.Container>
     );
 };
