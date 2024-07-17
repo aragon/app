2bad75f5740638dbbf19c9f1c14df194a782b1b3
Ruggero Cino <ruggero.cino@gmail.com>
2024-07-17 15:53:10 +0200

    Update useCurrentUrl.test.ts

--- src/shared/hooks/useCurrentUrl/useCurrentUrl.test.ts
+++ src/shared/hooks/useCurrentUrl/useCurrentUrl.test.ts
@@ -2,8 +2,33 @@ import { renderHook } from '@testing-library/react';
 import { useCurrentUrl } from './useCurrentUrl';
 
 describe('useCurrentUrl hook', () => {
-    it('returns the current page url', () => {
+    const originalLocation = window.location;
+
+    beforeEach(() => {
+        Object.defineProperty(window, 'location', {
+            configurable: true,
+            enumerable: true,
+            value: new URL(window.location.href),
+        });
+    });
+
+    afterEach(() => {
+        Object.defineProperty(window, 'location', {
+            configurable: true,
+            enumerable: true,
+            value: originalLocation,
+        });
+    });
+
+    it('returns the current page url removing the https protocol', () => {
+        window.location.href = 'https://app.aragon.org';
         const { result } = renderHook(() => useCurrentUrl());
-        expect(result.current).toEqual('localhost/');
+        expect(result.current).toEqual('app.aragon.org/');
+    });
+
+    it('returns the current page url removing the http protocol', () => {
+        window.location.href = 'http://test-page.com';
+        const { result } = renderHook(() => useCurrentUrl());
+        expect(result.current).toEqual('test-page.com/');
     });
 });
