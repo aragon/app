219b50d2f0c450a5328fcdcad7017bd85f553afd
cgero.eth <ruggero.cino@gmail.com>
2024-07-22 16:06:19 +0200

    fix(APP-3410): Reset ErrorBoundary error on route change (#52)

--- src/modules/application/components/errorBoundary/errorBoundary.tsx
+++ src/modules/application/components/errorBoundary/errorBoundary.tsx
@@ -1,46 +1,12 @@
 'use client';
 
-import { Component, type ReactNode } from 'react';
-import { ErrorBoundaryFeedback } from './errorBoundaryFeedback';
+import { usePathname } from 'next/navigation';
+import { ErrorBoundaryClass, type IErrorBoundaryClassProps } from './errorBoundaryClass';
 
-export interface IErrorBoundaryState {
-    /**
-     * Indicates if an error has occurred.
-     */
-    hasError: boolean;
-    /**
-     * The error that occurred.
-     */
-    error?: Error;
-}
+export interface IErrorBoundaryProps extends Omit<IErrorBoundaryClassProps, 'pathname'> {}
 
-export interface IErrorBoundaryProps {
-    /**
-     * The children to render.
-     */
-    children?: ReactNode;
-}
+export const ErrorBoundary: React.FC<IErrorBoundaryProps> = (props) => {
+    const pathname = usePathname();
 
-export class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> {
-    constructor(props: IErrorBoundaryProps) {
-        super(props);
-        this.state = { hasError: false };
-    }
-
-    static getDerivedStateFromError(error: Error): IErrorBoundaryState {
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true, error };
-    }
-
-    componentDidCatch() {
-        // TODO: Report the error to an error reporting service (APP-3107)
-    }
-
-    render() {
-        if (this.state.hasError) {
-            return <ErrorBoundaryFeedback />;
-        }
-
-        return this.props.children;
-    }
-}
+    return <ErrorBoundaryClass pathname={pathname} {...props} />;
+};
