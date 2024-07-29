c1491852e0228dfdeb2ed8b3c4f0fc0b237f8317
Kevin Davis <whomst@kevind.xyz>
2024-07-29 18:52:36 +0200

    chore: update proposal generator

--- src/modules/governance/testUtils/generators/proposal.ts
+++ src/modules/governance/testUtils/generators/proposal.ts
@@ -14,5 +14,6 @@ export const generateProposal = (proposal?: Partial<IProposal>): IProposal => ({
     transactionHash: '0x123',
     resources: [],
     network: Network.ARBITRUM_MAINNET,
+    actions: [],
     ...proposal,
 });
