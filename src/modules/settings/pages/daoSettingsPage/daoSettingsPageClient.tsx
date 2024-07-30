2272d92b56f909762538f5adba3a34721af7e1ea
shan8851 <asamshan456@gmail.com>
2024-07-30 22:06:28 +0100

    chore: save point - init plugin

--- src/modules/settings/pages/daoSettingsPage/daoSettingsPageClient.tsx
+++ src/modules/settings/pages/daoSettingsPage/daoSettingsPageClient.tsx
@@ -1,9 +1,13 @@
 'use client';
 import { DaoDefinitionList } from '@/modules/governance/components/daoDefinitionList/daoDefinitionList';
 import { DaoVersionInfoDefinitionList } from '@/modules/governance/components/daoVersionInfoDefinitionList';
+import { GovernanceSlotId } from '@/modules/governance/constants/moduleSlots';
 import { type IGetDaoParams } from '@/shared/api/daoService';
 import { Page } from '@/shared/components/page';
 import { useTranslations } from '@/shared/components/translationsProvider';
+import { useDaoPluginIds } from '@/shared/hooks/useDaoPluginIds';
+import { useSlotFunction } from '@/shared/hooks/useSlotFunction';
+import type { IDaoSettingTermAndDefinition } from '../../types';
 
 export interface IDaoSettingsPageClientProps {
     /**
@@ -14,7 +18,20 @@ export interface IDaoSettingsPageClientProps {
 
 export const DaoSettingsPageClient: React.FC<IDaoSettingsPageClientProps> = (props) => {
     const { initialParams } = props;
+
     const { t } = useTranslations();
+
+    const daoId = initialParams.urlParams.id;
+
+    const pluginIds = useDaoPluginIds(daoId);
+    const governanceParams = { id: daoId };
+    const governanceInformation = useSlotFunction<IDaoSettingTermAndDefinition[]>({
+        params: governanceParams,
+        slotId: GovernanceSlotId.SETTINGS_GOVERNANCE_INFO,
+        pluginIds,
+    });
+
+    console.log('=====>>>>', governanceInformation);
 
     return (
         <>
