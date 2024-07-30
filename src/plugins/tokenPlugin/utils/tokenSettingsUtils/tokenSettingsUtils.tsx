b993b143bfcb1349776ddee25b00c51c9e93ec83
Ruggero Cino <ruggero.cino@gmail.com>
2024-07-30 12:43:21 +0200

    Update tokenSettingsUtils.tsx

--- src/plugins/tokenPlugin/utils/tokenSettingsUtils/tokenSettingsUtils.tsx
+++ src/plugins/tokenPlugin/utils/tokenSettingsUtils/tokenSettingsUtils.tsx
@@ -1,11 +1,10 @@
 class TokenSettingsUtils {
-    private percentageDecimals = 4;
-
     /**
-     * Percentage values for token-based DAO settings are stored as values with 4 decimals. The function parses the
-     * value set on the blockchain and returns it as percentage value between 0 and 100.
+     * Percentage values for token-based DAO settings are stored values between 0 and 10**6 (defined as RATIO_BASE). The
+     * function parses the value set on the blockchain and returns it as percentage value between 0 and 100.
+     * (see https://github.com/aragon/osx-commons/blob/main/contracts/src/utils/math/Ratio.sol)
      */
-    parsePercentageSetting = (percentage: number) => percentage / 10 ** this.percentageDecimals;
+    parsePercentageSetting = (percentage: number) => percentage / 10 ** 4;
 }
 
 export const tokenSettingsUtils = new TokenSettingsUtils();
