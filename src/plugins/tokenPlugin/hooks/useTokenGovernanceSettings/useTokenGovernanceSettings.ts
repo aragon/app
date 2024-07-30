b6dc3f5a7c6fa5f42809ef346fe48deaf9da81f9
Shan8851.eth <asamshan456@gmail.com>
2024-07-30 14:25:08 +0100

    feat(APP-3453): Implement reusable useMultisigGovernanceSettings and useTokenGovernanceSettings plugin-specific hooks (#58)

---
+++ src/plugins/tokenPlugin/hooks/useTokenGovernanceSettings/useTokenGovernanceSettings.ts
@@ -0,0 +1,103 @@
+import { DaoTokenVotingMode, type IDaoTokenSettings } from '@/plugins/tokenPlugin/types';
+import { useDaoSettings } from '@/shared/api/daoService';
+import { useTranslations } from '@/shared/components/translationsProvider';
+import { formatterUtils, NumberFormat } from '@aragon/ods';
+import { Duration } from 'luxon';
+import type { IDaoSettingTermAndDefinition } from '../../../../modules/settings/types';
+
+interface IUseTokenGovernanceSettingsParams {
+    /**
+     * ID of the Dao.
+     */
+    daoId: string;
+    /**
+     * Settings of the token based Dao.
+     */
+    settings?: IDaoTokenSettings;
+}
+
+export const useTokenGovernanceSettings = (
+    params: IUseTokenGovernanceSettingsParams,
+): IDaoSettingTermAndDefinition[] => {
+    const { daoId, settings } = params;
+
+    const { t } = useTranslations();
+
+    const daoSettingsParams = { daoId };
+
+    const { data: currentSettings } = useDaoSettings<IDaoTokenSettings>(
+        { urlParams: daoSettingsParams },
+        { enabled: settings == null },
+    );
+
+    const processedSettings = settings ?? currentSettings;
+
+    if (!processedSettings) {
+        return [];
+    }
+
+    const tokenSymbol = processedSettings.token.symbol;
+    const tokenSupply = Number(processedSettings.token.totalSupply);
+
+    const formattedApproveThreshold = formatterUtils.formatNumber(processedSettings.settings.supportThreshold, {
+        format: NumberFormat.PERCENTAGE_SHORT,
+    });
+    const formattedMinParticipation = formatterUtils.formatNumber(processedSettings.settings.supportThreshold, {
+        format: NumberFormat.PERCENTAGE_SHORT,
+    });
+
+    const minParticipationTokenValue = (tokenSupply * processedSettings.settings.minParticipation) / 100;
+    const formattedMinParticipationToken = formatterUtils.formatNumber(minParticipationTokenValue, {
+        format: NumberFormat.TOKEN_AMOUNT_LONG,
+    });
+    const durationInSeconds = processedSettings.settings.minDuration ?? 0;
+    const duration = Duration.fromObject({ seconds: durationInSeconds }).shiftTo('days', 'hours', 'minutes');
+    const formattedDuration = t('app.plugins.token.tokenGovernanceSettings.duration', {
+        days: duration.days,
+        hours: duration.hours,
+        minutes: duration.minutes,
+    });
+    const formattedProposerVotingPower = formatterUtils.formatNumber(
+        processedSettings.settings.minProposerVotingPower,
+        { format: NumberFormat.TOKEN_AMOUNT_LONG },
+    );
+
+    return [
+        {
+            term: t('app.plugins.token.tokenGovernanceSettings.approvalThreshold'),
+            definition: t('app.plugins.token.tokenGovernanceSettings.approval', {
+                approvalThreshold: formattedApproveThreshold,
+            }),
+        },
+        {
+            term: t('app.plugins.token.tokenGovernanceSettings.minimumParticipation'),
+            definition: t('app.plugins.token.tokenGovernanceSettings.participation', {
+                participation: formattedMinParticipation,
+                tokenValue: formattedMinParticipationToken,
+                tokenSymbol,
+            }),
+        },
+        { term: t('app.plugins.token.tokenGovernanceSettings.minimumDuration'), definition: formattedDuration },
+        {
+            term: t('app.plugins.token.tokenGovernanceSettings.earlyExecution'),
+            definition:
+                processedSettings.settings.votingMode === DaoTokenVotingMode.EARLY_EXECUTION
+                    ? t('app.plugins.token.tokenGovernanceSettings.yes')
+                    : t('app.plugins.token.tokenGovernanceSettings.no'),
+        },
+        {
+            term: t('app.plugins.token.tokenGovernanceSettings.voteChange'),
+            definition:
+                processedSettings.settings.votingMode === DaoTokenVotingMode.VOTE_REPLACEMENT
+                    ? t('app.plugins.token.tokenGovernanceSettings.yes')
+                    : t('app.plugins.token.tokenGovernanceSettings.no'),
+        },
+        {
+            term: t('app.plugins.token.tokenGovernanceSettings.proposalThreshold'),
+            definition: t('app.plugins.token.tokenGovernanceSettings.proposalAccess', {
+                balance: formattedProposerVotingPower,
+                symbol: tokenSymbol,
+            }),
+        },
+    ];
+};
