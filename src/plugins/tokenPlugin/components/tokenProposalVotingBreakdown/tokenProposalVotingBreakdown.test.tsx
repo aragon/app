92665996d480ebb19fec458c99feaf138e0c386b
Ruggero Cino <ruggero.cino@gmail.com>
2024-07-30 14:58:39 +0200

    Start implementation of token voting breakdown tests

--- src/plugins/tokenPlugin/components/tokenProposalVotingBreakdown/tokenProposalVotingBreakdown.test.tsx
+++ src/plugins/tokenPlugin/components/tokenProposalVotingBreakdown/tokenProposalVotingBreakdown.test.tsx
@@ -1,5 +1,43 @@
+import { generateToken } from '@/modules/finance/testUtils';
+import * as governanceService from '@/modules/governance/api/governanceService';
+import { generateReactQueryResultError, generateReactQueryResultSuccess } from '@/shared/testUtils';
+import { ProposalVotingTab, Tabs } from '@aragon/ods';
+import { render, screen } from '@testing-library/react';
+import { generateDaoTokenSettings, generateTokenProposal } from '../../testUtils';
+import { TokenProposalVotingBreakdown, type ITokenProposalVotingBreakdownProps } from './tokenProposalVotingBreakdown';
+
 describe('<TokenProposalVotingBreakdown /> component', () => {
-    it('', () => {
-        //
+    const useProposalSpy = jest.spyOn(governanceService, 'useProposal');
+
+    afterEach(() => {
+        useProposalSpy.mockReset();
+    });
+
+    const createTestComponent = (props?: Partial<ITokenProposalVotingBreakdownProps>) => {
+        const completeProps: ITokenProposalVotingBreakdownProps = {
+            proposalId: 'proposal-id',
+            ...props,
+        };
+
+        return (
+            <Tabs.Root defaultValue={ProposalVotingTab.BREAKDOWN}>
+                <TokenProposalVotingBreakdown {...completeProps} />
+            </Tabs.Root>
+        );
+    };
+
+    it('returns null when proposal cannot be fetched', () => {
+        useProposalSpy.mockReturnValue(generateReactQueryResultError({ error: new Error() }));
+        render(createTestComponent());
+        expect(screen.queryByRole('tabpanel')).not.toBeInTheDocument();
+    });
+
+    it('WIP', () => {
+        const token = generateToken({ totalSupply: '100' });
+        const settings = { ...generateDaoTokenSettings().settings, minParticipation: 2000 };
+        const proposal = generateTokenProposal({ token, settings });
+        useProposalSpy.mockReturnValue(generateReactQueryResultSuccess({ data: proposal }));
+        render(createTestComponent());
+        expect(screen.getByRole('tabpanel')).toBeInTheDocument();
     });
 });
--- src/plugins/tokenPlugin/testUtils/generators/tokenProposal.ts
+++ src/plugins/tokenPlugin/testUtils/generators/tokenProposal.ts
@@ -1,7 +1,15 @@
+import { generateToken } from '@/modules/finance/testUtils';
 import { generateProposal } from '@/modules/governance/testUtils';
 import type { ITokenProposal } from '../../types';
+import { generateDaoTokenSettings } from './daoTokenSettings';
 
 export const generateTokenProposal = (proposal?: Partial<ITokenProposal>): ITokenProposal => ({
     ...generateProposal(),
+    settings: generateDaoTokenSettings().settings,
+    token: generateToken(),
+    metrics: {
+        totalVotes: 0,
+        votesByOption: [],
+    },
     ...proposal,
 });
