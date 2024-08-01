a9daf56afdc25dbeb9fde8e436a6800acdc64d6c
shan8851 <asamshan456@gmail.com>
2024-08-01 06:42:39 +0100

    chore: linting

--- src/modules/settings/pages/daoSettingsPage/index.ts
+++ src/modules/settings/pages/daoSettingsPage/index.ts
@@ -1,2 +1,2 @@
 export { DaoSettingsPage, type IDaoSettingsPageProps } from './daoSettingsPage';
-export { type IDaoSettingsPageClientProps, DaoSettingsPageClient } from './daoSettingsPageClient';
+export { DaoSettingsPageClient, type IDaoSettingsPageClientProps } from './daoSettingsPageClient';
