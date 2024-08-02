57189f3804940c7da7cd9de59b5f546e68a9ffec
Ruggero Cino <ruggero.cino@gmail.com>
2024-08-02 13:00:47 +0200

    Add tests to error-feedback component

--- src/shared/components/errorFeedback/errorFeedback.test.tsx
+++ src/shared/components/errorFeedback/errorFeedback.test.tsx
@@ -8,13 +8,13 @@ describe('<ErrorFeedback /> component', () => {
         return <ErrorFeedback {...completeProps} />;
     };
 
-    it('renders the error title and description', () => {
+    it('renders the default error title and description', () => {
         render(createTestComponent());
         expect(screen.getByText(/errorFeedback.title/)).toBeInTheDocument();
         expect(screen.getByText(/errorFeedback.description/)).toBeInTheDocument();
     });
 
-    it('renders the correct CTAs', () => {
+    it('renders the correct default CTAs', () => {
         render(createTestComponent());
 
         const exploreDaosButton = screen.getByRole('link', { name: /errorFeedback.link.explore/ });
@@ -29,5 +29,27 @@ describe('<ErrorFeedback /> component', () => {
             'https://aragonassociation.atlassian.net/servicedesk/customer/portal/3',
         );
         expect(reportIssueButton).toHaveAttribute('target', '_blank');
+    });
+
+    it('supports the customisation of the title, description, illustration and primary button', () => {
+        const title = 'test-title';
+        const description = 'test-description';
+        const illustration = 'NOT_FOUND';
+        const primaryButton = { label: 'test-primary-button', href: '/test' };
+        render(createTestComponent({ title, description, illustration, primaryButton }));
+
+        expect(screen.getByText(title)).toBeInTheDocument();
+        expect(screen.getByText(description)).toBeInTheDocument();
+        expect(screen.getByTestId(illustration)).toBeInTheDocument();
+
+        const button = screen.getByRole('link', { name: primaryButton.label });
+        expect(button).toBeInTheDocument();
+        expect(button.getAttribute('href')).toEqual(primaryButton.href);
+    });
+
+    it('hides the report button when the hideReportButton property is set to true', () => {
+        const hideReportButton = true;
+        render(createTestComponent({ hideReportButton }));
+        expect(screen.queryByRole('link', { name: /errorFeedback.link.report/ })).not.toBeInTheDocument();
     });
 });
