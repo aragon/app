45530fad740dd2d3c55b3655855986c9e38e697b
Kevin Davis <whomst@kevind.xyz>
2024-08-10 15:40:59 +0200

    chore: resolve more PR convos

--- src/modules/governance/testUtils/generators/proposalActionWithdrawToken.ts
+++ src/modules/governance/testUtils/generators/proposalActionWithdrawToken.ts
@@ -1,16 +1,5 @@
+import { generateToken } from '@/modules/finance/testUtils';
 import { type IProposalActionWithdrawToken } from '@aragon/ods';
-
-export const generateToken = (
-    token?: Partial<IProposalActionWithdrawToken['token']>,
-): IProposalActionWithdrawToken['token'] => ({
-    name: 'Dai Stablecoin',
-    symbol: 'DAI',
-    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png',
-    decimals: 18,
-    priceUsd: '1.00',
-    address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-    ...token,
-});
 
 export const generateProposalActionWithdrawToken = (
     action?: Partial<IProposalActionWithdrawToken>,
