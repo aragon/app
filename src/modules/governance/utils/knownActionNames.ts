e7cfebdab62a6f5de59509a83811dd9d425f14ab
Kevin Davis <whomst@kevind.xyz>
2024-08-12 12:51:21 +0200

    chore: remove knownactions

--- src/modules/governance/utils/knownActionNames.ts
+++
@@ -1,9 +0,0 @@
-export const knownActionNames: Record<string, string> = {
-    WITHDRAW_TOKEN: 'Withdraw token',
-    TOKEN_MINT: 'Mint token',
-    ADD_MEMBERS: 'Add members',
-    REMOVE_MEMBERS: 'Remove members',
-    UPDATE_METADATA: 'Update metadata',
-    CHANGE_SETTINGS_MULTISIG: 'Multisig settings',
-    CHANGE_SETTINGS_TOKENVOTE: 'Token voting settings',
-};
