0ff5c3eefead37796a885a43870092eaab31a687
Ruggero Cino <ruggero.cino@gmail.com>
2024-08-26 17:40:34 +0200

    Remove transaction dialog

--- src/shared/components/transactionDialog/index.ts
+++
@@ -1,9 +0,0 @@
-export { TransactionDialog } from './transactionDialog';
-export {
-    TransactionDialogStep,
-    type ITransactionDialogActionParams,
-    type ITransactionDialogProps,
-    type ITransactionDialogStep,
-    type ITransactionDialogStepMeta,
-    type TransactionDialogPrepareReturn,
-} from './transactionDialog.api';
