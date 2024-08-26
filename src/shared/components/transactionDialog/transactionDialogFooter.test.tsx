45fca839eb5a441459d3d6631df3a28f109e0487
Ruggero Cino <ruggero.cino@gmail.com>
2024-08-26 16:03:01 +0200

    Update transaction-dialog-footer to support success link

--- src/shared/components/transactionDialog/transactionDialogFooter.test.tsx
+++ src/shared/components/transactionDialog/transactionDialogFooter.test.tsx
@@ -1,9 +1,10 @@
 import { IconType } from '@aragon/ods';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
+import type { TransactionReceipt } from 'viem';
 import * as useDialogContext from '../dialogProvider';
-import { ITransactionDialogStep, TransactionDialogStep } from './transactionDialog.api';
-import { ITransactionDialogFooterProps, TransactionDialogFooter } from './transactionDialogFooter';
+import { type ITransactionDialogStep, TransactionDialogStep } from './transactionDialog.api';
+import { type ITransactionDialogFooterProps, TransactionDialogFooter } from './transactionDialogFooter';
 
 describe('<TransactionDialogFooter /> component', () => {
     const useDialogContextSpy = jest.spyOn(useDialogContext, 'useDialogContext');
@@ -16,6 +17,7 @@ describe('<TransactionDialogFooter /> component', () => {
         const completeProps: ITransactionDialogFooterProps = {
             submitLabel: 'label',
             onError: jest.fn(),
+            successLink: { href: '', label: '' },
             ...props,
         };
 
@@ -101,5 +103,54 @@ describe('<TransactionDialogFooter /> component', () => {
         render(createTestComponent({ submitLabel, activeStep, onError }));
         await userEvent.click(screen.getByRole('button', { name: submitLabel }));
         expect(approveAction).toHaveBeenCalledWith({ onError });
+    });
+
+    it('does not throw error on submit click when activeStep has no action', async () => {
+        const submitLabel = 'submit';
+        const activeStep = {
+            id: TransactionDialogStep.PREPARE,
+            meta: { state: 'idle', action: undefined },
+        } as unknown as ITransactionDialogStep;
+        render(createTestComponent({ submitLabel, activeStep }));
+        await userEvent.click(screen.getByRole('button', { name: submitLabel }));
+        expect(screen.getByRole('button', { name: submitLabel })).toBeInTheDocument();
+    });
+
+    it('renders the success link label on transaction success', () => {
+        const successLink = { label: 'View proposal', href: '/proposal/my-proposal' };
+        const activeStep = {
+            id: TransactionDialogStep.CONFIRM,
+            meta: { state: 'success' },
+        } as unknown as ITransactionDialogStep;
+        render(createTestComponent({ successLink, activeStep }));
+        const link = screen.getByRole('link', { name: successLink.label });
+        expect(link).toBeInTheDocument();
+        expect(link.getAttribute('href')).toEqual(successLink.href);
+    });
+
+    it('supports success href to be a function to build the link based on the transaction receipt', () => {
+        const txReceipt = { from: '0x123' } as unknown as TransactionReceipt;
+        const href = (txReceipt: TransactionReceipt) => `/custom-href-${txReceipt.from}`;
+        const successLink = { label: 'View proposal', href };
+        const activeStep = {
+            id: TransactionDialogStep.CONFIRM,
+            meta: { state: 'success' },
+        } as unknown as ITransactionDialogStep;
+        render(createTestComponent({ txReceipt, successLink, activeStep }));
+        expect(screen.getByRole('link').getAttribute('href')).toEqual(href(txReceipt));
+    });
+
+    it('closes the dialog on success link click', async () => {
+        const href = () => `/custom-link`;
+        const successLink = { label: 'View proposal', href };
+        const close = jest.fn();
+        useDialogContextSpy.mockReturnValue({ open: jest.fn(), close });
+        const activeStep = {
+            id: TransactionDialogStep.CONFIRM,
+            meta: { state: 'success' },
+        } as unknown as ITransactionDialogStep;
+        render(createTestComponent({ successLink, activeStep }));
+        await userEvent.click(screen.getByText(successLink.label));
+        expect(close).toHaveBeenCalled();
     });
 });
