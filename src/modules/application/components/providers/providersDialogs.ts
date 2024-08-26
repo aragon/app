da221a24035579eaac9c3fad593088445bb58b14
Ruggero Cino <ruggero.cino@gmail.com>
2024-08-26 17:39:33 +0200

    Remove out-of-scope changes

--- src/modules/application/components/providers/providersDialogs.ts
+++ src/modules/application/components/providers/providersDialogs.ts
@@ -1,8 +1,6 @@
-import { governanceDialogs } from '@/modules/governance/constants/moduleDialogs';
 import type { IDialogComponentDefinitions } from '@/shared/components/dialogProvider';
 import { applicationDialogs } from '../../constants/moduleDialogs';
 
 export const providersDialogs: Record<string, IDialogComponentDefinitions> = {
     ...applicationDialogs,
-    ...governanceDialogs,
 };
--- src/modules/governance/dialogs/publishProposalDialog/index.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-
-export const PublishProposalDialog = dynamic(() =>
-    import('./publishProposalDialog').then((mod) => mod.PublishProposalDialog),
-);
-export type { IPublishProposalDialogProps } from './publishProposalDialog';
