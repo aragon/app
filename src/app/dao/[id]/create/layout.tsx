7b969120ad608457a622ac08be26e70961721ae3
Kevin Davis <whomst@kevind.xyz>
2024-08-28 19:24:04 +0200

    chore: add comment with ticket for future work

--- src/app/dao/[id]/create/layout.tsx
+++ src/app/dao/[id]/create/layout.tsx
@@ -1,3 +1,4 @@
 import { LayoutWizardCreateProposal } from '@/modules/governance/layouts/layoutWizardCreateProposal';
 
+//TODO: Update the export for LayoutWizardCreateDao when available (APP-3589)
 export default LayoutWizardCreateProposal;
