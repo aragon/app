15cd873ee555f2c80c64927c9289a97ef688eadf
Kevin Davis <whomst@kevind.xyz>
2024-08-28 18:56:49 +0200

    chore: update interface for cleaner omit

--- src/modules/governance/layouts/layoutWizardCreateProposal/layoutWizardCreateProposal.tsx
+++ src/modules/governance/layouts/layoutWizardCreateProposal/layoutWizardCreateProposal.tsx
@@ -1,8 +1,7 @@
 import { type ILayoutWizardProps, LayoutWizard } from '@/modules/application/components/layouts/layoutWizard';
 
-export interface ILayoutWizardCreateProposalProps extends ILayoutWizardProps {}
+export interface ILayoutWizardCreateProposalProps extends Omit<ILayoutWizardProps, 'name'> {}
 
 export const LayoutWizardCreateProposal: React.FC<ILayoutWizardCreateProposalProps> = (props) => {
-    const { name, ...otherProps } = props;
-    return <LayoutWizard name="app.application.layoutWizardCreateProposal.name" {...otherProps} />;
+    return <LayoutWizard name="app.application.layoutWizardCreateProposal.name" {...props} />;
 };
