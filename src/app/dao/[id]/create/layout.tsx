cbf7b82c42e0d09fbbf133221bb0128eff43779e
Ruggero Cino <ruggero.cino@gmail.com>
2024-08-29 12:32:52 +0200

    chore: Move LayoutWizardCreateProposal component to /components folder

--- src/app/dao/[id]/create/layout.tsx
+++ src/app/dao/[id]/create/layout.tsx
@@ -1,4 +1,4 @@
-import { LayoutWizardCreateProposal } from '@/modules/governance/layouts/layoutWizardCreateProposal';
+import { LayoutWizardCreateProposal } from '@/modules/governance/components/layoutWizardCreateProposal';
 
 //TODO: Update the export for LayoutWizardCreateDao when available (APP-3589)
 export default LayoutWizardCreateProposal;
