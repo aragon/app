407778f14fa87bc255e5641a586579bb3b33329d
shan8851 <asamshan456@gmail.com>
2024-08-29 10:47:47 +0100

    chore: Move translations into the correct module

--- src/modules/governance/components/createProposalForm/createProposalFormMetadata/createProposalFormMetadata.tsx
+++ src/modules/governance/components/createProposalForm/createProposalFormMetadata/createProposalFormMetadata.tsx
@@ -9,46 +9,46 @@ export const CreateProposalFormMetadata: React.FC<ICreateProposalFormMetadataPro
     const { t } = useTranslations();
 
     const titleField = useFormField('title', {
-        label: t('app.createProposalForm.metadata.title.title'),
+        label: t('app.governance.createProposalForm.metadata.title.title'),
         rules: { required: true },
         defaultValue: '',
     });
     const summaryField = useFormField('summary', {
-        label: t('app.createProposalForm.metadata.summary.title'),
+        label: t('app.governance.createProposalForm.metadata.summary.title'),
         rules: { required: true },
     });
 
     const { ref: bodyRef, ...bodyField } = useFormField('body', {
-        label: t('app.createProposalForm.metadata.body.title'),
+        label: t('app.governance.createProposalForm.metadata.body.title'),
     });
     const { ref: addActionsRef, ...addActionsField } = useFormField('addActions', {
-        label: t('app.createProposalForm.metadata.actions.title'),
+        label: t('app.governance.createProposalForm.metadata.actions.title'),
         defaultValue: true,
     });
 
     return (
         <div className="flex flex-col gap-10">
             <InputText
-                helpText={t('app.createProposalForm.metadata.title.helpText')}
-                placeholder={t('app.createProposalForm.metadata.title.placeholder')}
+                helpText={t('app.governance.createProposalForm.metadata.title.helpText')}
+                placeholder={t('app.governance.createProposalForm.metadata.title.placeholder')}
                 {...titleField}
             />
             <TextArea
-                helpText={t('app.createProposalForm.metadata.summary.helpText')}
-                placeholder={t('app.createProposalForm.metadata.summary.placeholder')}
+                helpText={t('app.governance.createProposalForm.metadata.summary.helpText')}
+                placeholder={t('app.governance.createProposalForm.metadata.summary.placeholder')}
                 {...summaryField}
             />
             <TextAreaRichText
-                helpText={t('app.createProposalForm.metadata.body.helpText')}
-                placeholder={t('app.createProposalForm.metadata.body.placeholder')}
+                helpText={t('app.governance.createProposalForm.metadata.body.helpText')}
+                placeholder={t('app.governance.createProposalForm.metadata.body.placeholder')}
                 isOptional={true}
                 immediatelyRender={false}
                 {...bodyField}
             />
             <ResourcesInput />
             <Switch
-                helpText={t('app.createProposalForm.metadata.actions.helpText')}
-                inlineLabel={t('app.createProposalForm.metadata.actions.label')}
+                helpText={t('app.governance.createProposalForm.metadata.actions.helpText')}
+                inlineLabel={t('app.governance.createProposalForm.metadata.actions.label')}
                 onCheckedChanged={addActionsField.onChange}
                 checked={addActionsField.value}
                 {...addActionsField}
