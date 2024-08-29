3d8f5cc75c3c2c7e3338026862866bd353670f86
shan8851 <asamshan456@gmail.com>
2024-08-29 10:57:44 +0100

    chore: Use input container to avoid repitition

--- src/modules/governance/components/createProposalForm/createProposalFormMetadata/resourcesInput/resourcesInput.tsx
+++ src/modules/governance/components/createProposalForm/createProposalFormMetadata/resourcesInput/resourcesInput.tsx
@@ -1,5 +1,5 @@
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { Button, IconType, Tag } from '@aragon/ods';
+import { Button, IconType, InputContainer, Tag } from '@aragon/ods';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { ResourceItem } from '../resourceItem';
 
@@ -17,19 +17,13 @@ export const ResourcesInput: React.FC<IResourcesInputProps> = () => {
 
     return (
         <div className="flex flex-col gap-2 md:gap-3">
-            <label className="flex flex-col gap-0.5 md:gap-1">
-                <div className="flex flex-row items-center gap-3">
-                    <p className="text-base font-normal leading-tight text-neutral-800 md:text-lg">
-                        {t('app.governance.createProposalForm.metadata.resources.title')}
-                    </p>
-                    <Tag variant="neutral" label="Optional" />
-                </div>
-
-                <p className="text-sm font-normal leading-normal text-neutral-500 md:text-base">
-                    {t('app.governance.createProposalForm.metadata.resources.helpText')}
-                </p>
-            </label>
-
+            <InputContainer
+                id="resourcesInput"
+                label={t('app.governance.createProposalForm.metadata.resources.title')}
+                isOptional={true}
+                helpText={t('app.governance.createProposalForm.metadata.resources.helpText')}
+                useCustomWrapper={true}
+            />
             {fields.length > 0 && (
                 <div className="flex flex-col gap-3 md:gap-2">
                     {fields.map((field, index) => (
