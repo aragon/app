7c0edf7c5ad9a5d5f1a934e9569945db17fd07fe
Kevin Davis <65736142+thekidnamedkd@users.noreply.github.com>
2024-08-30 00:50:13 -0700

    feat(APP-3580): Implement CreateProposalFormActions component (#112)

--- src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.tsx
+++ src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.tsx
@@ -1,5 +1,24 @@
+import { useTranslations } from '@/shared/components/translationsProvider';
+import { Button, CardEmptyState, IconType } from '@aragon/ods';
+
 export interface ICreateProposalFormActionsProps {}
 
 export const CreateProposalFormActions: React.FC<ICreateProposalFormActionsProps> = () => {
-    return <div>Add actions</div>;
+    const { t } = useTranslations();
+
+    return (
+        <div className="flex flex-col gap-y-10">
+            <CardEmptyState
+                heading={t('app.governance.createProposalForm.actions.empty.heading')}
+                description={t('app.governance.createProposalForm.actions.empty.description')}
+                objectIllustration={{ object: 'SMART_CONTRACT' }}
+                isStacked={false}
+            />
+            <div>
+                <Button variant="primary" size="md" iconLeft={IconType.PLUS}>
+                    {t('app.governance.createProposalForm.actions.action')}
+                </Button>
+            </div>
+        </div>
+    );
 };
