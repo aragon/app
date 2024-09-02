3838596df45a6e815defda810569a5570ed23df2
Kevin Davis <whomst@kevind.xyz>
2024-09-02 14:47:58 +0200

    feat: add open graph metadata to DAO proposal creation links

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -1,3 +1,5 @@
 import { LayoutWizardCreateProposal } from '@/modules/governance/components/layoutWizardCreateProposal';
+import { daoUtils } from '@/shared/utils/daoUtils';
 
+export const generateMetadata = daoUtils.generateMetadata;
 export default LayoutWizardCreateProposal;
