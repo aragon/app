9ec176433a6b1e61f8fa0c49bdb5e2802ad7b2b7
Kevin Davis <whomst@kevind.xyz>
2024-09-02 19:35:17 +0200

    chore: remove unused import

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -1,5 +1,4 @@
 import { LayoutWizardCreateProposal } from '@/modules/governance/components/layoutWizardCreateProposal';
-import { daoUtils } from '@/shared/utils/daoUtils';
 
 //TODO: Update the export for LayoutWizardCreateDao when available (APP-3589)
 export default LayoutWizardCreateProposal;
