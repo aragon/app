71ea7479fdb78012c930edc66b6e218b2d625f5e
Kevin Davis <whomst@kevind.xyz>
2024-09-02 19:33:15 +0200

    chore: reverse metadata usage

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -1,5 +1,5 @@
 import { LayoutWizardCreateProposal } from '@/modules/governance/components/layoutWizardCreateProposal';
 import { daoUtils } from '@/shared/utils/daoUtils';
 
-export const generateMetadata = daoUtils.generateMetadata;
+//TODO: Update the export for LayoutWizardCreateDao when available (APP-3589)
 export default LayoutWizardCreateProposal;
