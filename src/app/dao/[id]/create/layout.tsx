1fdff7fa3c2da06ed83ebd97682b188aded727b6
Kevin Davis <whomst@kevind.xyz>
2024-09-02 17:01:00 +0200

    chore: remove create DAO

--- src/app/dao/[id]/create/layout.tsx
+++ src/app/dao/[id]/create/layout.tsx
@@ -1,6 +1,4 @@
 import { LayoutWizardCreateProposal } from '@/modules/governance/components/layoutWizardCreateProposal';
-import { daoUtils } from '@/shared/utils/daoUtils';
 
-export const generateMetadata = daoUtils.generateMetadata;
 //TODO: Update the export for LayoutWizardCreateDao when available (APP-3589)
 export default LayoutWizardCreateProposal;
