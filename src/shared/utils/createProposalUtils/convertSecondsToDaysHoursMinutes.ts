88aab457432a104b4ed26329c15028602000370e
shan8851 <asamshan456@gmail.com>
2024-09-03 10:35:32 +0100

    chore: refactor util function

--- src/shared/utils/createProposalUtils/convertSecondsToDaysHoursMinutes.ts
+++ src/shared/utils/createProposalUtils/convertSecondsToDaysHoursMinutes.ts
@@ -1,8 +1,6 @@
-export const convertSecondsToDaysHoursMinutes = (seconds: number) => {
-    const days = Math.floor(seconds / (24 * 60 * 60));
-    seconds -= days * 24 * 60 * 60;
-    const hours = Math.floor(seconds / (60 * 60));
-    seconds -= hours * 60 * 60;
-    const minutes = Math.floor(seconds / 60);
+export const convertSecondsToDaysHoursMinutes = (totalSeconds: number) => {
+    const days = Math.floor(totalSeconds / (24 * 60 * 60));
+    const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
     return { days, hours, minutes };
 };
