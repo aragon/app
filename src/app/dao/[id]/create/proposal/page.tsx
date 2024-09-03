4e98918c0d98acb8144e27857ea9c9223863e1a0
shan8851 <asamshan456@gmail.com>
2024-09-03 17:49:12 +0100

    chore: Restore page.tsx file

--- src/app/dao/[id]/create/proposal/page.tsx
+++ src/app/dao/[id]/create/proposal/page.tsx
@@ -1,16 +1,3 @@
 import { CreateProposalPage } from '@/modules/governance/pages/createProposalPage';
-import { type IDaoPageParams } from '@/shared/types';
 
-export interface ICreateProposalPageProps {
-    /**
-     * DAO page parameters.
-     */
-    params: IDaoPageParams;
-}
-
-const CreateProposal: React.FC<ICreateProposalPageProps> = (props) => {
-    const { params } = props;
-    return <CreateProposalPage params={params} />;
-};
-
-export default CreateProposal;
+export default CreateProposalPage;
