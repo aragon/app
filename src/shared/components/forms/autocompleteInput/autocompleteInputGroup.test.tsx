6925bf182a025b79516c653ba6e818594c59e04a
Ruggero Cino <ruggero.cino@gmail.com>
2024-09-03 17:37:01 +0200

    Add tests for input item and group components

--- src/shared/components/forms/autocompleteInput/autocompleteInputGroup.test.tsx
+++ src/shared/components/forms/autocompleteInput/autocompleteInputGroup.test.tsx
@@ -1,5 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import { AutocompleteInputGroup, type IAutocompleteInputGroupProps } from './autocompleteInputGroup';
+
 describe('<AutocompleteInputGroup /> component', () => {
-    it('', () => {
-        //
+    const createTestComponent = (props?: Partial<IAutocompleteInputGroupProps>) => {
+        const completeProps: IAutocompleteInputGroupProps = {
+            ...props,
+        };
+
+        return <AutocompleteInputGroup {...completeProps} />;
+    };
+
+    it('renders the children property', () => {
+        const children = 'test-child';
+        render(createTestComponent({ children }));
+        expect(screen.getByText(children)).toBeInTheDocument();
+    });
+
+    it('renders the group info when defined', () => {
+        const group = { id: '0', name: 'test-group', info: 'some-info' };
+        render(createTestComponent({ group }));
+        expect(screen.getByText(group.name)).toBeInTheDocument();
+        expect(screen.getByText(group.info)).toBeInTheDocument();
     });
 });
