f1175074dbb7826dbfe794fd725a18a4074dddd0
Ruggero Cino <ruggero.cino@gmail.com>
2024-09-04 16:11:45 +0200

    Implement tests for keyboard-shortcut component

--- src/shared/components/keyboardShortcut/keyboardShortcut.tsx
+++ src/shared/components/keyboardShortcut/keyboardShortcut.tsx
@@ -6,14 +6,14 @@ export interface IKeyboardShortcutProps extends ComponentProps<'div'> {}
 export const KeyboardShortcut: React.FC<IKeyboardShortcutProps> = (props) => {
     const { className, children, ...otherProps } = props;
 
+    const containerClassName = classNames(
+        'flex h-[18px] w-5 items-center justify-center rounded border border-neutral-100 bg-neutral-0',
+        'text-xs font-normal leading-tight text-neutral-800',
+        className,
+    );
+
     return (
-        <div
-            className={classNames(
-                'flex h-[18px] w-5 items-center justify-center rounded border border-neutral-100 bg-neutral-0 text-xs font-normal leading-tight text-neutral-800',
-                className,
-            )}
-            {...otherProps}
-        >
+        <div className={containerClassName} {...otherProps}>
             {children}
         </div>
     );
