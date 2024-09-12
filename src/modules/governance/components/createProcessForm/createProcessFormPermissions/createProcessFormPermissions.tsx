c2c1140ad8823374cc9bf590cd772015417317a5
Kevin Davis <whomst@kevind.xyz>
2024-09-12 11:34:52 +0300

    chore: TBD page step 3

--- src/modules/governance/components/createProcessForm/createProcessFormPermissions/createProcessFormPermissions.tsx
+++ src/modules/governance/components/createProcessForm/createProcessFormPermissions/createProcessFormPermissions.tsx
@@ -1,5 +1,19 @@
+import { Card, Heading } from '@aragon/ods';
+
 export interface ICreateProcessFormPermissionProps {}
 
 export const CreateProcessFormPermissions: React.FC<ICreateProcessFormPermissionProps> = () => {
-    return <div>CreateProcessFormPermissions</div>;
+    return (
+        <Card className="flex grow flex-col justify-center p-6">
+            <div className="mx-auto flex flex-col space-y-2">
+                <Heading size="h1">TBD</Heading>
+                <p>Open questions at:</p>
+                <ul className="flex flex-col items-start">
+                    <li>Proposal Creation (Threshold)</li>
+                    <li>Permissions to protected OSx contracts</li>
+                    <li>Permissions to any smart contract actions</li>
+                </ul>
+            </div>
+        </Card>
+    );
 };
