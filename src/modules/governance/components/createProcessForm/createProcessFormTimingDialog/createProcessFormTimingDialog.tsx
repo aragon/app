4bbcb4ae7b44b12ee225aadc3d5b40e33c59a61e
Kevin Davis <whomst@kevind.xyz>
2024-09-12 14:51:05 +0300

    chore: begin timing dialog

---
+++ src/modules/governance/components/createProcessForm/createProcessFormTimingDialog/createProcessFormTimingDialog.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const createProcessFormTimingDialog = (props: Props) => {
+  return (
+    <div>createProcessFormTimingDialog</div>
+  )
+}
+
+export default createProcessFormTimingDialog
