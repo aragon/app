1ac7b26bd1ee24382e9cb0f9e99a7a1a0d357258
shan8851 <asamshan456@gmail.com>
2024-09-13 11:01:25 +0300

    Add entry point dialog

--- src/modules/settings/pages/daoSettingsPage/daoSettingsPageClient.tsx
+++ src/modules/settings/pages/daoSettingsPage/daoSettingsPageClient.tsx
@@ -5,9 +5,10 @@ import { DaoMembersInfo } from '@/modules/settings/components/daoMembersInfo';
 import { useDao } from '@/shared/api/daoService';
 import { Page } from '@/shared/components/page';
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { Card } from '@aragon/ods';
+import { Card, Dialog, IllustrationObject } from '@aragon/ods';
 import { DaoSettingsInfo } from '../../components/daoSettingsInfo';
 import { DaoVersionInfo } from '../../components/daoVersionInfo';
+import { useState } from 'react';
 
 export interface IDaoSettingsPageClientProps {
     /**
@@ -18,6 +19,7 @@ export interface IDaoSettingsPageClientProps {
 
 export const DaoSettingsPageClient: React.FC<IDaoSettingsPageClientProps> = (props) => {
     const { daoId } = props;
+    const [dialogOpen, setDialogOpen] = useState(false);
 
     const { t } = useTranslations();
 
@@ -32,7 +34,12 @@ export const DaoSettingsPageClient: React.FC<IDaoSettingsPageClientProps> = (pro
         <>
             <Page.Main
                 title={t('app.settings.daoSettingsPage.main.title')}
-                action={{ label: 'Process', href: `/dao/${daoId}/create/process` }}
+                action={{
+                    label: 'Process',
+                    onClick: () => {
+                        setDialogOpen(true);
+                    },
+                }}
             >
                 <Page.Section title={t('app.settings.daoSettingsPage.main.settingsInfoTitle')}>
                     <DaoSettingsInfo dao={dao} />
@@ -53,6 +60,40 @@ export const DaoSettingsPageClient: React.FC<IDaoSettingsPageClientProps> = (pro
                     <DaoVersionInfo dao={dao} />
                 </Page.Section>
             </Page.Aside>
+            <Dialog.Root open={dialogOpen} onOpenChange={setDialogOpen}>
+                <Dialog.Header title="Create governance process" />
+                <Dialog.Content className="flex flex-col gap-4">
+                    <p className="text-base font-normal leading-normal text-neutral-500">
+                        Define any kind of governance process to help your onchain organisation making great decisions
+                        and only allow to execute what it’s right for certain decisions 😉
+                    </p>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex items-center justify-between">
+                            <IllustrationObject className="size-16" object="LABELS" />
+                            <p className="text-lg font-normal leading-normal text-neutral-800">
+                                Describe governance process
+                            </p>
+                            <p className="text-base font-normal leading-normal text-neutral-500">Step 1</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <IllustrationObject className="size-16" object="USERS" />
+                            <p className="text-lg font-normal leading-normal text-neutral-800">
+                                Setup governance process
+                            </p>
+                            <p className="text-base font-normal leading-normal text-neutral-500">Step 2</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <IllustrationObject className="size-16" object="SETTINGS" />
+                            <p className="text-lg font-normal leading-normal text-neutral-800">Manage permissions</p>
+                            <p className="text-base font-normal leading-normal text-neutral-500">Step 3</p>
+                        </div>
+                    </div>
+                </Dialog.Content>
+                <Dialog.Footer
+                    primaryAction={{ label: 'Create new', href: `/dao/${daoId}/create/process` }}
+                    secondaryAction={{ label: 'Cancel', onClick: () => setDialogOpen(false) }}
+                />
+            </Dialog.Root>
         </>
     );
 };
