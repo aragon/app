5d15a4d03ec9ad5c09e7fe57bf5a0c9655184149
Ruggero Cino <ruggero.cino@gmail.com>
2024-09-19 18:34:18 +0200

    Update voteDialog.tsx

--- src/modules/governance/dialogs/voteDialog/voteDialog.tsx
+++ src/modules/governance/dialogs/voteDialog/voteDialog.tsx
@@ -7,7 +7,7 @@ import {
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { useStepper } from '@/shared/hooks/useStepper';
 import { useSupportedDaoPlugin } from '@/shared/hooks/useSupportedDaoPlugin';
-import { DataList, invariant, type VoteIndicator, VoteProposalDataListItemStructure } from '@aragon/ods';
+import { invariant, type VoteIndicator, VoteProposalDataListItemStructure } from '@aragon/ods';
 import { useAccount } from 'wagmi';
 import { voteDialogUtils } from './voteDialogUtils';
 
@@ -68,13 +68,7 @@ export const VoteDialog: React.FC<IVoteDialogProps> = (props) => {
             stepper={stepper}
             prepareTransaction={handlePrepareTransaction}
         >
-            <DataList.Root entityLabel="">
-                <VoteProposalDataListItemStructure
-                    proposalId={title}
-                    proposalTitle={summary}
-                    voteIndicator={vote.label}
-                />
-            </DataList.Root>
+            <VoteProposalDataListItemStructure proposalId={title} proposalTitle={summary} voteIndicator={vote.label} />
         </TransactionDialog>
     );
 };
