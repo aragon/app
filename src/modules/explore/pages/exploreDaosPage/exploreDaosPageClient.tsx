ddba1d6c63b834769dc5351cf1230286b0b6aa9f
Kevin Davis <whomst@kevind.xyz>
2024-09-27 12:36:40 +0200

    feat: add wallet button to main explore page

--- src/modules/explore/pages/exploreDaosPage/exploreDaosPageClient.tsx
+++ src/modules/explore/pages/exploreDaosPage/exploreDaosPageClient.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { Button, IconType, Toggle, ToggleGroup } from '@aragon/ods';
+import { ApplicationDialog } from '@/modules/application/constants/moduleDialogs';
+import { useDialogContext } from '@/shared/components/dialogProvider';
+import { Button, IconType, Toggle, ToggleGroup, Wallet } from '@aragon/ods';
 import { useState } from 'react';
+import { mainnet } from 'viem/chains';
 import { useAccount } from 'wagmi';
 import type { IGetDaoListParams } from '../../api/daoExplorerService';
 import { DaoList } from '../../components/daoList';
@@ -16,28 +19,40 @@ export interface IExploreDaosPageClientProps {
 export const ExploreDaosPageClient: React.FC<IExploreDaosPageClientProps> = (props) => {
     const { initialParams } = props;
 
-    const { address } = useAccount();
+    const { address, isConnected } = useAccount();
 
     const [daoFilter, setDaoFilter] = useState<string | undefined>('all');
+
+    const walletUser = address != null ? { address } : undefined;
 
     const daoListParams = daoFilter === 'all' ? initialParams : undefined;
     const daoListMemberParams =
         daoFilter === 'member' ? { urlParams: { address: address! }, queryParams: {} } : undefined;
 
+    const { open } = useDialogContext();
+
+    const handleWalletClick = () => {
+        const dialog = isConnected ? ApplicationDialog.USER : ApplicationDialog.CONNECT_WALLET;
+        open(dialog);
+    };
+
     return (
         <div className="flex grow flex-col gap-5">
-            <div className="flex items-center gap-x-2 md:gap-x-3">
-                <ToggleGroup isMultiSelect={false} onChange={setDaoFilter} value={daoFilter}>
-                    <Toggle value="all" label="All DAOs" />
-                    <Toggle value="member" label="Member" disabled={address == null} />
-                </ToggleGroup>
-                <Button
-                    iconLeft={IconType.PLUS}
-                    href="/create/dao"
-                    className="!rounded-full"
-                    variant="tertiary"
-                    size="md"
-                />
+            <div className="flex justify-between">
+                <div className="flex w-full items-center gap-x-2 md:gap-x-3">
+                    <ToggleGroup isMultiSelect={false} onChange={setDaoFilter} value={daoFilter}>
+                        <Toggle value="all" label="All DAOs" />
+                        <Toggle value="member" label="Member" disabled={address == null} />
+                    </ToggleGroup>
+                    <Button
+                        iconLeft={IconType.PLUS}
+                        href="/create/dao"
+                        className="!rounded-full"
+                        variant="tertiary"
+                        size="md"
+                    />
+                </div>
+                <Wallet className="self-end" user={walletUser} onClick={handleWalletClick} chainId={mainnet.id} />
             </div>
             <DaoList initialParams={daoListParams} daoListByMemberParams={daoListMemberParams} />
         </div>
