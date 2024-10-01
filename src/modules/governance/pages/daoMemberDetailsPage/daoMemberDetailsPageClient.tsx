f4fecb26df38b0401355837524892a46b7bdc173
Shan8851.eth <asamshan456@gmail.com>
2024-10-01 08:54:43 +0100

    feat(APP-3623): Remove vote and proposal lists from dao member profile page (#155)

--- src/modules/governance/pages/daoMemberDetailsPage/daoMemberDetailsPageClient.tsx
+++ src/modules/governance/pages/daoMemberDetailsPage/daoMemberDetailsPageClient.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { DaoList } from '@/modules/explore/components/daoList';
-import { DaoProposalList } from '@/modules/governance/components/daoProposalList';
-import { VoteList } from '@/modules/governance/components/voteList';
 import { useDao } from '@/shared/api/daoService';
 import { Page } from '@/shared/components/page';
 import { type IPageHeaderStat } from '@/shared/components/page/pageHeader/pageHeaderStat';
@@ -101,10 +99,6 @@ export const DaoMemberDetailsPageClient: React.FC<IDaoMemberDetailsPageClientPro
         { label: memberName },
     ];
 
-    const proposalsByMemberParams = { queryParams: { daoId, creatorAddress: address, pageSize: 3 } };
-
-    const votesByMemberAddressQueryParams = { queryParams: { daoId, address, includeInfo: true, pageSize: 5 } };
-
     const daoListByMemberParams = { urlParams: { address }, queryParams: { pageSize: 3, excludeDaoId: daoId } };
 
     return (
@@ -143,12 +137,6 @@ export const DaoMemberDetailsPageClient: React.FC<IDaoMemberDetailsPageClientPro
             </Page.Header>
             <Page.Content>
                 <Page.Main>
-                    <Page.Section title={t('app.governance.daoMemberDetailsPage.main.votingActivity.title')}>
-                        <VoteList initialParams={votesByMemberAddressQueryParams} daoId={daoId} />
-                    </Page.Section>
-                    <Page.Section title={t('app.governance.daoMemberDetailsPage.main.proposalCreation.title')}>
-                        <DaoProposalList initialParams={proposalsByMemberParams} />
-                    </Page.Section>
                     <Page.Section title={t('app.governance.daoMemberDetailsPage.main.daoMemberships.title')}>
                         <DaoList daoListByMemberParams={daoListByMemberParams} layoutClassNames="grid grid-cols-1" />
                     </Page.Section>
