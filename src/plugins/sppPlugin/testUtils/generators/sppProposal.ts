83c5d8f9365f539c38772706db015ef33fb69504
shan8851 <asamshan456@gmail.com>
2024-10-04 15:55:30 +0100

    chore: fix build error in generators

--- src/plugins/sppPlugin/testUtils/generators/sppProposal.ts
+++ src/plugins/sppPlugin/testUtils/generators/sppProposal.ts
@@ -1,9 +1,11 @@
 import { generateProposal } from '@/modules/governance/testUtils';
 import type { ISppProposal } from '../../types';
+import { generateSppPluginSettings } from './sppSettings';
 
 export const generateSppProposal = (proposal?: Partial<ISppProposal>): ISppProposal => ({
     ...generateProposal(proposal),
     currentStageIndex: 0,
     subProposals: [],
+    settings: generateSppPluginSettings(),
     ...proposal,
 });
