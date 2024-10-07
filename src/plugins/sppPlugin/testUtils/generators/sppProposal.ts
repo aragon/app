b7b6e50a544508aca55bb8ebf92b2c3d3783989d
shan8851 <asamshan456@gmail.com>
2024-10-07 15:52:19 +0100

    chore: add missing fields to generator

--- src/plugins/sppPlugin/testUtils/generators/sppProposal.ts
+++ src/plugins/sppPlugin/testUtils/generators/sppProposal.ts
@@ -5,6 +5,7 @@ import { generateSppPluginSettings } from './sppSettings';
 export const generateSppProposal = (proposal?: Partial<ISppProposal>): ISppProposal => ({
     ...generateProposal(proposal),
     currentStageIndex: 0,
+    lastStageTransition: 0,
     subProposals: [],
     settings: generateSppPluginSettings(),
     ...proposal,
