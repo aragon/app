4cf56b243577d2691706bec49a772c82363d058c
Shan8851.eth <asamshan456@gmail.com>
2024-10-11 08:51:10 +0100

    feat(APP-3627): Implement parent and stage proposal status utilities for SPP plugin (#170)

---
+++ src/plugins/sppPlugin/testUtils/generators/sppStagePlugin.ts
@@ -0,0 +1,17 @@
+import { generatePlugin } from '@/shared/testUtils';
+import { type ISppStagePlugin, SppProposalType } from '../../types';
+import { generateSppPluginSettings } from './sppSettings';
+
+export const generateSppStagePlugin = (plugin?: Partial<ISppStagePlugin>): ISppStagePlugin => ({
+    ...generatePlugin(),
+    proposalType: SppProposalType.APPROVAL,
+    address: '0xtest',
+    subdomain: 'test',
+    release: '0',
+    build: '0',
+    isProcess: false,
+    isBody: false,
+    isSubPlugin: false,
+    settings: generateSppPluginSettings(),
+    ...plugin,
+});
