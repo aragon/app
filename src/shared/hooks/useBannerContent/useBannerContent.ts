c35fceba85f02048e5e8ba44be687fe9d7a35898
Kevin Davis <whomst@kevind.xyz>
2024-10-15 16:14:41 +0200

    chore: clean up hook

--- src/shared/hooks/useBannerContent/useBannerContent.ts
+++ src/shared/hooks/useBannerContent/useBannerContent.ts
@@ -11,18 +11,21 @@ export function useBannerContent({ id }: IBannerProps) {
     const { address } = useAccount();
     const isConnected = address != null;
 
+    // 'admin' subdomain is not currently set up
     const adminPlugin = useDaoPlugins({
         daoId: id,
-        subdomain: 'multisig',
+        subdomain: 'admin',
     });
     const adminPluginAddress = adminPlugin?.[0]?.meta?.address;
 
+    // this endpoint will change, nullish metrics is just an easy boolean until a proper endpoint is available
     const { data: isMember } = useMember(
         { urlParams: { address: address ?? '' }, queryParams: { daoId: id } },
         { enabled: isConnected && !!adminPluginAddress },
     );
     const isDaoMember = isMember?.metrics != null;
 
+    // Not currently working as expected due to no '.../exists' endpoint
     const { data: isAdminMember } = useMemberOf(
         { urlParams: { address: address ?? '', pluginAddress: adminPluginAddress ?? '' } },
         { enabled: isDaoMember },
