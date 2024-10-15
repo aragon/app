3e3bad378c4adff98a1bd589d4f9f4d712993900
Ruggero Cino <ruggero.cino@gmail.com>
2024-10-15 10:45:14 +0200

    Update pluginTabComponent component and tests

--- src/shared/components/pluginTabComponent/pluginTabComponent.tsx
+++ src/shared/components/pluginTabComponent/pluginTabComponent.tsx
@@ -1,3 +1,4 @@
+import { pluginRegistryUtils } from '@/shared/utils/pluginRegistryUtils';
 import { Tabs } from '@aragon/ods';
 import { useEffect, useState } from 'react';
 import { PluginSingleComponent } from '../pluginSingleComponent';
@@ -7,6 +8,10 @@ export const PluginTabComponent = <TMeta extends object, TProps extends object>(
     props: IPluginTabComponentProps<TMeta, TProps>,
 ) => {
     const { slotId, plugins = [], value, onValueChange, Fallback, children, ...otherProps } = props;
+
+    const supportedPlugins = plugins.filter(
+        (plugin) => pluginRegistryUtils.getSlotComponent({ slotId, pluginId: plugin.id }) != null,
+    );
 
     const defaultActivePlugin = value ?? plugins[0];
     const [activePlugin, setActivePlugin] = useState<ITabComponentPlugin<TMeta, TProps> | undefined>(
@@ -26,11 +31,20 @@ export const PluginTabComponent = <TMeta extends object, TProps extends object>(
         }
     }, [value]);
 
-    if (!plugins.length) {
+    // The components renders null if there is no fallback specified for the slot-id AND the slot has no supported plugins.
+    const hasNoContent = Fallback == null && !supportedPlugins.length;
+
+    // The component renders a single slot component in two cases:
+    // 1 - The fallback is not specified and the slot has only one supported plugin
+    // 2 - The slot has one plugin and the fallback is specified
+    const isSingleComponent =
+        (supportedPlugins.length === 1 && Fallback == null) || (plugins.length === 1 && Fallback != null);
+
+    if (hasNoContent) {
         return null;
     }
 
-    if (plugins.length === 1) {
+    if (isSingleComponent) {
         return (
             <PluginSingleComponent slotId={slotId} pluginId={plugins[0].id} {...plugins[0].props} {...otherProps}>
                 {Fallback != null && <Fallback>{children}</Fallback>}
