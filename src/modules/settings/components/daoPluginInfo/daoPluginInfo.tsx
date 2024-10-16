8bc03dd8d3215fd855f3d10ddbcc042d93952ccf
Kevin Davis <whomst@kevind.xyz>
2024-10-16 09:19:39 +0200

    chore: add basic split for body & process

--- src/modules/settings/components/daoPluginInfo/daoPluginInfo.tsx
+++ src/modules/settings/components/daoPluginInfo/daoPluginInfo.tsx
@@ -1,8 +1,9 @@
 import { type IDaoPlugin } from '@/shared/api/daoService';
+import { IResource } from '@/shared/api/daoService/domain/resource';
 
 import { Page } from '@/shared/components/page';
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { type PluginType } from '@/shared/types';
+import { PluginType } from '@/shared/types';
 import { addressUtils, DateFormat, DefinitionList, formatterUtils, IconType, Link } from '@aragon/ods';
 
 export interface IDaoPlugInfoProps {
@@ -27,44 +28,67 @@ export const DaoPluginInfo: React.FC<IDaoPlugInfoProps> = (props) => {
             inset={false}
             className="gap-y-4"
         >
-            <p>
-                This is an example description for the purposes of establishing layout while we we await description
-                aggregation from the backend service.
-            </p>
-            <div>
-                <Link href="/" target="_blank" iconRight={IconType.LINK_EXTERNAL}>
-                    Delete these
-                </Link>
-                <p className="truncate">https://localhost:3000</p>
-            </div>
-            <div>
-                <Link href="/" target="_blank" iconRight={IconType.LINK_EXTERNAL}>
-                    Example Links
-                </Link>
-                <p className="truncate">https://localhost:3000</p>
-            </div>
-            {/* {plugin.resources((resource: IResource, index: number) => () => (
-                    <div key={index}>
-                        <Link href={resource.url}>{resource.name}</Link>
-                        <p className="truncate">{resource.url}</p>
-                    </div>
-                ))} */}
-            <DefinitionList.Container>
-                <DefinitionList.Item term={t('Body Name')}>{plugin.name}</DefinitionList.Item>
-                <DefinitionList.Item term={t('Plugin')}>
-                    <Link className="capitalize" href="/" iconRight={IconType.LINK_EXTERNAL}>
-                        {plugin.subdomain}
-                    </Link>
-                    <p>{addressUtils.truncateAddress(plugin.address)}</p>
-                </DefinitionList.Item>
-                <DefinitionList.Item term="Launched at">
-                    <Link href="/" target="_blank" iconRight={IconType.LINK_EXTERNAL}>
-                        {formatterUtils.formatDate(plugin.blockTimestamp * 1000, {
-                            format: DateFormat.YEAR_MONTH,
-                        })}
-                    </Link>
-                </DefinitionList.Item>
-            </DefinitionList.Container>
+            {type === PluginType.PROCESS && (
+                <>
+                    <p className="text-neutral-500">{plugin.description}</p>
+                    {plugin.resources?.map((resource: IResource, index: number) => (
+                        <div key={index}>
+                            <Link href={resource.url} target="_blank" iconRight={IconType.LINK_EXTERNAL}>
+                                {resource.name}
+                            </Link>
+                            <p className="truncate">{resource.url}</p>
+                        </div>
+                    ))}
+                    <DefinitionList.Container>
+                        <DefinitionList.Item term={t('Process name')}>{plugin.name}</DefinitionList.Item>
+                        <DefinitionList.Item term={t('Process key')} className="text-neutral-500">
+                            <p className="text-neutral-500"> {plugin.processKey}</p>
+                        </DefinitionList.Item>
+                        <DefinitionList.Item term={t('Plugin')}>
+                            <Link className="capitalize" href="/" iconRight={IconType.LINK_EXTERNAL}>
+                                {plugin.subdomain}
+                            </Link>
+                            <p className="text-neutral-500">{addressUtils.truncateAddress(plugin.address)}</p>
+                        </DefinitionList.Item>
+                        <DefinitionList.Item term="Launched at">
+                            <Link href="/" target="_blank" iconRight={IconType.LINK_EXTERNAL}>
+                                {formatterUtils.formatDate(plugin.blockTimestamp * 1000, {
+                                    format: DateFormat.YEAR_MONTH,
+                                })}
+                            </Link>
+                        </DefinitionList.Item>
+                    </DefinitionList.Container>
+                </>
+            )}
+            {type === PluginType.BODY && (
+                <>
+                    <p className="text-neutral-500">{plugin.description}</p>
+                    {plugin.resources?.map((resource: IResource, index: number) => (
+                        <div key={index}>
+                            <Link href={resource.url} target="_blank" iconRight={IconType.LINK_EXTERNAL}>
+                                {resource.name}
+                            </Link>
+                            <p className="truncate">{resource.url}</p>
+                        </div>
+                    ))}
+                    <DefinitionList.Container>
+                        <DefinitionList.Item term={t('Body Name')}>{plugin.name}</DefinitionList.Item>
+                        <DefinitionList.Item term={t('Plugin')}>
+                            <Link className="capitalize" href="/" iconRight={IconType.LINK_EXTERNAL}>
+                                {plugin.subdomain}
+                            </Link>
+                            <p className="text-neutral-500">{addressUtils.truncateAddress(plugin.address)}</p>
+                        </DefinitionList.Item>
+                        <DefinitionList.Item term="Launched at">
+                            <Link href="/" target="_blank" iconRight={IconType.LINK_EXTERNAL}>
+                                {formatterUtils.formatDate(plugin.blockTimestamp * 1000, {
+                                    format: DateFormat.YEAR_MONTH,
+                                })}
+                            </Link>
+                        </DefinitionList.Item>
+                    </DefinitionList.Container>
+                </>
+            )}
         </Page.Section>
     );
 };
