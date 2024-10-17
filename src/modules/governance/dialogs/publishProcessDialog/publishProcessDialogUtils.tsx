a61a378381552a2d07703d41736fbab2ac660cbc
shan8851 <asamshan456@gmail.com>
2024-10-17 12:05:06 +0300

    chore: build error

--- src/modules/governance/dialogs/publishProcessDialog/publishProcessDialogUtils.tsx
+++ src/modules/governance/dialogs/publishProcessDialog/publishProcessDialogUtils.tsx
@@ -4,11 +4,8 @@ import type {
 } from '@/modules/governance/components/createProcessForm';
 import type { IDaoPlugin } from '@/shared/api/daoService';
 import type { TransactionDialogPrepareReturn } from '@/shared/components/transactionDialog';
-import { dateUtils, type IDateDuration } from '@/shared/utils/dateUtils';
 import { pluginRegistryUtils } from '@/shared/utils/pluginRegistryUtils';
 import { transactionUtils } from '@/shared/utils/transactionUtils';
-import { invariant } from '@aragon/ods';
-import { DateTime, Duration } from 'luxon';
 import { decodeAbiParameters, type Hex, type TransactionReceipt } from 'viem';
 import type { IProposalAction } from '../../api/governanceService';
 import type { ICreateProposalFormData, PrepareProposalActionMap } from '../../components/createProposalForm';
@@ -55,15 +52,13 @@ class PublishProcessDialogUtils {
 
         const actions = this.formToProposalActions(values.actions);
         const metadata = transactionUtils.cidToHex(metadataCid);
-        const startDate = this.parseStartDate(values);
-        const endDate = this.parseEndDate(values);
 
         const buildDataFunction = pluginRegistryUtils.getSlotFunction<IBuildCreateProposalDataParams, Hex>({
             pluginId: plugin.subdomain,
             slotId: GovernanceSlotId.GOVERNANCE_BUILD_CREATE_PROPOSAL_DATA,
         })!;
 
-        const buildDataParams: IBuildCreateProposalDataParams = { actions, metadata, startDate, endDate };
+        const buildDataParams: IBuildCreateProposalDataParams = { actions, metadata, values };
         const transactionData = buildDataFunction(buildDataParams);
 
         const transaction: TransactionDialogPrepareReturn = {
@@ -108,60 +103,8 @@ class PublishProcessDialogUtils {
         return processedActions;
     };
 
-    private parseStartDate = (formValues: ICreateProposalFormData): number => {
-        const { startTimeMode, startTimeFixed } = formValues;
-
-        invariant(
-            startTimeMode === 'now' || startTimeFixed != null,
-            'PublishProposalDialogUtils.parseStartDate: startTimeFixed must be properly set when startTimeMode is set to fixed',
-        );
-
-        // Returning 0 to let the smart contracts set the start time when the transaction is executed.
-        if (startTimeMode === 'now') {
-            return 0;
-        }
-
-        const parsedStartDate = dateUtils.parseFixedDate(startTimeFixed!);
-
-        return this.dateToSeconds(parsedStartDate);
-    };
-
-    private parseEndDate = (formValues: ICreateProposalFormData): number => {
-        const { startTimeMode, startTimeFixed, endTimeMode, endTimeDuration, endTimeFixed, minimumDuration } =
-            formValues;
-        const { hours, minutes, days } = endTimeDuration ?? {};
-
-        invariant(
-            endTimeMode === 'duration' ? endTimeDuration != null : endTimeFixed != null,
-            'PublishProposalDialogUtils.parseEndDate: endTimeDuration/endTimeFixed must be properly set.',
-        );
-
-        // Return 0 when endTime is set as duration and equals to minimumDuration to let smart contract set the correct end
-        // time when the transaction is executed, otherwise the end time will be set as a few seconds before the minimum
-        // duration and the transaction would fail.
-        if (endTimeMode === 'duration' && this.compareTimeDuration(minimumDuration, endTimeDuration)) {
-            return 0;
-        }
-
-        if (endTimeMode === 'duration') {
-            const startDate = startTimeMode === 'now' ? DateTime.now() : dateUtils.parseFixedDate(startTimeFixed!);
-            const endDate = startDate.plus({ hours, minutes, days });
-
-            return this.dateToSeconds(endDate);
-        }
-
-        const parsedEndDate = dateUtils.parseFixedDate(endTimeFixed!);
-
-        return this.dateToSeconds(parsedEndDate);
-    };
-
-    private dateToSeconds = (date: DateTime): number => Math.round(date.toMillis() / 1000);
-
     private formToProposalActions = (actions: IProposalAction[]) =>
         actions.map(({ to, value, data }) => ({ to, value, data }));
-
-    private compareTimeDuration = (first?: IDateDuration, second?: IDateDuration) =>
-        Duration.fromObject(first ?? {}).equals(Duration.fromObject(second ?? {}));
 }
 
 export const publishProcessDialogUtils = new PublishProcessDialogUtils();
