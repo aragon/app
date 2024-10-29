63348b48e04827eda1a18b5070b7316e4f440877
shan8851 <asamshan456@gmail.com>
2024-10-29 14:00:58 +0000

    cohre: simplify strings

--- src/plugins/multisigPlugin/components/multisigSubmitVote/multisigSubmitVote.tsx
+++ src/plugins/multisigPlugin/components/multisigSubmitVote/multisigSubmitVote.tsx
@@ -34,11 +34,11 @@ export const MultisigSubmitVote: React.FC<IMultisigSubmitVoteProps> = (props) =>
     return (
         <div className="pt-4">
             <Button onClick={openTransactionDialog} size="md" variant="primary">
-                {t('app.plugins.multisig.multisigSubmitVote.voteLabel', {
-                    label: isVeto
+                {t(
+                    isVeto
                         ? t('app.plugins.multisig.multisigSubmitVote.vetoLabel')
                         : t('app.plugins.multisig.multisigSubmitVote.approveLabel'),
-                })}
+                )}
             </Button>
         </div>
     );
