029dd73fdd50d19d0930940434bfbc4f08625a65
shan8851 <asamshan456@gmail.com>
2024-11-01 09:34:54 +0000

    chore: lint

--- src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.api.ts
+++ src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.api.ts
@@ -1,7 +1,7 @@
 import type { IProposalAction } from '@/modules/governance/api/governanceService';
+import type { IAutocompleteInputGroup, IAutocompleteInputItem } from '@/shared/components/forms/autocompleteInput';
 import type { IProposalActionComponentProps } from '@aragon/gov-ui-kit';
 import type { IProposalActionData } from '../createProposalFormDefinitions';
-import type { IAutocompleteInputGroup, IAutocompleteInputItem } from '@/shared/components/forms/autocompleteInput';
 
 export interface IPluginActionItem extends IAutocompleteInputItem {
     defaultValue: IProposalAction;
