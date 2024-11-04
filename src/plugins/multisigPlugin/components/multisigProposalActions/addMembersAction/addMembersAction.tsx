5674d26c802770713c7d46c902ee2d813a138773
shan8851 <asamshan456@gmail.com>
2024-11-04 09:18:04 +0000

    save point

--- src/plugins/multisigPlugin/components/multisigProposalActions/addMembersAction/addMembersAction.tsx
+++ src/plugins/multisigPlugin/components/multisigProposalActions/addMembersAction/addMembersAction.tsx
@@ -2,11 +2,11 @@ import type { IProposalAction } from '@/modules/governance/api/governanceService
 import type { IProposalActionData } from '@/modules/governance/components/createProposalForm';
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { useFormField } from '@/shared/hooks/useFormField';
-import { Button, IconType, type IProposalActionComponentProps } from '@aragon/gov-ui-kit';
+import { addressUtils, Button, IconType, type IProposalActionComponentProps } from '@aragon/gov-ui-kit';
 import { useFieldArray, useFormContext, useWatch } from 'react-hook-form';
 import type { IAddOrRemoveMembersActionFormData } from './addMembersActionFormDefinitions';
 import { useEffect } from 'react';
-import { encodeFunctionData } from 'viem';
+import { encodeFunctionData, zeroAddress } from 'viem';
 import { AddMemberItem } from './addMemberItem';
 import { useAccount } from 'wagmi';
 
@@ -41,22 +41,24 @@ export const AddMembersAction: React.FC<IAddMembersActionProps> = (props) => {
     const controlledFields = fields.map((field, index) => ({ ...field, ...watchFieldArray[index] }));
 
     useEffect(() => {
-        const addresses = controlledFields.map((field) => field.address).filter(Boolean);
+        const addresses = fields
+            .map((field) => (addressUtils.isAddress(field.address) ? field.address : zeroAddress))
+            .filter(Boolean);
         const newData = encodeFunctionData({ abi: [addMembersAbi], args: [addresses] });
 
         setValue(`${fieldName}.data`, newData);
         setValue(`${fieldName}.to`, action.pluginAddress);
         setValue(`${fieldName}.from`, address);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [fieldName, fields, setValue, action.pluginAddress, address]);
 
-    console.log(controlledFields);
+    console.log('CONTROLLED_FIELDS =>', controlledFields);
+    console.log('FIELDS =>', fields);
 
     return (
         <>
-            {controlledFields.length > 0 && (
+            {fields.length > 0 && (
                 <div className="flex w-full flex-col gap-3 md:gap-2">
-                    {controlledFields.map((field, index) => (
+                    {fields.map((field, index) => (
                         <AddMemberItem
                             key={field.id}
                             index={index}
