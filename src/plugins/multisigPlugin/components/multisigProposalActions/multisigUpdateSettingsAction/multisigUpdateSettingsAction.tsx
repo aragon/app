42ced108003dcdea4bf06d91e737da90a9a32823
shan8851 <asamshan456@gmail.com>
2024-11-13 12:33:55 +0700

    chore: Add input data for decoded view

--- src/plugins/multisigPlugin/components/multisigProposalActions/multisigUpdateSettingsAction/multisigUpdateSettingsAction.tsx
+++ src/plugins/multisigPlugin/components/multisigProposalActions/multisigUpdateSettingsAction/multisigUpdateSettingsAction.tsx
@@ -76,6 +76,7 @@ export const MultisigUpdateSettingsAction: React.FC<IMultisigUpdateSettingsActio
         const newData = encodeFunctionData({ abi: [updateMultisigSettingsAbi], args: [updateSettingsParams] });
 
         setValue(`${actionFieldName}.data`, newData);
+        setValue(`${actionFieldName}.inputData.parameters[0].value`, `[${onlyListedFieldValue}, ${minimumApproval}]`);
     }, [setValue, actionFieldName, onlyListedFieldValue, minimumApproval]);
 
     return (
