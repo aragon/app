e9c190408f4c4fa48ab267a61c385164a90a2e17
shan8851 <asamshan456@gmail.com>
2024-11-13 11:23:08 +0700

    chore: add only listed field

--- src/plugins/multisigPlugin/components/multisigProposalActions/multisigUpdateSettingsAction/multisigUpdateSettingsAction.tsx
+++ src/plugins/multisigPlugin/components/multisigProposalActions/multisigUpdateSettingsAction/multisigUpdateSettingsAction.tsx
@@ -1,35 +1,99 @@
 import { useMemberList, type IProposalAction } from '@/modules/governance/api/governanceService';
 import type { IProposalActionData } from '@/modules/governance/components/createProposalForm';
-import type { IMultisigPluginSettings } from '@/plugins/multisigPlugin/types';
-import type { IDaoPlugin } from '@/shared/api/daoService';
 import { NumberProgressInput } from '@/shared/components/forms/numberProgressInput';
 import { useTranslations } from '@/shared/components/translationsProvider';
+import { useFormField } from '@/shared/hooks/useFormField';
 import type { IProposalActionComponentProps } from '@aragon/gov-ui-kit';
+import { RadioCard, RadioGroup } from '@aragon/gov-ui-kit';
 import { useWatch } from 'react-hook-form';
 
 export interface IMultisigUpdateSettingsActionProps
-    extends IProposalActionComponentProps<IProposalActionData<IProposalAction, IDaoPlugin<IMultisigPluginSettings>>> {}
+    extends IProposalActionComponentProps<IProposalActionData<IProposalAction>> {}
+
+export interface IMultisigUpdateSettingsFormData {
+    /**
+     * Who can create proposals. Members or any wallet.
+     */
+    onlyListed: string;
+}
+
+const updateMultisigSettingsAbi = {
+    type: 'function',
+    inputs: [
+        {
+            name: '_multisigSettings',
+            internalType: 'struct Multisig.MultisigSettings',
+            type: 'tuple',
+            components: [
+                { name: 'onlyListed', internalType: 'bool', type: 'bool' },
+                { name: 'minApprovals', internalType: 'uint16', type: 'uint16' },
+            ],
+        },
+    ],
+    name: 'updateMultisigSettings',
+    outputs: [],
+    stateMutability: 'nonpayable',
+};
 
 export const MultisigUpdateSettingsAction: React.FC<IMultisigUpdateSettingsActionProps> = (props) => {
-    const { action } = props;
+    const { index, action } = props;
 
     const { t } = useTranslations();
+
+    const actionFieldName = `actions.[${index}]`;
+    useFormField<Record<string, IProposalActionData>, typeof actionFieldName>(actionFieldName);
+
     const value = useWatch<Record<string, string>>({ name: 'test' });
 
     const memberParams = { pluginAddress: action.to, daoId: action.daoId };
     const { data: memberList } = useMemberList({ queryParams: memberParams });
     const membersCount = memberList?.pages[0].metadata.totalRecords;
 
+    const onlyListedField = useFormField<Record<string, IMultisigUpdateSettingsFormData['onlyListed']>, 'onlyListed'>(
+        'onlyListed',
+        {
+            fieldPrefix: `${actionFieldName}.proposedSettings`,
+            label: t('app.plugins.multisig.multisigUpdateSettingsAction.onlyListed.label'),
+        },
+    );
+
     return (
-        <NumberProgressInput
-            fieldName="test"
-            label={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.label')}
-            helpText={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.helpText')}
-            valueLabel={value ?? 0}
-            total={membersCount ?? 1}
-            totalLabel={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.total', {
-                total: membersCount,
-            })}
-        />
+        <>
+            <NumberProgressInput
+                fieldName="test"
+                label={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.label')}
+                helpText={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.helpText')}
+                valueLabel={value ?? 0}
+                total={membersCount ?? 1}
+                totalLabel={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.total', {
+                    total: membersCount,
+                })}
+            />
+            <RadioGroup
+                className="flex w-full flex-col gap-4"
+                helpText={t('app.plugins.multisig.multisigUpdateSettingsAction.onlyListed.helpText')}
+                onValueChange={(value) => {
+                    if (value === 'members') {
+                        onlyListedField.onChange(true);
+                    } else {
+                        onlyListedField.onChange(false);
+                    }
+                }}
+                {...onlyListedField}
+            >
+                <RadioCard
+                    label={t('app.plugins.multisig.multisigUpdateSettingsAction.onlyListed.members.label')}
+                    description={t('app.plugins.multisig.multisigUpdateSettingsAction.onlyListed.members.description')}
+                    value="members"
+                />
+                <RadioCard
+                    label={t('app.plugins.multisig.multisigUpdateSettingsAction.onlyListed.anyWallet.label')}
+                    description={t(
+                        'app.plugins.multisig.multisigUpdateSettingsAction.onlyListed.anyWallet.description',
+                    )}
+                    value="any"
+                />
+            </RadioGroup>
+        </>
     );
 };
