c0b61e6331b82d75a3414660393d25688fd5f320
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-13 10:41:05 +0700

    Update number-progress-input

--- src/plugins/multisigPlugin/components/multisigProposalActions/multisigUpdateSettingsAction/multisigUpdateSettingsAction.tsx
+++ src/plugins/multisigPlugin/components/multisigProposalActions/multisigUpdateSettingsAction/multisigUpdateSettingsAction.tsx
@@ -1,14 +1,35 @@
-import type { IProposalAction } from '@/modules/governance/api/governanceService';
+import { useMemberList, type IProposalAction } from '@/modules/governance/api/governanceService';
 import type { IProposalActionData } from '@/modules/governance/components/createProposalForm';
+import type { IMultisigPluginSettings } from '@/plugins/multisigPlugin/types';
+import type { IDaoPlugin } from '@/shared/api/daoService';
 import { NumberProgressInput } from '@/shared/components/forms/numberProgressInput';
+import { useTranslations } from '@/shared/components/translationsProvider';
 import type { IProposalActionComponentProps } from '@aragon/gov-ui-kit';
 import { useWatch } from 'react-hook-form';
 
 export interface IMultisigUpdateSettingsActionProps
-    extends IProposalActionComponentProps<IProposalActionData<IProposalAction>> {}
+    extends IProposalActionComponentProps<IProposalActionData<IProposalAction, IDaoPlugin<IMultisigPluginSettings>>> {}
 
-export const MultisigUpdateSettingsAction: React.FC<IMultisigUpdateSettingsActionProps> = () => {
+export const MultisigUpdateSettingsAction: React.FC<IMultisigUpdateSettingsActionProps> = (props) => {
+    const { action } = props;
+
+    const { t } = useTranslations();
     const value = useWatch<Record<string, string>>({ name: 'test' });
 
-    return <NumberProgressInput fieldName="test" valueLabel={value} total={100} totalLabel="total label" />;
+    const memberParams = { pluginAddress: action.to, daoId: action.daoId };
+    const { data: memberList } = useMemberList({ queryParams: memberParams });
+    const membersCount = memberList?.pages[0].metadata.totalRecords;
+
+    return (
+        <NumberProgressInput
+            fieldName="test"
+            label={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.label')}
+            helpText={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.helpText')}
+            valueLabel={value ?? 0}
+            total={membersCount ?? 1}
+            totalLabel={t('app.plugins.multisig.multisigUpdateSettingsAction.minimumApproval.total', {
+                total: membersCount,
+            })}
+        />
+    );
 };
--- src/shared/components/forms/numberProgressInput/numberProgressInput.tsx
+++ src/shared/components/forms/numberProgressInput/numberProgressInput.tsx
@@ -1,6 +1,7 @@
 import { useFormField } from '@/shared/hooks/useFormField';
-import { Card, type IInputNumberProps, InputNumber, invariant, Progress } from '@aragon/gov-ui-kit';
+import { Card, type IInputNumberProps, InputContainer, InputNumber, invariant, Progress } from '@aragon/gov-ui-kit';
 import classNames from 'classnames';
+import { useId } from 'react';
 
 export interface INumberProgressInputProps extends Omit<IInputNumberProps, 'value'> {
     /**
@@ -22,28 +23,39 @@ export interface INumberProgressInputProps extends Omit<IInputNumberProps, 'valu
 }
 
 export const NumberProgressInput: React.FC<INumberProgressInputProps> = (props) => {
-    const { fieldName, valueLabel, total, totalLabel, className, ...otherProps } = props;
+    const { label, helpText, fieldName, valueLabel, total, totalLabel, className, ...otherProps } = props;
 
     invariant(total > 0, 'NumberProgressInput: total property must be greater than 0');
 
-    const { value, ...numberField } = useFormField<Record<string, number>, typeof fieldName>(fieldName);
+    const { value, ...numberField } = useFormField<Record<string, number>, typeof fieldName>(fieldName, {
+        rules: { min: 1 },
+    });
+
+    const containerId = useId();
 
     const progressValue = (value * 100) / total;
     const labelLeft = Math.max(progressValue, valueLabel.length * 0.6);
 
     return (
-        <Card className={classNames('flex w-full flex-row justify-between gap-6', className)}>
-            <InputNumber value={value} className="max-w-40" {...numberField} {...otherProps} />
-            <div className="relative flex grow flex-col gap-2 self-end">
-                <p
-                    className="absolute -top-6 transition-all duration-500 ease-in-out"
-                    style={{ left: `${labelLeft}%`, transform: 'translateX(-50%)' }}
-                >
-                    {valueLabel}
-                </p>
-                <Progress value={progressValue} />
-                <p className="self-end text-xs font-normal leading-tight text-neutral-500">{totalLabel}</p>
-            </div>
-        </Card>
+        <InputContainer id={containerId} label={label} helpText={helpText} useCustomWrapper={true}>
+            <Card
+                className={classNames(
+                    'flex w-full flex-row justify-between gap-6 rounded-xl border border-neutral-100 p-4 md:p-6',
+                    className,
+                )}
+            >
+                <InputNumber value={value} className="max-w-40" min={1} {...numberField} {...otherProps} />
+                <div className="relative flex grow flex-col gap-2 self-end">
+                    <p
+                        className="absolute -top-6 text-primary-400 transition-all duration-500 ease-in-out"
+                        style={{ left: `${labelLeft}%`, transform: 'translateX(-50%)' }}
+                    >
+                        {valueLabel}
+                    </p>
+                    <Progress value={progressValue} />
+                    <p className="self-end text-xs font-normal leading-tight text-neutral-500">{totalLabel}</p>
+                </div>
+            </Card>
+        </InputContainer>
     );
 };
