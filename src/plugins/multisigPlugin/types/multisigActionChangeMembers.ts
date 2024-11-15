223a1e99a87a49b23894ceb49e6f5d0483dad40b
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-15 11:19:39 +0700

    fix(APP-3564): Fix action normalization process

--- src/plugins/multisigPlugin/types/multisigActionChangeMembers.ts
+++ src/plugins/multisigPlugin/types/multisigActionChangeMembers.ts
@@ -1,16 +1,9 @@
-import {
-    type ICompositeAddress,
-    type IProposalActionChangeMembers as IGukProposalActionChangeMembers,
-} from '@aragon/gov-ui-kit';
+import { type IProposalActionChangeMembers as IGukProposalActionChangeMembers } from '@aragon/gov-ui-kit';
 import type { MultisigProposalActionType } from './enum';
 
-export interface IMultisigActionChangeMembers extends Omit<IGukProposalActionChangeMembers, 'type' | 'currentMembers'> {
+export interface IMultisigActionChangeMembers extends Omit<IGukProposalActionChangeMembers, 'type'> {
     /**
      * The type of the proposal action.
      */
     type: MultisigProposalActionType.MULTISIG_ADD_MEMBERS | MultisigProposalActionType.MULTISIG_REMOVE_MEMBERS;
-    /**
-     * The current members of the DAO.
-     */
-    currentMembers: ICompositeAddress[];
 }
