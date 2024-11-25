ead52e1eb160d9362f2493bfc7e9763ecc00a782
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-25 11:24:57 +0100

    Remove children-height check

--- src/shared/components/page/pageSection/pageSection.tsx
+++ src/shared/components/page/pageSection/pageSection.tsx
@@ -2,7 +2,7 @@
 
 import { Heading } from '@aragon/gov-ui-kit';
 import classNames from 'classnames';
-import { useEffect, useRef, useState, type ComponentProps } from 'react';
+import { type ComponentProps } from 'react';
 import { usePageContext } from '../pageContext';
 
 export interface IPageSectionProps extends ComponentProps<'div'> {
@@ -25,17 +25,6 @@ export const PageSection: React.FC<IPageSectionProps> = (props) => {
     const { children, className, inset = true, title, description, ...otherProps } = props;
 
     const { contentType } = usePageContext();
-    const childrenRef = useRef<HTMLDivElement>(null);
-
-    const [childrenHeight, setChildrenHeight] = useState<number | undefined>();
-
-    useEffect(() => setChildrenHeight(childrenRef.current?.offsetHeight), []);
-
-    // childrenHeight will be undefined on the server side, therefore check if it is zero to properly prerender
-    // sections with content on the server side
-    if (childrenHeight === 0) {
-        return null;
-    }
 
     return (
         <div className={classNames('flex flex-col', { 'gap-4': inset }, className)} {...otherProps}>
@@ -43,7 +32,7 @@ export const PageSection: React.FC<IPageSectionProps> = (props) => {
                 <Heading size={contentType === 'main' ? 'h2' : 'h3'}>{title}</Heading>
                 {description && <p className="text-base font-normal leading-normal text-neutral-500">{description}</p>}
             </div>
-            <div ref={childrenRef}>{children}</div>
+            {children}
         </div>
     );
 };
