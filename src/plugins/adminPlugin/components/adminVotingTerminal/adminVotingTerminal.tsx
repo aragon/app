952c7582fa773805f97da07f536b00231fef5d15
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-25 13:06:14 +0100

    Use card-empty-state

--- src/plugins/adminPlugin/components/adminVotingTerminal/adminVotingTerminal.tsx
+++ src/plugins/adminPlugin/components/adminVotingTerminal/adminVotingTerminal.tsx
@@ -1,14 +1,14 @@
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { AlertCard } from '@aragon/gov-ui-kit';
+import { CardEmptyState } from '@aragon/gov-ui-kit';
 
 export const AdminVotingTerminal: React.FC = () => {
     const { t } = useTranslations();
 
     return (
-        <AlertCard
-            message={t('app.plugins.admin.adminVotingTerminal.title')}
+        <CardEmptyState
+            heading={t('app.plugins.admin.adminVotingTerminal.title')}
             description={t('app.plugins.admin.adminVotingTerminal.description')}
-            variant="info"
+            objectIllustration={{ object: 'USERS' }}
         />
     );
 };
