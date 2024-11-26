8a81ebf7d9a04d0d48e54190af52f0c88e844819
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-26 17:18:03 +0100

    fix: Update createProposal ABI of SPP plugin, dedupe dependencies

--- src/plugins/sppPlugin/utils/sppTransactionUtils/sppPluginAbi.ts
+++ src/plugins/sppPlugin/utils/sppTransactionUtils/sppPluginAbi.ts
@@ -3,56 +3,19 @@ export const sppPluginAbi = [
         type: 'function',
         name: 'createProposal',
         inputs: [
-            {
-                name: '_metadata',
-                type: 'bytes',
-                internalType: 'bytes',
-            },
+            { name: '_metadata', type: 'bytes' },
             {
                 name: '_actions',
                 type: 'tuple[]',
-                internalType: 'struct Action[]',
                 components: [
-                    {
-                        name: 'to',
-                        type: 'address',
-                        internalType: 'address',
-                    },
-                    {
-                        name: 'value',
-                        type: 'uint256',
-                        internalType: 'uint256',
-                    },
-                    {
-                        name: 'data',
-                        type: 'bytes',
-                        internalType: 'bytes',
-                    },
+                    { name: 'to', type: 'address' },
+                    { name: 'value', type: 'uint256' },
+                    { name: 'data', type: 'bytes' },
                 ],
             },
-            {
-                name: '_allowFailureMap',
-                type: 'uint256',
-                internalType: 'uint256',
-            },
-            {
-                name: '_startDate',
-                type: 'uint64',
-                internalType: 'uint64',
-            },
-            {
-                name: '_proposalParams',
-                type: 'bytes[][]',
-                internalType: 'bytes[][]',
-            },
+            { name: '_allowFailureMap', type: 'uint128' },
+            { name: '_startDate', type: 'uint64' },
+            { name: '_proposalParams', type: 'bytes[][]' },
         ],
-        outputs: [
-            {
-                name: 'proposalId',
-                type: 'uint256',
-                internalType: 'uint256',
-            },
-        ],
-        stateMutability: 'nonpayable',
     },
 ];
