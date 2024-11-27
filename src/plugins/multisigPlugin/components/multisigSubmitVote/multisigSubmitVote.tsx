655e46a1c9113c12c7c020200350c01a1d0a706b
Kevin Davis <whomst@kevind.xyz>
2024-11-27 15:16:38 +0100

    chore: clean up multisig vote

--- src/plugins/multisigPlugin/components/multisigSubmitVote/multisigSubmitVote.tsx
+++ src/plugins/multisigPlugin/components/multisigSubmitVote/multisigSubmitVote.tsx
@@ -8,64 +8,50 @@ import { Button, ChainEntityType, IconType, useBlockExplorer } from '@aragon/gov
 import type { IMultisigProposal, IMultisigVote } from '../../types';
 
 export interface IMultisigSubmitVoteProps {
-    /**
-     * ID of the DAO to create the proposal for.
-     */
     daoId: string;
-    /**
-     * Proposal to submit the vote for.
-     */
     proposal: IMultisigProposal;
-    /**
-     *  Defines if the vote to approve or veto the proposal.
-     */
     isVeto?: boolean;
 }
 
-export const MultisigSubmitVote: React.FC<IMultisigSubmitVoteProps> = (props) => {
-    const { daoId, proposal, isVeto } = props;
-
+export const MultisigSubmitVote: React.FC<IMultisigSubmitVoteProps> = ({ daoId, proposal, isVeto }) => {
     const { t } = useTranslations();
     const { open } = useDialogContext();
+
+    const { voteStatus, didVote } = useVotedStatus({ proposal });
+    const latestVote = voteStatus?.pages[0].data[0] as IMultisigVote;
+    const { transactionHash } = latestVote ?? {};
+
+    const chainId = networkDefinitions[proposal.network].chainId;
+    const { buildEntityUrl } = useBlockExplorer({ chainId });
+    const latestVoteTxHref = buildEntityUrl({
+        type: ChainEntityType.TRANSACTION,
+        id: transactionHash,
+    });
 
     const openTransactionDialog = () => {
         const params: IVoteDialogParams = { daoId, proposal, vote: { label: 'approve' }, isVeto };
         open(GovernanceDialogs.VOTE, { params });
     };
-    const { voteStatus, didVote } = useVotedStatus({ proposal });
-    const latestVote = voteStatus?.pages[0].data[0] as IMultisigVote | undefined;
-    const { transactionHash } = latestVote ?? {};
-
-    const latestVoteTx = transactionHash;
-    const chainId = networkDefinitions[proposal.network].chainId;
-    const { buildEntityUrl } = useBlockExplorer({ chainId });
-    const latestVoteTxHref = buildEntityUrl({
-        type: ChainEntityType.TRANSACTION,
-        id: latestVoteTx ?? '',
-    });
 
     return (
         <div className="w-full pt-4">
-            {didVote ? (
-                <Button
-                    href={latestVoteTxHref}
-                    target="_blank"
-                    size="md"
-                    iconLeft={IconType.CHECKMARK}
-                    variant="secondary"
-                    className="w-full md:w-fit"
-                >
-                    {isVeto
+            <Button
+                onClick={didVote ? undefined : openTransactionDialog}
+                href={didVote ? latestVoteTxHref : undefined}
+                target={didVote ? '_blank' : undefined}
+                size="md"
+                iconLeft={didVote ? IconType.CHECKMARK : undefined}
+                variant={didVote ? 'secondary' : 'primary'}
+                className="w-full md:w-fit"
+            >
+                {didVote
+                    ? isVeto
                         ? t('app.plugins.multisig.multisigSubmitVote.vetoedLabel')
-                        : t('app.plugins.multisig.multisigSubmitVote.approvedLabel')}
-                </Button>
-            ) : (
-                <Button onClick={openTransactionDialog} size="md" variant="primary" className="w-full md:w-fit">
-                    {isVeto
-                        ? t('app.plugins.multisig.multisigSubmitVote.vetoLabel')
-                        : t('app.plugins.multisig.multisigSubmitVote.approveLabel')}
-                </Button>
-            )}
+                        : t('app.plugins.multisig.multisigSubmitVote.approvedLabel')
+                    : isVeto
+                      ? t('app.plugins.multisig.multisigSubmitVote.vetoLabel')
+                      : t('app.plugins.multisig.multisigSubmitVote.approveLabel')}
+            </Button>
         </div>
     );
 };
