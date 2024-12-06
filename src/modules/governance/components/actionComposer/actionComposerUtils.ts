b1bc950b1411f7244923b31af3dc24281fd18153
Ruggero Cino <ruggero.cino@gmail.com>
2024-12-06 15:19:09 +0100

    Make actions abi & function specific

--- src/modules/governance/components/actionComposer/actionComposerUtils.ts
+++ src/modules/governance/components/actionComposer/actionComposerUtils.ts
@@ -58,9 +58,9 @@ class ActionComposerUtils {
         }));
 
     getCustomActionItems = ({ abis, t }: IGetCustomActionParams): IActionComposerItem[] => {
-        const customActionItems = abis.map(({ name, address, functions }) =>
-            functions.map(({ name: functionName, stateMutability, parameters }) => ({
-                id: `${address}-${functionName}`,
+        const customActionItems = abis.map(({ name, address, functions }, abiIndex) =>
+            functions.map(({ name: functionName, stateMutability, parameters }, functionIndex) => ({
+                id: `${address}-${functionName}-${abiIndex.toString()}-${functionIndex.toString()}`,
                 name: functionName,
                 icon: IconType.SLASH,
                 groupId: address,
