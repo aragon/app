063db4185eae6dd1b7e5374dc0620b3db78083b9
cgero.eth <ruggero.cino@gmail.com>
2024-12-20 16:45:55 +0100

    feat(APP-3577): Implement workflow for preview deployments (#290)

--- scripts/setupEnv.sh
+++ scripts/setupEnv.sh
@@ -9,7 +9,7 @@ SOURCE_ENV_FILE=./config/.env.$ENV
 TARGET_ENV_FILE=.env.local
 
 # Copy environment variables into TARGET_ENV_FILE
-if [[ "$ENV" =~ ^(development|staging|production|local)$ ]]
+if [[ "$ENV" =~ ^(development|staging|production|local|preview)$ ]]
 then
     echo "Setup ENV: using $ENV environment"
     cp $SOURCE_ENV_FILE $TARGET_ENV_FILE
