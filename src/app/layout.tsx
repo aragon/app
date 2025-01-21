90ebac082fdcb2b89c8c8ed08912ea37aac52ac5
Ruggero Cino <ruggero.cino@gmail.com>
2025-01-21 13:18:14 +0100

    Remove force-dynamic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { LayoutRoot } from '@/modules/application/components/layouts/layoutRoot';
 import { defaultMetadata } from '@/modules/application/constants/defaultMetadata';
 
-export const dynamic = 'force-dynamic';
 export const metadata = defaultMetadata;
 
 export default LayoutRoot;
