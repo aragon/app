5968fd50446c6c1713a0f8a1505f8ddcbad9a65d
Ruggero Cino <ruggero.cino@gmail.com>
2025-02-04 12:53:23 +0100

    Cleanup sentry configs handling

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,10 +1,4 @@
 import { monitoringUtils } from '@/shared/utils/monitoringUtils';
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
-    enabled: monitoringUtils.isEnabled(),
-
-    // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-    tracesSampleRate: 1.0,
-});
+Sentry.init(monitoringUtils.getBaseConfig());
--- src/shared/utils/monitoringUtils/monitoringUtils.ts
+++ src/shared/utils/monitoringUtils/monitoringUtils.ts
@@ -1,3 +1,4 @@
+import type { ClientOptions } from '@sentry/core';
 import { captureException, captureRequestError, withServerActionInstrumentation } from '@sentry/nextjs';
 
 export interface ILogErrorParams {
@@ -8,6 +9,9 @@ export interface ILogErrorParams {
 }
 
 class MonitoringUtils {
+    private serviceDsn =
+        'https://b8ff2260832ae51c6193ee0491746768@o4506830869233664.ingest.us.sentry.io/4508653516816384';
+
     logError = (error: unknown, params?: ILogErrorParams) => {
         const { context } = params ?? {};
 
@@ -20,6 +24,12 @@ class MonitoringUtils {
 
     // Only enable error tracking for development, staging and production environments
     isEnabled = () => ['development', 'staging', 'production', 'local'].includes(process.env.NEXT_PUBLIC_ENV!);
+
+    getBaseConfig = (): Pick<ClientOptions, 'enabled' | 'dsn' | 'tracesSampleRate'> => ({
+        enabled: this.isEnabled(),
+        dsn: this.serviceDsn,
+        tracesSampleRate: 1.0, // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+    });
 }
 
 export const monitoringUtils = new MonitoringUtils();
