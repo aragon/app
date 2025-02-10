0c453e023bec83ec6bea3ed58be12617c493e113
Ruggero Cino <ruggero.cino@gmail.com>
2025-02-10 14:19:08 +0100

    Update tests for permission transaction utils

--- src/shared/utils/permissionTransactionUtils/permissionTransactionUtils.test.tsx
+++ src/shared/utils/permissionTransactionUtils/permissionTransactionUtils.test.tsx
@@ -2,7 +2,6 @@ import * as Viem from 'viem';
 import { encodeFunctionData, type Hex, zeroHash } from 'viem';
 import { permissionTransactionUtils } from '../permissionTransactionUtils';
 import { permissionManagerAbi } from './abi/permissionManagerAbi';
-import type { IConditionRule } from './permissionTransactionUtils';
 
 jest.mock('viem', () => ({ __esModule: true, ...jest.requireActual<typeof Viem>('viem') }));
 
@@ -18,124 +17,115 @@ describe('permissionTransaction utils', () => {
     });
 
     describe('buildGrantPermissionTransaction', () => {
-        it('returns a transaction object with correct data for grant', () => {
-            const grantParams = {
-                where: '0xWhere' as Hex,
-                who: '0xWho' as Hex,
-                what: 'what',
-                to: '0xTo' as Hex,
-            };
+        it('returns a transaction for granting the specified permission', () => {
+            const grantParams = { where: '0x123' as Hex, who: '0x456' as Hex, what: 'what', to: '0x789' as Hex };
+            const permissionHash = '0x0000';
+            const transactionData = '0x0001';
 
             toBytesSpy.mockReturnValueOnce(Viem.hexToBytes('0x4279746573'));
-            keccak256Spy.mockReturnValueOnce('0xGrantHash');
-            encodeFunctionDataSpy.mockReturnValueOnce('0xGrantTxData');
+            keccak256Spy.mockReturnValueOnce(permissionHash);
+            encodeFunctionDataSpy.mockReturnValueOnce(transactionData);
 
-            const tx = permissionTransactionUtils.buildGrantPermissionTransaction(grantParams);
+            const transaction = permissionTransactionUtils.buildGrantPermissionTransaction(grantParams);
 
-            expect(encodeFunctionData).toHaveBeenCalledWith(
-                expect.objectContaining({
-                    abi: permissionManagerAbi,
-                    functionName: 'grant',
-                    args: [grantParams.where, grantParams.who, '0xGrantHash'],
-                }),
-            );
+            expect(toBytesSpy).toHaveBeenCalledWith(grantParams.what);
+            expect(encodeFunctionData).toHaveBeenCalledWith({
+                abi: permissionManagerAbi,
+                functionName: 'grant',
+                args: [grantParams.where, grantParams.who, permissionHash],
+            });
 
-            const expectedTransaction = { to: grantParams.to, data: '0xGrantTxData', value: '0' };
-
-            expect(tx).toEqual(expectedTransaction);
+            const expectedTransaction = { to: grantParams.to, data: transactionData, value: '0' };
+            expect(transaction).toEqual(expectedTransaction);
         });
     });
 
     describe('buildRevokePermissionTransaction', () => {
-        it('returns a transaction object with correct data for revoke', () => {
-            const revokeParams = {
-                where: '0xWhere' as Hex,
-                who: '0xWho' as Hex,
-                what: 'what',
-                to: '0xTo' as Hex,
-            };
+        it('returns a transaction for revoking the specified permission', () => {
+            const grantParams = { where: '0x123' as Hex, who: '0x456' as Hex, what: 'what', to: '0x789' as Hex };
+            const permissionHash = '0x0000';
+            const transactionData = '0x0001';
 
             toBytesSpy.mockReturnValueOnce(Viem.hexToBytes('0x4279746573'));
-            keccak256Spy.mockReturnValueOnce('0xRevokeHash');
-            encodeFunctionDataSpy.mockReturnValueOnce('0xRevokeTxData');
+            keccak256Spy.mockReturnValueOnce(permissionHash);
+            encodeFunctionDataSpy.mockReturnValueOnce(transactionData);
 
-            const tx = permissionTransactionUtils.buildRevokePermissionTransaction(revokeParams);
+            const transaction = permissionTransactionUtils.buildRevokePermissionTransaction(grantParams);
 
-            expect(encodeFunctionData).toHaveBeenCalledWith(
-                expect.objectContaining({
-                    abi: permissionManagerAbi,
-                    functionName: 'revoke',
-                    args: [revokeParams.where, revokeParams.who, '0xRevokeHash'],
-                }),
-            );
+            expect(toBytesSpy).toHaveBeenCalledWith(grantParams.what);
+            expect(encodeFunctionData).toHaveBeenCalledWith({
+                abi: permissionManagerAbi,
+                functionName: 'revoke',
+                args: [grantParams.where, grantParams.who, permissionHash],
+            });
 
-            expect(tx).toEqual({ to: revokeParams.to, data: '0xRevokeTxData', value: '0' });
+            const expectedTransaction = { to: grantParams.to, data: transactionData, value: '0' };
+            expect(transaction).toEqual(expectedTransaction);
         });
     });
 
     describe('buildRuleConditions', () => {
-        it('returns original conditionRules if conditionAddresses is empty', () => {
+        it('returns original condition rules if condition addresses array is empty', () => {
             const conditionRules = [{ id: 1, op: 1, value: '0x123', permissionId: '0x456' }];
             const result = permissionTransactionUtils.buildRuleConditions([], conditionRules);
 
             expect(result).toEqual(conditionRules);
         });
 
-        it('creates a new condition rule when starting with an empty rules array', () => {
-            const conditionAddress = '0xConditionAddress';
-            const result = permissionTransactionUtils.buildRuleConditions([conditionAddress], []);
+        it('appends the condition address rule to the existing condition rules when having only one condition addres', () => {
+            const conditionAddress = '0x123';
+            const conditionRules = [{ id: 1, op: 1, value: '0x123', permissionId: '0x456' }];
 
+            const result = permissionTransactionUtils.buildRuleConditions([conditionAddress], conditionRules);
+            expect(result[0]).toEqual(conditionRules[0]);
+            expect(result[1]).toEqual({ id: 202, op: 1, permissionId: zeroHash, value: conditionAddress });
+        });
+
+        it('builds correct ruled conditions for one condition address', () => {
+            const conditionAddress = '0x123';
+            const result = permissionTransactionUtils.buildRuleConditions([conditionAddress], []);
+            expect(result).toEqual([{ id: 202, op: 1, permissionId: zeroHash, value: conditionAddress }]);
+        });
+
+        it('builds correct ruled conditions for two condition addresses', () => {
+            const conditionAddresses = ['0x123', '0x456'];
+            const result = permissionTransactionUtils.buildRuleConditions([...conditionAddresses], []);
             expect(result).toEqual([
-                {
-                    id: permissionTransactionUtils['ruleConditionId'].condition,
-                    op: permissionTransactionUtils['ruleConditionOperator'].eq,
-                    value: conditionAddress,
-                    permissionId: zeroHash,
-                },
+                { id: 203, op: 10, permissionId: zeroHash, value: BigInt(8589934593) }, // OR operator on indexes 1 & 2
+                { id: 202, op: 1, permissionId: zeroHash, value: conditionAddresses[0] },
+                { id: 202, op: 1, permissionId: zeroHash, value: conditionAddresses[1] },
             ]);
         });
 
-        it('builds correct rule conditions with multiple conditionAddresses', () => {
-            const addresses = ['0x123', '0x456'];
-            const results = permissionTransactionUtils.buildRuleConditions([...addresses], []);
+        it('builds correct ruled conditions for three condition addresses', () => {
+            const conditionAddresses = ['0x123', '0x456', '0x789'];
+            const result = permissionTransactionUtils.buildRuleConditions([...conditionAddresses], []);
+            expect(result).toEqual([
+                { id: 203, op: 10, permissionId: zeroHash, value: BigInt(8589934593) }, // OR operator on indexes 1 & 2
+                { id: 203, op: 10, permissionId: zeroHash, value: BigInt(17179869187) }, // OR operator on indexes 3 & 4
+                { id: 202, op: 1, permissionId: zeroHash, value: conditionAddresses[0] },
+                { id: 202, op: 1, permissionId: zeroHash, value: conditionAddresses[1] },
+                { id: 202, op: 1, permissionId: zeroHash, value: conditionAddresses[2] },
+            ]);
+        });
+    });
 
-            const ruleConditionId = permissionTransactionUtils['ruleConditionId'];
-            const ruleConditionOperator = permissionTransactionUtils['ruleConditionOperator'];
+    describe('encodeLogicalOperator', () => {
+        it('encodes two indexes into a uint 240 value', () => {
+            expect(permissionTransactionUtils['encodeLogicalOperator'](0, 1)).toEqual(BigInt(4294967296));
+            expect(permissionTransactionUtils['encodeLogicalOperator'](1, 2)).toEqual(BigInt(8589934593));
+            expect(permissionTransactionUtils['encodeLogicalOperator'](10, 11)).toEqual(BigInt(47244640266));
+        });
+    });
 
-            const addressConditions = results.filter(
-                (result: IConditionRule) => result.id === ruleConditionId.condition,
-            );
-
-            expect(addressConditions).toEqual(
-                expect.arrayContaining([
-                    {
-                        id: ruleConditionId.condition,
-                        op: ruleConditionOperator.eq,
-                        value: addresses[0],
-                        permissionId: zeroHash,
-                    },
-                    {
-                        id: ruleConditionId.condition,
-                        op: ruleConditionOperator.eq,
-                        value: addresses[1],
-                        permissionId: zeroHash,
-                    },
-                ]),
-            );
-
-            const logicalCondition = results.find(
-                (result: IConditionRule) => result.id === ruleConditionId.logicOperation,
-            );
-
-            // Index is actually baseIndex + 1 so we use 1 and 2 here
-            const expectedLogicalValue = BigInt(1) + (BigInt(2) << BigInt(32));
-
-            expect(logicalCondition).toEqual({
-                id: ruleConditionId.logicOperation,
-                op: ruleConditionOperator.or,
-                value: expectedLogicalValue,
-                permissionId: zeroHash,
-            });
+    describe('addressToCondition', () => {
+        it('builds a ruled condition from a condition address', () => {
+            const address = '0x111';
+            const result = permissionTransactionUtils['addressToCondition'](address);
+            expect(result.id).toEqual(202);
+            expect(result.op).toEqual(1);
+            expect(result.permissionId).toEqual(zeroHash);
+            expect(result.value).toEqual(address);
         });
     });
 });
