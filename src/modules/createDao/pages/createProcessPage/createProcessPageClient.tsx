03e1d12bbf0518269a3519339cf64e4a19f6e752
Milos Dzepina <milos@aragon.org>
2025-02-25 10:05:33 +0100

    Implement wallet connection checks in create-process page
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/createDao/pages/createProcessPage/createProcessPageClient.tsx
+++ src/modules/createDao/pages/createProcessPage/createProcessPageClient.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { useConnectedWalletGuard } from '@/modules/application/hooks/useConnectedWalletGuard';
+import { useAdminStatus } from '@/plugins/adminPlugin/hooks/useAdminStatus';
 import { useDialogContext } from '@/shared/components/dialogProvider';
 import { Page } from '@/shared/components/page';
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { WizardPage } from '@/shared/components/wizards/wizardPage';
-import { useMemo } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCallback, useEffect, useMemo } from 'react';
 import { defaultStage, type ICreateProcessFormData } from '../../components/createProcessForm';
 import { CreateDaoDialog } from '../../constants/moduleDialogs';
 import type { IPrepareProcessDialogParams } from '../../dialogs/prepareProcessDialog';
@@ -22,8 +25,32 @@ export const CreateProcessPageClient: React.FC<ICreateProcessPageClientProps> = 
     const { daoId } = props;
 
     const { t } = useTranslations();
-
     const { open } = useDialogContext();
+    const router = useRouter();
+    const { isAdminMember } = useAdminStatus({ daoId });
+
+    // use useConnectedWalletGuard to check for connection
+    // use isAdminMember to check if admin member - what to do if not? Redirect to home page?
+    // use the same approach as for proposals
+
+    const handlePermissionCheckError = useCallback(() => router.push(`/dao/${daoId}/dashboard`), [router, daoId]);
+
+    const handleWalletConnectedAdminChecks = useCallback(() => {
+        if (!isAdminMember) {
+            handlePermissionCheckError();
+        }
+    }, [handlePermissionCheckError, isAdminMember]);
+
+    const { check: checkWalletConnection, result: isConnected } = useConnectedWalletGuard({
+        onSuccess: handleWalletConnectedAdminChecks,
+        onError: handlePermissionCheckError,
+    });
+
+    useEffect(() => {
+        if (!isConnected) {
+            checkWalletConnection(); // todo: better name is promptWalletConnection!
+        }
+    }, [isConnected, checkWalletConnection]);
 
     const handleFormSubmit = (values: ICreateProcessFormData) => {
         const dialogParams: IPrepareProcessDialogParams = { daoId, values };
