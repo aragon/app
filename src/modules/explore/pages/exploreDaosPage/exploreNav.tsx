b4d71639796c80e4f357c738764e790ae2b8189a
Milos Dzepina <milos@aragon.org>
2025-03-03 22:14:54 +0100

    Align nav and hero with body
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/explore/pages/exploreDaosPage/exploreNav.tsx
+++ src/modules/explore/pages/exploreDaosPage/exploreNav.tsx
@@ -6,11 +6,13 @@ import { Image } from '@/shared/components/image';
 import { IconType, Wallet } from '@aragon/gov-ui-kit';
 import { Button } from '@aragon/gov-ui-kit-original';
 import classNames from 'classnames';
+import Link from 'next/link';
 import { mainnet } from 'viem/chains';
 import { useAccount } from 'wagmi';
 import AragonAppLogo from './logotype.svg';
 
 const gradient = 'bg-gradient-to-b from-primary-400 to-transparent';
+
 export const ExploreNav: React.FC = () => {
     const { address, isConnected } = useAccount();
     const walletUser = address != null ? { address } : undefined;
@@ -26,32 +28,34 @@ export const ExploreNav: React.FC = () => {
     };
 
     return (
-        <div className="sticky top-0 z-[var(--app-navbar-z-index)] w-full">
-            <nav className={classNames('flex flex-col items-center gap-0 p-0', gradient)}>
-                {/*Container*/}
-                <div className="flex max-w-screen-xl items-center gap-6 self-stretch px-4 py-3 xl:gap-12 xl:px-6 xl:py-5">
-                    {/*Wrapper*/}
-                    <div className="flex flex-1 items-center justify-between">
-                        {/*Logo*/}
-                        <div className="h-10">
-                            <Image alt="Aragon App logo" className="h-10" fill={false} src={AragonAppLogo as string} />
-                        </div>
-                        {/*Navigation Group*/}
-                        <div className="flex items-center justify-end gap-4 xl:gap-6">
-                            <Button
-                                variant="secondary"
-                                iconRight={IconType.LINK_EXTERNAL}
-                                onClick={handleLegacyAppClick}
-                                size="sm"
-                                responsiveSize={{ sm: 'md' }}
-                            >
-                                Legacy app
-                            </Button>
-                            <Wallet onClick={handleWalletClick} user={walletUser} chainId={mainnet.id} />
-                        </div>
-                    </div>
+        <div className={classNames('sticky top-0 z-[var(--app-navbar-z-index)] w-full', gradient)}>
+            <header className="mx-auto flex max-w-screen-xl justify-between gap-6 self-stretch px-4 py-3 xl:gap-12 xl:px-6 xl:py-5">
+                {/*Left - Logo*/}
+                <div className="h-10">
+                    <Link href="/">
+                        <Image
+                            alt="Aragon App logo"
+                            className="h-10 min-w-40"
+                            fill={false}
+                            src={AragonAppLogo as string}
+                        />
+                    </Link>
                 </div>
-            </nav>
+
+                {/*Right - Navigation Group*/}
+                <nav className="flex items-center justify-end gap-4 xl:gap-6">
+                    <Button
+                        variant="secondary"
+                        iconRight={IconType.LINK_EXTERNAL}
+                        onClick={handleLegacyAppClick}
+                        size="sm"
+                        responsiveSize={{ sm: 'md' }}
+                    >
+                        Legacy app
+                    </Button>
+                    <Wallet onClick={handleWalletClick} user={walletUser} chainId={mainnet.id} />
+                </nav>
+            </header>
         </div>
     );
 };
--- src/modules/explore/pages/exploreDaosPage/hero.tsx
+++ src/modules/explore/pages/exploreDaosPage/hero.tsx
@@ -3,8 +3,6 @@
 import { useTranslations } from '@/shared/components/translationsProvider';
 import Blueprint from './net_bg.svg';
 
-console.log('AragonAppLogo as string', Blueprint);
-
 const Hero = () => {
     const { t } = useTranslations();
 
@@ -40,26 +38,29 @@ function Hero2() {
 
     return (
         <div className="relative -mt-[72px] flex flex-col items-center self-stretch bg-primary-400 pt-[72px] xl:-mt-24 xl:pt-24">
-            {/* Background Image */}
-            <img
-                src={Blueprint.src}
-                alt="Background Texture"
-                // className="absolute right-0 top-[-412px] h-[1178.485px] w-[1440px]"
-                className="absolute left-0 top-0 size-full object-cover"
-            />
-            {/* Wrapper*/}
-            <div className="relative flex max-w-screen-xl flex-col items-start justify-center gap-2 self-stretch px-4 py-10 xl:px-6 xl:py-16">
-                {/* Content Group*/}
-                <div className="flex max-w-[720px] flex-col items-start gap-2 self-stretch xl:gap-4">
-                    <h1 className="self-stretch text-2xl font-normal leading-[115%] tracking-[-0.576px] text-primary-50 xl:text-5xl xl:tracking-[-1.152px]">
-                        {/*{t('explore.hero.title')}*/}
-                        Governed on Aragon
-                    </h1>
+            <div className="mx-auto w-full max-w-screen-xl">
+                {/* Background Image */}
+                <img
+                    src={Blueprint.src}
+                    alt="Background Texture"
+                    // className="absolute right-0 top-[-412px] h-[1178.485px] w-[1440px]"
+                    className="absolute left-0 top-0 size-full object-cover"
+                />
+                {/* Wrapper*/}
+                <div className="relative flex max-w-screen-xl flex-col items-start justify-center gap-2 self-stretch px-4 py-10 xl:px-6 xl:py-16">
+                    {/* Content Group*/}
+                    <div className="flex max-w-[720px] flex-col items-start gap-2 self-stretch xl:gap-4">
+                        <h1 className="self-stretch text-2xl font-normal leading-[115%] tracking-[-0.576px] text-primary-50 xl:text-5xl xl:tracking-[-1.152px]">
+                            {/*{t('explore.hero.title')}*/}
+                            Governed on Aragon
+                        </h1>
 
-                    <h3 className="text-base font-normal leading-[150%] text-primary-50 xl:text-xl">
-                        {/*{t('explore.hero.subtitle')}*/}
-                        Explore the organizations using our modular governance stack to secure their onchain governance.
-                    </h3>
+                        <h3 className="text-base font-normal leading-[150%] text-primary-50 xl:text-xl">
+                            {/*{t('explore.hero.subtitle')}*/}
+                            Explore the organizations using our modular governance stack to secure their onchain
+                            governance.
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
