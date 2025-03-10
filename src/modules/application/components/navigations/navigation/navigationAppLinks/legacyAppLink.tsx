84149672699fa63279efab0203dde5250940d742
shan8851 <asamshan456@gmail.com>
2025-03-10 10:35:34 +0000

    chore: refactr legacy app link button

--- src/modules/application/components/navigations/navigation/navigationAppLinks/legacyAppLink.tsx
+++ src/modules/application/components/navigations/navigation/navigationAppLinks/legacyAppLink.tsx
@@ -14,6 +14,25 @@ const legacyNetworkMap: Partial<Record<Network, string>> = {
 
 const baseUrl = 'https://app.aragon.org';
 
+const getLegacyUrl = (dao?: IDao): string | null => {
+    // If no DAO is provided, use the explore page
+    if (!dao) {
+        return baseUrl;
+    }
+
+    const { version, network, address } = dao;
+    const legacyNetwork = legacyNetworkMap[network];
+    const [release, build] = version.split('.').map(Number);
+
+    // Check if the DAO is supported in the legacy app
+    const isLegacyDao = legacyNetwork && (release < 1 || (release === 1 && build < 4));
+
+    if (!isLegacyDao) {
+        return null;
+    }
+
+    return `${baseUrl}/#/daos/${legacyNetwork}/${address}`;
+};
 export interface ILegacyAppLinkProps {
     /**
      * The DAO to enable us to navigate to the correct dao dashboard in the legacy app.
@@ -26,30 +45,14 @@ export const LegacyAppLink: React.FC<ILegacyAppLinkProps> = (props) => {
 
     const { t } = useTranslations();
 
-    // If no DAO is provided use the explore page
-    if (!dao) {
-        return (
-            <Button target="_blank" href={baseUrl} iconRight={IconType.LINK_EXTERNAL} variant="tertiary" size="sm">
-                {t('app.application.navigationAppLinks.legacy')}
-            </Button>
-        );
-    }
+    const legacyUrl = getLegacyUrl(dao);
 
-    const { version, network, address } = dao;
-
-    const legacyNetwork = legacyNetworkMap[network];
-    const [release, build] = version.split('.').map(Number);
-
-    const showDaoLegacyButton = legacyNetwork && (release < 1 || (release === 1 && build < 4));
-
-    if (!showDaoLegacyButton) {
+    if (!legacyUrl) {
         return null;
     }
 
-    const legacyDaoUrl = `https://app.aragon.org/#/daos/${legacyNetwork}/${address}`;
-
     return (
-        <Button target="_blank" href={legacyDaoUrl} iconRight={IconType.LINK_EXTERNAL} variant="tertiary" size="sm">
+        <Button target="_blank" href={legacyUrl} iconRight={IconType.LINK_EXTERNAL} variant="tertiary" size="sm">
             {t('app.application.navigationAppLinks.legacy')}
         </Button>
     );
