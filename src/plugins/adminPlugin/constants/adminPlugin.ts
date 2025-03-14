8e26a6ff0bf49192c3151f3af0807b5d4a6a0a56
Evan Aronson <93671071+evanaronson@users.noreply.github.com>
2025-03-14 11:26:41 +0100

    Update adminPlugin.ts
    update admin plugin

--- src/plugins/adminPlugin/constants/adminPlugin.ts
+++ src/plugins/adminPlugin/constants/adminPlugin.ts
@@ -5,15 +5,15 @@ import { zeroAddress } from 'viem';
 export const adminPlugin: IPluginInfo = {
     id: 'admin',
     name: 'Admin',
-    installVersion: { release: 1, build: 6 },
+    installVersion: { release: 1, build: 2 },
     repositoryAddresses: {
         [Network.ARBITRUM_MAINNET]: zeroAddress,
         [Network.BASE_MAINNET]: zeroAddress,
         [Network.ETHEREUM_MAINNET]: zeroAddress,
-        [Network.ETHEREUM_SEPOLIA]: '0xEdA3074437375DC71007AFC9D421644656d72287',
+        [Network.ETHEREUM_SEPOLIA]: '0x152c9E28995E418870b85cbbc0AEE4e53020edb2',
         [Network.POLYGON_MAINNET]: zeroAddress,
         [Network.ZKSYNC_MAINNET]: zeroAddress,
         [Network.ZKSYNC_SEPOLIA]: zeroAddress,
-        [Network.PEAQ_MAINNET]: zeroAddress,
+        [Network.PEAQ_MAINNET]: '0x86C87Aa7C09a447048adf4197fec7C12eF62A07F',
     },
 };
