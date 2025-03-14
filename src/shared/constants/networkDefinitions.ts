ee12e456f7f38169356f778920450e548a5ae776
Evan Aronson <93671071+evanaronson@users.noreply.github.com>
2025-03-14 11:46:00 +0100

    Update networkDefinitions.ts

--- src/shared/constants/networkDefinitions.ts
+++ src/shared/constants/networkDefinitions.ts
@@ -111,9 +111,9 @@ export const networkDefinitions: Record<Network, INetworkDefinition> = {
         logo: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png',
         privateRpc: 'https://eth-sepolia.g.alchemy.com/v2/',
         addresses: {
-            daoFactory: '0x20A8bDAbF02fcAca65CB799C0ed9CE4Ff25F3a90',
-            pluginSetupProcessor: '0x9e99D11b513dD2cc5e117a5793412106502FF04B',
-            globalExecutor: '0x67744773b8C29aaDc8a11010C09306c0029219Ff',
+            daoFactory: '0xB815791c233807D39b7430127975244B36C19C8e',
+            pluginSetupProcessor: '0xC24188a73dc09aA7C721f96Ad8857B469C01dC9f',
+            globalExecutor: '0x619c4C957370CD288D64b46D4cF3a560229b593b',
         },
     },
     [Network.ZKSYNC_SEPOLIA]: {
