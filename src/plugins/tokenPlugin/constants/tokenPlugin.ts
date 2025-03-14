55c4fc636e667a598448300a5a07d4ea5b5e9ceb
Evan Aronson <93671071+evanaronson@users.noreply.github.com>
2025-03-14 11:35:08 +0100

    Update tokenPlugin.ts
    upgrade tokenvoting

--- src/plugins/tokenPlugin/constants/tokenPlugin.ts
+++ src/plugins/tokenPlugin/constants/tokenPlugin.ts
@@ -5,15 +5,15 @@ import { zeroAddress } from 'viem';
 export const tokenPlugin: IPluginInfo = {
     id: 'token-voting',
     name: 'Token',
-    installVersion: { release: 1, build: 5 },
+    installVersion: { release: 1, build: 3 },
     repositoryAddresses: {
         [Network.ARBITRUM_MAINNET]: zeroAddress,
         [Network.BASE_MAINNET]: zeroAddress,
         [Network.ETHEREUM_MAINNET]: zeroAddress,
-        [Network.ETHEREUM_SEPOLIA]: '0x6241ad0D3f162028d2e0000f1A878DBc4F5c4aD0',
+        [Network.ETHEREUM_SEPOLIA]: '0x424F4cA6FA9c24C03f2396DF0E96057eD11CF7dF',
         [Network.POLYGON_MAINNET]: zeroAddress,
         [Network.ZKSYNC_MAINNET]: zeroAddress,
         [Network.ZKSYNC_SEPOLIA]: zeroAddress,
-        [Network.PEAQ_MAINNET]: zeroAddress,
+        [Network.PEAQ_MAINNET]: '0xFBFbE98845B4E2751a8A004B5A1759e3A278FC68',
     },
 };
