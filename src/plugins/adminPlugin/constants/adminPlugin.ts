3e32c3d69b2d7e01f031cbfa3a0d116ed68eaeea
heykd.eth <65736142+thekidnamedkd@users.noreply.github.com>
2025-03-19 05:09:45 -0700

    feat(APP-3961): Add contract addresses for remaining chains (#430)

--- src/plugins/adminPlugin/constants/adminPlugin.ts
+++ src/plugins/adminPlugin/constants/adminPlugin.ts
@@ -1,6 +1,5 @@
 import { Network } from '@/shared/api/daoService';
 import type { IPluginInfo } from '@/shared/types';
-import { zeroAddress } from 'viem';
 
 export const adminPlugin: IPluginInfo = {
     id: 'admin',
@@ -8,11 +7,11 @@ export const adminPlugin: IPluginInfo = {
     installVersion: { release: 1, build: 2 },
     repositoryAddresses: {
         [Network.ARBITRUM_MAINNET]: '0x326A2aee6A8eE78D79E7E956DE60C6E452f76a8e',
-        [Network.BASE_MAINNET]: zeroAddress,
-        [Network.ETHEREUM_MAINNET]: zeroAddress,
+        [Network.BASE_MAINNET]: '0x212eF339C77B3390599caB4D46222D79fAabcb5c',
+        [Network.ETHEREUM_MAINNET]: '0xA4371a239D08bfBA6E8894eccf8466C6323A52C3',
         [Network.ETHEREUM_SEPOLIA]: '0x152c9E28995E418870b85cbbc0AEE4e53020edb2',
-        [Network.POLYGON_MAINNET]: zeroAddress,
-        [Network.ZKSYNC_MAINNET]: zeroAddress,
+        [Network.POLYGON_MAINNET]: '0x7fF570473d0876db16A59e8F04EE7F17Ab117309',
+        [Network.ZKSYNC_MAINNET]: '0x2BAb29B3483F8972f6112c68a4076e79c3035E02',
         [Network.ZKSYNC_SEPOLIA]: '0x2e23fD7985979AE78561adD5D76d2fa10f20fbB1',
         [Network.PEAQ_MAINNET]: '0x86C87Aa7C09a447048adf4197fec7C12eF62A07F',
     },
--- src/plugins/tokenPlugin/constants/tokenPlugin.ts
+++ src/plugins/tokenPlugin/constants/tokenPlugin.ts
@@ -1,6 +1,5 @@
 import { Network } from '@/shared/api/daoService';
 import type { IPluginInfo } from '@/shared/types';
-import { zeroAddress } from 'viem';
 
 export const tokenPlugin: IPluginInfo = {
     id: 'token-voting',
@@ -8,11 +7,11 @@ export const tokenPlugin: IPluginInfo = {
     installVersion: { release: 1, build: 3 },
     repositoryAddresses: {
         [Network.ARBITRUM_MAINNET]: '0x1AeD2BEb470aeFD65B43f905Bd5371b1E4749d18',
-        [Network.BASE_MAINNET]: zeroAddress,
-        [Network.ETHEREUM_MAINNET]: zeroAddress,
+        [Network.BASE_MAINNET]: '0x2532570DcFb749A7F976136CC05648ef2a0f60b0',
+        [Network.ETHEREUM_MAINNET]: '0xb7401cD221ceAFC54093168B814Cc3d42579287f',
         [Network.ETHEREUM_SEPOLIA]: '0x424F4cA6FA9c24C03f2396DF0E96057eD11CF7dF',
-        [Network.POLYGON_MAINNET]: zeroAddress,
-        [Network.ZKSYNC_MAINNET]: zeroAddress,
+        [Network.POLYGON_MAINNET]: '0xae67aea0B830ed4504B36670B5Fa70c5C386Bb58',
+        [Network.ZKSYNC_MAINNET]: '0xE8F4C59f83CeE31A867E61c9959533A6e95ebCB3',
         [Network.ZKSYNC_SEPOLIA]: '0x1f5f8f677164AA4D9b4465A99D22e1e01dC24160',
         [Network.PEAQ_MAINNET]: '0xFBFbE98845B4E2751a8A004B5A1759e3A278FC68',
     },
