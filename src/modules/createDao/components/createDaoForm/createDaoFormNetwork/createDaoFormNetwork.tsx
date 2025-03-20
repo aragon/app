3ff70376907c56ba0ed86c4d3c3906fc5bba23ee
Milos Dzepina <milos@aragon.org>
2025-03-20 09:58:22 +0000

    Add tag instead of suffix
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/createDao/components/createDaoForm/createDaoFormNetwork/createDaoFormNetwork.tsx
+++ src/modules/createDao/components/createDaoForm/createDaoFormNetwork/createDaoFormNetwork.tsx
@@ -1,6 +1,6 @@
 import { Network } from '@/shared/api/daoService';
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { networkDefinitions } from '@/shared/constants/networkDefinitions';
+import { INetworkDefinition, networkDefinitions } from '@/shared/constants/networkDefinitions';
 import { useFormField } from '@/shared/hooks/useFormField';
 import { RadioCard, RadioGroup } from '@aragon/gov-ui-kit';
 import type { ICreateDaoFormData } from '../createDaoFormDefinitions';
@@ -16,19 +16,37 @@ export const CreateDaoFormNetwork: React.FC<ICreateDaoFormNetworkProps> = () => 
     });
 
     const testnetTag = { variant: 'info' as const, label: t('app.createDao.createDaoForm.network.testnetLabel') };
+    const disabledTag = { variant: 'info' as const, label: t('app.createDao.createDaoForm.network.disabledLabel') };
+
+    const enabledNetworkEntries = Object.entries(networkDefinitions).filter(([, network]) => !network.disabled);
+    const disabledNetworkEntries = Object.entries(networkDefinitions).filter(([, network]) => network.disabled);
+
+    const getNetworkTag = (network: INetworkDefinition) => {
+        if (network.disabled) {
+            return disabledTag;
+        }
+
+        if (network.testnet) {
+            return testnetTag;
+        }
+
+        return undefined;
+    };
 
     return (
         <RadioGroup onValueChange={onNetworkChange} {...networkField}>
-            {Object.entries(networkDefinitions).map(([key, network]) => (
-                <RadioCard
-                    tag={network.testnet ? testnetTag : undefined}
-                    key={key}
-                    value={key}
-                    label={network.disabled ? `${network.name} (Coming soon)` : network.name}
-                    disabled={network.disabled}
-                    avatar={network.logo}
-                />
-            ))}
+            {[...enabledNetworkEntries, ...disabledNetworkEntries].map(([key, network]) => {
+                return (
+                    <RadioCard
+                        tag={getNetworkTag(network)}
+                        key={key}
+                        value={key}
+                        label={network.name}
+                        disabled={network.disabled}
+                        avatar={network.logo}
+                    />
+                );
+            })}
         </RadioGroup>
     );
 };
