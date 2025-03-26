f86c08541b2c5137f52c859172e830cc640e4b0f
Milos Dzepina <milos@aragon.org>
2025-03-26 13:23:11 +0100

    Add MultisigSetupGovernance to create-process
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/createDao/components/createProcessForm/createProcessFormPluginFlows/createProcessFormMultisigFlow/createProcessFormMultsigParams/createProcessFormMultisigParams.tsx
+++ src/modules/createDao/components/createProcessForm/createProcessFormPluginFlows/createProcessFormMultisigFlow/createProcessFormMultsigParams/createProcessFormMultisigParams.tsx
@@ -1,8 +1,5 @@
-import { useTranslations } from '@/shared/components/translationsProvider';
-import { useFormField } from '@/shared/hooks/useFormField';
-import { InputNumber } from '@aragon/gov-ui-kit';
+import { MultisigSetupGovernance } from '@/plugins/multisigPlugin/components/multisigSetupGovernance';
 import { useFormContext } from 'react-hook-form';
-import type { ICreateProcessFormBody } from '../../../createProcessFormDefinitions';
 import type { ICreateProcessFormBodyDialogStepsProps } from '../../../createProcessFormStages/fields/stageBodiesField/stageBodiesFieldDefinitions';
 
 export interface ICreateProcessFormMultisigParamsProps extends ICreateProcessFormBodyDialogStepsProps {}
@@ -10,34 +7,10 @@ export interface ICreateProcessFormMultisigParamsProps extends ICreateProcessFor
 export const CreateProcessFormMultisigParams: React.FC<ICreateProcessFormMultisigParamsProps> = (props) => {
     const { fieldPrefix } = props;
 
-    const { t } = useTranslations();
-
     const { watch } = useFormContext();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const members = watch(`${fieldPrefix}.members`);
 
-    const multisigThresholdField = useFormField<ICreateProcessFormBody, 'minApprovals'>('minApprovals', {
-        label: t('app.createDao.createProcessForm.multisigFlow.params.label'),
-        defaultValue: 1,
-        fieldPrefix,
-        rules: {
-            required: t('app.createDao.createProcessForm.multisigFlow.params.required'),
-            min: { value: 1, message: t('app.createDao.createProcessForm.multisigFlow.params.required') },
-        },
-    });
-
-    return (
-        <InputNumber
-            helpText={t('app.createDao.createProcessForm.multisigFlow.params.helpText')}
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            max={members?.length}
-            min={1}
-            suffix={t('app.createDao.createProcessForm.multisigFlow.params.suffix', {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                members: members?.length as number,
-            })}
-            placeholder={t('app.createDao.createProcessForm.multisigFlow.params.placeholder')}
-            {...multisigThresholdField}
-        />
-    );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    return <MultisigSetupGovernance fieldPrefix={fieldPrefix} membersCount={members?.length ?? 0} />;
 };
