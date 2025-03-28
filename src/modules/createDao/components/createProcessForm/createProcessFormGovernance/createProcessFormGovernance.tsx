cc5c3abd261bf8743972703f704abc9c4876bb56
shan8851 <asamshan456@gmail.com>
2025-03-28 21:44:56 +0000

    chore: save point - add basic UI

--- src/modules/createDao/components/createProcessForm/createProcessFormGovernance/createProcessFormGovernance.tsx
+++ src/modules/createDao/components/createProcessForm/createProcessFormGovernance/createProcessFormGovernance.tsx
@@ -1,9 +1,11 @@
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { Button, IconType } from '@aragon/gov-ui-kit';
+import { Button, IconType, RadioCard, RadioGroup } from '@aragon/gov-ui-kit';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 import { type ICreateProcessFormData } from '../createProcessFormDefinitions';
 import { createProcessFormUtils } from '../createProcessFormUtils';
 import { CreateProcessFormGovernanceItem } from './createProcessFormGovernanceItem';
+import { useState } from 'react';
+import { GovernanceBodiesField } from './fields/governanceBodiesField';
 
 export interface ICreateProcessFormGovernanceProps {}
 
@@ -11,6 +13,8 @@ export const CreateProcessFormGovernance: React.FC<ICreateProcessFormGovernanceP
     const { t } = useTranslations();
 
     const { setValue, getValues } = useFormContext<ICreateProcessFormData>();
+
+    const [processType, setProcessType] = useState<'basic' | 'advanced'>('basic');
 
     const {
         fields: stages,
@@ -27,28 +31,60 @@ export const CreateProcessFormGovernance: React.FC<ICreateProcessFormGovernanceP
         setValue('bodies', updatedBodies);
     };
 
+    const handleProcessTypeChanged = (value: string) => {
+            setProcessType(value as typeof processType);
+            if (value === 'basic') {
+                setValue('stages', []);
+            } else {
+                handleAddStage();
+            }
+        };
+
     return (
-        <div className="flex flex-col gap-2 md:gap-3">
-            <div className="flex flex-col gap-3 md:gap-2">
-                {stages.map((stage, index) => (
-                    <CreateProcessFormGovernanceItem
-                        key={stage.id}
-                        formPrefix={`stages.${index.toString()}`}
-                        stage={stage}
-                        stagesCount={stages.length}
-                        onDelete={() => handleRemoveStage(index)}
-                    />
-                ))}
-            </div>
-            <Button
-                size="md"
-                variant="tertiary"
-                className="self-start"
-                iconLeft={IconType.PLUS}
-                onClick={handleAddStage}
+        <div className="flex w-full flex-col gap-10">
+            <RadioGroup
+                label={t('app.createDao.createProcessForm.processType.label')}
+                helpText={t('app.createDao.createProcessForm.processType.helpText')}
+                value={processType}
+                onValueChange={handleProcessTypeChanged}
+                className="w-full !flex-row gap-4"
             >
-                {t('app.createDao.createProcessForm.stages.action.add')}
-            </Button>
+                <RadioCard
+                    label={t('app.createDao.createProcessForm.processType.basic.label')}
+                    description={t('app.createDao.createProcessForm.processType.basic.description')}
+                    value="basic"
+                />
+                <RadioCard
+                    label={t('app.createDao.createProcessForm.processType.advanced.label')}
+                    description={t('app.createDao.createProcessForm.processType.advanced.description')}
+                    value="advanced"
+                />
+            </RadioGroup>
+            {processType === 'basic' && <GovernanceBodiesField />}
+            {processType === 'advanced' && (
+                <div className="flex flex-col gap-2 md:gap-3">
+                    <div className="flex flex-col gap-3 md:gap-2">
+                        {stages.map((stage, index) => (
+                            <CreateProcessFormGovernanceItem
+                                key={stage.id}
+                                formPrefix={`stages.${index.toString()}`}
+                                stage={stage}
+                                stagesCount={stages.length}
+                                onDelete={() => handleRemoveStage(index)}
+                            />
+                        ))}
+                    </div>
+                    <Button
+                        size="md"
+                        variant="tertiary"
+                        className="self-start"
+                        iconLeft={IconType.PLUS}
+                        onClick={handleAddStage}
+                    >
+                        {t('app.createDao.createProcessForm.stages.action.add')}
+                    </Button>
+                </div>
+            )}
         </div>
     );
 };
