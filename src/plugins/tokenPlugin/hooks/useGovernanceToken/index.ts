2708fd30c23e3c923b364ade9b360ea0f223ef1a
Milos Dzepina <milos@aragon.org>
2025-03-28 15:28:02 +0100

    Start useGovernmentToken impl
    Signed-off-by: Milos Dzepina <milos@aragon.org>

---
+++ src/plugins/tokenPlugin/hooks/useGovernanceToken/index.ts
@@ -0,0 +1,1 @@
+export { useGovernanceToken } from './useGovernanceToken';
---
+++ src/plugins/tokenPlugin/hooks/useGovernanceToken/useGovernanceToken.test.ts
@@ -0,0 +1,19 @@
+describe('useGovernanceToken', () => {
+    // const useTokenSpy = jest.spyOn(Wagmi, 'useToken');
+    //
+    // beforeEach(() => {
+    //     useTokenSpy.mockReturnValue({} as unknown as Wagmi.UseAccountReturnType);
+    // });
+    //
+    // afterEach(() => {
+    //     useTokenSpy.mockReset();
+    // });
+    //
+    // it('exists', () => {
+    //     expect(() => useGovernanceToken({address: '0xasdasdsa', chain})).not.toThrow();
+    // });
+
+    it('is in development', () => {
+        expect(true).toBe(true);
+    });
+});
