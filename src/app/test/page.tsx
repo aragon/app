6bae20997df42a1ad23306d7faa0d9e5fc1de17f
Milos Dzepina <milos@aragon.org>
2025-03-31 14:41:57 +0200

    Implement basic components
    Signed-off-by: Milos Dzepina <milos@aragon.org>

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { ImportTokenCheck } from '@/app/test/importToken';
+
+export default function PageTest() {
+    return (
+        <div className="p-6">
+            <h1>Hello</h1>
+            <ImportTokenCheck />
+        </div>
+    );
+}
