54db78658329c1deaf470282d643b78c630f5c55
Milos Dzepina <milos@aragon.org>
2025-04-01 12:10:34 +0200

    feat(APP-4120): Implement useGovernanceToken hook (#457)

---
+++ src/plugins/tokenPlugin/hooks/useGovernanceToken/useGovernanceToken.ts
@@ -0,0 +1,24 @@
+import { useToken } from '../useToken';
+import { useERC20VotesTokenCheck } from './useERC20VotesTokenCheck';
+import type { IUseGovernanceTokenParams, IUseGovernanceTokenResult } from './useGovernanceToken.api';
+
+export const useGovernanceToken = (params: IUseGovernanceTokenParams): IUseGovernanceTokenResult => {
+    const { isLoading: isTokenLoading, isError: isTokenError, token } = useToken(params);
+    const {
+        isLoading: areGovernanceChecksLoading,
+        isGovernanceCompatible,
+        isDelegationCompatible,
+        isError,
+    } = useERC20VotesTokenCheck({
+        ...params,
+        enabled: token != null,
+    });
+
+    return {
+        isLoading: isTokenLoading || areGovernanceChecksLoading,
+        isError: isTokenError || isError,
+        token,
+        isGovernanceCompatible,
+        isDelegationCompatible,
+    };
+};
