0b0f51df902c569fe763dcdac1e3ce29f3857189
cgero.eth <ruggero.cino@gmail.com>
2025-04-02 10:11:59 +0200

    fix(APP-3893): Fix validation of bodies of create process form (#465)

---
+++ .changeset/spotty-donkeys-pick.md
@@ -0,0 +1,5 @@
+---
+'@aragon/app-next': patch
+---
+
+Fix validation of bodies of create process form
--- src/modules/createDao/components/createProcessForm/createProcessFormGovernance/fields/governanceBodiesField/governanceBodiesField.tsx
+++ src/modules/createDao/components/createProcessForm/createProcessFormGovernance/fields/governanceBodiesField/governanceBodiesField.tsx
@@ -2,8 +2,8 @@ import type { ISetupBodyDialogParams } from '@/modules/createDao/dialogs/setupBo
 import type { ISetupBodyForm } from '@/modules/createDao/dialogs/setupBodyDialog/setupBodyDialogDefinitions';
 import { useDialogContext } from '@/shared/components/dialogProvider';
 import { useTranslations } from '@/shared/components/translationsProvider';
-import { Button, IconType, InputContainer } from '@aragon/gov-ui-kit';
-import { useFieldArray, useWatch } from 'react-hook-form';
+import { Button, IconType, type IInputContainerProps, InputContainer } from '@aragon/gov-ui-kit';
+import { useFieldArray, useFormContext, useWatch } from 'react-hook-form';
 import { GovernanceType, type ICreateProcessFormData } from '../../../createProcessFormDefinitions';
 import { GovernanceBodiesFieldItem } from './governanceBodiesFieldItem';
 
@@ -20,26 +20,39 @@ export interface IGovernanceBodiesFieldProps {
      * Type of governance to setup.
      */
     governanceType: GovernanceType;
+    /**
+     * Alert to be displayed on the field.
+     */
+    alert?: IInputContainerProps['alert'];
 }
 
 export const GovernanceBodiesField: React.FC<IGovernanceBodiesFieldProps> = (props) => {
-    const { isOptimisticStage, stageId, governanceType } = props;
+    const { isOptimisticStage, stageId, governanceType, alert } = props;
 
     const { open, close } = useDialogContext();
     const { t } = useTranslations();
+    const { formState } = useFormContext<ICreateProcessFormData>();
+
+    const isAdvancedGovernance = governanceType === GovernanceType.ADVANCED;
+
+    // Only apply min-length rule for simple governance as stage bodies are already validated per-stage on the stage field
+    const validateBodies = (bodies: ICreateProcessFormData['bodies']) => {
+        const bodiesFieldError = 'app.createDao.createProcessForm.governance.bodiesField.error.minLength';
+        const isValid = bodies.length > 0;
+
+        return isAdvancedGovernance || isValid ? undefined : bodiesFieldError;
+    };
 
     const {
         fields: bodiesField,
         remove: removeBody,
         update: updateBody,
         append: appendBody,
-    } = useFieldArray<ICreateProcessFormData>({ name: 'bodies' });
+    } = useFieldArray<ICreateProcessFormData, 'bodies'>({ name: 'bodies', rules: { validate: validateBodies } });
     const watchBodiesField = useWatch<ICreateProcessFormData, 'bodies'>({ name: 'bodies' });
     const controlledBodiesField = bodiesField.map((field, index) => ({ ...field, ...watchBodiesField[index] }));
 
     const bodies = controlledBodiesField.filter((body) => stageId == null || body.stageId === stageId);
-
-    const isAdvancedGovernance = governanceType === GovernanceType.ADVANCED;
     const renderAddButton = isAdvancedGovernance || bodies.length === 0;
 
     const handleBodySubmit = (index?: number) => (values: ISetupBodyForm) => {
@@ -66,6 +79,9 @@ export const GovernanceBodiesField: React.FC<IGovernanceBodiesFieldProps> = (pro
         open('SETUP_BODY', { params });
     };
 
+    const { message: errorMessage } = formState.errors.bodies?.root ?? {};
+    const internalFieldAlert = errorMessage ? { message: t(errorMessage), variant: 'critical' as const } : undefined;
+
     const bodiesLabelContext = isOptimisticStage ? 'vetoing' : 'voting';
 
     return (
@@ -76,6 +92,7 @@ export const GovernanceBodiesField: React.FC<IGovernanceBodiesFieldProps> = (pro
                 label={t(`app.createDao.createProcessForm.governance.bodiesField.label.${bodiesLabelContext}`)}
                 helpText={t('app.createDao.createProcessForm.governance.bodiesField.helpText')}
                 useCustomWrapper={true}
+                alert={alert ?? internalFieldAlert}
             >
                 <div className="flex flex-col gap-3 md:gap-2">
                     {bodies.map((body, index) => (
