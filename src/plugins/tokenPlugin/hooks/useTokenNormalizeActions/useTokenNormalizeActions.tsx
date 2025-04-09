f6805dbfa1eadd56147bccb8fedad07c4adb0e38
Kevin Davis <whomst@kevind.xyz>
2025-04-09 15:40:17 +0200

    chore: update useNormalizeActions for fetching relevant token settings

--- src/plugins/tokenPlugin/hooks/useTokenNormalizeActions/useTokenNormalizeActions.tsx
+++ src/plugins/tokenPlugin/hooks/useTokenNormalizeActions/useTokenNormalizeActions.tsx
@@ -1,12 +1,17 @@
 import type { INormalizeActionsParams } from '@/modules/governance/types';
+import type { IDaoPlugin } from '@/shared/api/daoService';
 import { useTranslations } from '@/shared/components/translationsProvider';
+import { useDaoPlugins } from '@/shared/hooks/useDaoPlugins';
+import { addressUtils } from '@aragon/gov-ui-kit';
 import type { ITokenPluginSettings } from '../../types';
 import { tokenActionUtils } from '../../utils/tokenActionUtils';
 
 export interface IUseTokenNormalizeActionsParams extends INormalizeActionsParams<ITokenPluginSettings> {}
 
 export const useTokenNormalizeActions = (params: IUseTokenNormalizeActionsParams) => {
-    const { actions, settings } = params;
+    const { actions, daoId } = params;
+
+    const daoPlugins = useDaoPlugins({ daoId, includeSubPlugins: true }) ?? [];
 
     const { t } = useTranslations();
 
@@ -14,7 +19,11 @@ export const useTokenNormalizeActions = (params: IUseTokenNormalizeActionsParams
         if (tokenActionUtils.isTokenMintAction(action)) {
             return tokenActionUtils.normalizeTokenMintAction(action);
         } else if (tokenActionUtils.isChangeSettingsAction(action)) {
-            return tokenActionUtils.normalizeChangeSettingsAction({ action, settings, t });
+            const { to: pluginAddress } = action;
+            const plugin = daoPlugins.find(({ meta }) => addressUtils.isAddressEqual(pluginAddress, meta.address))
+                ?.meta as IDaoPlugin<ITokenPluginSettings>;
+
+            return tokenActionUtils.normalizeChangeSettingsAction({ action, settings: plugin.settings, t });
         }
 
         return action;
