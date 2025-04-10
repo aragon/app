19c4b348a0ea5f3558086ba9c0c33208522c8f17
shan8851 <asamshan456@gmail.com>
2025-04-10 17:02:18 +0100

    chore: prettier

--- src/modules/governance/dialogs/publishProposalDialog/publishProposalDialogUtils.test.tsx
+++ src/modules/governance/dialogs/publishProposalDialog/publishProposalDialogUtils.test.tsx
@@ -1,5 +1,6 @@
 import { generateDaoPlugin } from '@/shared/testUtils';
 import { pluginRegistryUtils } from '@/shared/utils/pluginRegistryUtils';
+import type { Hex } from 'viem';
 import * as Viem from 'viem';
 import { ProposalActionType } from '../../api/governanceService';
 import { GovernanceSlotId } from '../../constants/moduleSlots';
@@ -69,7 +70,7 @@ describe('publishProposalDialog utils', () => {
             expect(slotFunction).toHaveBeenCalledWith({
                 actions: processedActions,
                 metadata: '0x697066733a2f2f746573742d636964',
-                proposal: { ...proposal, actions: processedActions },
+                values: { ...proposal, actions: processedActions },
             });
 
             expect(transaction.data).toEqual(transactionData);
@@ -84,8 +85,24 @@ describe('publishProposalDialog utils', () => {
             const transferAction = generateProposalActionWithdrawToken({ data: '0x123' });
             const transferActionData = 'transfer-async-data';
             const actions = [
-                { ...updateMetadataAction, daoId: 'test', meta: undefined },
-                { ...transferAction, daoId: 'test', meta: undefined },
+                {
+                    ...updateMetadataAction,
+                    to: updateMetadataAction.to as Hex,
+                    from: updateMetadataAction.from as Hex,
+                    data: updateMetadataAction.data as Hex,
+                    value: updateMetadataAction.value as unknown as bigint,
+                    daoId: 'test',
+                    meta: undefined,
+                },
+                {
+                    ...transferAction,
+                    to: transferAction.to as Hex,
+                    from: transferAction.from as Hex,
+                    data: transferAction.data as Hex,
+                    value: transferAction.value as unknown as bigint,
+                    daoId: 'test',
+                    meta: undefined,
+                },
             ];
             const prepareActions = {
                 [ProposalActionType.METADATA_UPDATE]: () => Promise.resolve(updateMetadataActionData),
@@ -104,8 +121,24 @@ describe('publishProposalDialog utils', () => {
             const transferAction = generateProposalActionWithdrawToken({ data: '0x123' });
             const updateAction = generateProposalActionUpdateMetadata({ data: '0x456' });
             const actions = [
-                { ...transferAction, daoId: 'test', meta: undefined },
-                { ...updateAction, daoId: 'test', meta: undefined },
+                {
+                    ...transferAction,
+                    to: transferAction.to as Hex,
+                    from: transferAction.from as Hex,
+                    data: transferAction.data as Hex,
+                    value: transferAction.value as unknown as bigint,
+                    daoId: 'test',
+                    meta: undefined,
+                },
+                {
+                    ...updateAction,
+                    to: updateAction.to as Hex,
+                    from: updateAction.from as Hex,
+                    data: updateAction.data as Hex,
+                    value: updateAction.value as unknown as bigint,
+                    daoId: 'test',
+                    meta: undefined,
+                },
             ];
 
             const result = await publishProposalDialogUtils.prepareActions({ actions });
