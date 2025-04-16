b4313b98ced5b566d15317420c6fcf7077ed3919
Milos Dzepina <milos@aragon.org>
2025-04-16 17:22:57 +0200

    Implement live permissions check on GD page
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/createDao/pages/createProcessPage/createProcessPageClient.tsx
+++ src/modules/createDao/pages/createProcessPage/createProcessPageClient.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { useConnectedWalletGuard } from '@/modules/application/hooks/useConnectedWalletGuard';
 import { useDialogContext } from '@/shared/components/dialogProvider';
 import { Page } from '@/shared/components/page';
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { WizardPage } from '@/shared/components/wizards/wizardPage';
-import { useMemo } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCallback, useEffect, useMemo } from 'react';
+import { useDaoPlugins } from '../../../../shared/hooks/useDaoPlugins';
+import { GovernanceSlotId } from '../../../governance/constants/moduleSlots';
+import { usePermissionCheckGuard } from '../../../governance/hooks/usePermissionCheckGuard';
 import { createProcessFormUtils, type ICreateProcessFormData } from '../../components/createProcessForm';
 import { CreateDaoDialogId } from '../../constants/createDaoDialogId';
 import type { IPrepareProcessDialogParams } from '../../dialogs/prepareProcessDialog';
@@ -17,20 +20,40 @@ export interface ICreateProcessPageClientProps {
      * ID of the current DAO.
      */
     daoId: string;
+    /**
+     * Plugin address used to create a proposal for adding a new process.
+     */
+    pluginAddress: string;
 }
 
 export const CreateProcessPageClient: React.FC<ICreateProcessPageClientProps> = (props) => {
-    const { daoId } = props;
+    const { daoId, pluginAddress } = props;
 
     const { t } = useTranslations();
     const { open } = useDialogContext();
-    const { check: checkWalletConnection } = useConnectedWalletGuard();
+    const router = useRouter();
+
+    const { meta: plugin } = useDaoPlugins({ daoId, pluginAddress })![0];
+
+    const handlePermissionCheckError = useCallback(() => router.push(`/dao/${daoId}/settings`), [router, daoId]);
+
+    const { check: createProposalGuard, result: canCreateProposal } = usePermissionCheckGuard({
+        permissionNamespace: 'proposal',
+        slotId: GovernanceSlotId.GOVERNANCE_PERMISSION_CHECK_PROPOSAL_CREATION,
+        onError: handlePermissionCheckError,
+        plugin,
+        daoId,
+    });
+
+    useEffect(() => {
+        if (!canCreateProposal) {
+            createProposalGuard();
+        }
+    }, [canCreateProposal, createProposalGuard]);
 
     const handleFormSubmit = (values: ICreateProcessFormData) => {
-        const dialogParams: IPrepareProcessDialogParams = { daoId, values };
-        checkWalletConnection({
-            onSuccess: () => open(CreateDaoDialogId.PREPARE_PROCESS, { params: dialogParams }),
-        });
+        const dialogParams: IPrepareProcessDialogParams = { daoId, values, pluginAddress };
+        open(CreateDaoDialogId.PREPARE_PROCESS, { params: dialogParams });
     };
 
     const processedSteps = useMemo(
