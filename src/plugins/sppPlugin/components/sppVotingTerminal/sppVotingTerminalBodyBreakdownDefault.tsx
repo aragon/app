4c787e5aa2b6319007ff6cea1819770d5a0bf977
Milos Dzepina <milos@aragon.org>
2025-04-22 16:58:00 +0200

    Add external breakdown component
    Signed-off-by: Milos Dzepina <milos@aragon.org>

---
+++ src/plugins/sppPlugin/components/sppVotingTerminal/sppVotingTerminalBodyBreakdownDefault.tsx
@@ -0,0 +1,9 @@
+export interface ISppVotingTerminalBodyBreakdownDefaultProps {}
+
+export const SppVotingTerminalBodyBreakdownDefault: React.FC<ISppVotingTerminalBodyBreakdownDefaultProps> = () => {
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+            <p className="text-neutral-500">This plugin does not support the voting breakdown view.</p>
+        </div>
+    );
+};
