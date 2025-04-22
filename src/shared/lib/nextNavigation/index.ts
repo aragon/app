68641231b4f87aaba82e4e2c77eca31557170126
Milos Dzepina <milos@aragon.org>
2025-04-22 12:56:25 +0200

    Fix lint
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/shared/lib/nextNavigation/index.ts
+++ src/shared/lib/nextNavigation/index.ts
@@ -1,3 +1,5 @@
-export * from 'next/navigation-original';
+'use client';
+
+export { useParams, usePathname, useSearchParams } from 'next/navigation-original';
 // override the default useRouter from next/navigation to enable the top loader
 export { useRouter } from 'nextjs-toploader/app';
