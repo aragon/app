6b5ecfb254d01bb2f03fce16c604cc3e2143dc4d
Milos Dzepina <milos@aragon.org>
2025-04-23 14:58:38 +0200

    Fix referential stability issue with useRouter
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/shared/lib/nextNavigation/index.ts
+++ src/shared/lib/nextNavigation/index.ts
@@ -1,5 +1,14 @@
 'use client';
 
+import { useMemo } from 'react';
 export { useParams, usePathname, useSearchParams } from 'next/navigation-original';
 // override the default useRouter from next/navigation to enable the top loader
-export { useRouter } from 'nextjs-toploader/app';
+import { useRouter as useRouterWithTopLoader } from 'nextjs-toploader/app';
+
+export const useRouter = () => {
+    const routerInstance = useRouterWithTopLoader();
+
+    // Memoize the router to ensure referential stability - temp hack until a fix is applied to nextjs-toploader package
+    // eslint-disable-next-line react-hooks/exhaustive-deps, @typescript-eslint/unbound-method
+    return useMemo(() => routerInstance, [routerInstance.push, routerInstance.replace]);
+};
