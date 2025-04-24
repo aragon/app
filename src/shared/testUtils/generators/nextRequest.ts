e8d8f94e0186b29997b4dd732adb165959b5c55f
Ruggero Cino <ruggero.cino@gmail.com>
2025-04-24 15:45:05 +0200

    Cleanup generator function

--- src/shared/testUtils/generators/nextRequest.ts
+++ src/shared/testUtils/generators/nextRequest.ts
@@ -4,13 +4,27 @@ import { INTERNALS } from 'next/dist/server/web/spec-extension/request';
 import type { NextRequest } from 'next/server';
 import { generateRequest } from './request';
 
+export const generateNextRequestCookies = (cookies?: Partial<RequestCookies>): RequestCookies => ({
+    [Symbol.iterator]: jest.fn(),
+    size: 0,
+    get: jest.fn(),
+    getAll: jest.fn(),
+    has: jest.fn(),
+    set: jest.fn(),
+    delete: jest.fn(),
+    clear: jest.fn(),
+    toString: jest.fn(),
+    // eslint-disable-next-line @typescript-eslint/no-misused-spread
+    ...cookies,
+});
+
 export const generateNextRequest = (request?: Partial<NextRequest>): NextRequest => ({
     // eslint-disable-next-line @typescript-eslint/no-misused-spread
-    ...generateRequest(request),
-    cookies: {} as RequestCookies,
+    ...generateRequest(),
+    cookies: generateNextRequestCookies(),
     nextUrl: { href: 'http://test.com' } as NextURL,
     page: undefined,
     ua: undefined,
-    [INTERNALS]: { cookies: {} as RequestCookies, url: '', nextUrl: {} as NextURL },
+    [INTERNALS]: { cookies: generateNextRequestCookies(), url: '', nextUrl: {} as NextURL },
     ...request,
 });
