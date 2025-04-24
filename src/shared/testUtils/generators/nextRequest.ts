fa3758ecd6f4f9c410712d5e57efb15f435f54ca
Ruggero Cino <ruggero.cino@gmail.com>
2025-04-24 16:08:49 +0200

    Fix generator

--- src/shared/testUtils/generators/nextRequest.ts
+++ src/shared/testUtils/generators/nextRequest.ts
@@ -1,6 +1,5 @@
 import type { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
 import type { NextURL } from 'next/dist/server/web/next-url';
-import { INTERNALS } from 'next/dist/server/web/spec-extension/request';
 import type { NextRequest } from 'next/server';
 import { generateRequest } from './request';
 
@@ -18,13 +17,13 @@ export const generateNextRequestCookies = (cookies?: Partial<RequestCookies>): R
     ...cookies,
 });
 
-export const generateNextRequest = (request?: Partial<NextRequest>): NextRequest => ({
-    // eslint-disable-next-line @typescript-eslint/no-misused-spread
-    ...generateRequest(),
-    cookies: generateNextRequestCookies(),
-    nextUrl: { href: 'http://test.com' } as NextURL,
-    page: undefined,
-    ua: undefined,
-    [INTERNALS]: { cookies: generateNextRequestCookies(), url: '', nextUrl: {} as NextURL },
-    ...request,
-});
+export const generateNextRequest = (request?: Partial<NextRequest>): NextRequest =>
+    ({
+        // eslint-disable-next-line @typescript-eslint/no-misused-spread
+        ...generateRequest(),
+        cookies: generateNextRequestCookies(),
+        nextUrl: { href: 'http://test.com' } as NextURL,
+        page: undefined,
+        ua: undefined,
+        ...request,
+    }) as NextRequest;
