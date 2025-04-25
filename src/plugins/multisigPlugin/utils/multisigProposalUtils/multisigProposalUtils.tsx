63813b7ef7845fe01bbc5eec2274f10f6dfd78ab
shan8851 <asamshan456@gmail.com>
2025-04-25 13:14:14 +0100

    chore: refactor multisig status

--- src/plugins/multisigPlugin/utils/multisigProposalUtils/multisigProposalUtils.tsx
+++ src/plugins/multisigPlugin/utils/multisigProposalUtils/multisigProposalUtils.tsx
@@ -4,58 +4,20 @@ import { type IMultisigProposal } from '../../types';
 
 class MultisigProposalUtils {
     getProposalStatus = (proposal: IMultisigProposal): ProposalStatus => {
-        const isExecuted = proposal.executed.status;
-        const isVetoed = false;
-        const startDate = proposal.startDate;
-        const endDate = proposal.endDate;
-        const paramsMet = this.isApprovalReached(proposal);
-        const hasActions = proposal.actions.length > 0;
-        const canExecuteEarly = paramsMet;
+        const { startDate, endDate, executed, actions } = proposal;
 
         const status = proposalStatusUtils.getProposalStatus({
-            isExecuted,
-            isVetoed,
+            isExecuted: executed.status,
+            isVetoed: false,
             startDate,
             endDate,
-            paramsMet,
-            hasActions,
+            paramsMet: this.isApprovalReached(proposal),
+            hasActions: actions.length > 0,
             executionExpiryDate: endDate,
-            canExecuteEarly,
+            canExecuteEarly: this.isApprovalReached(proposal),
         });
 
         return status;
-
-        // const now = DateTime.utc();
-
-        // const startDate = DateTime.fromMillis(proposal.startDate * 1000).toUTC();
-        // const endDate = DateTime.fromMillis(proposal.endDate * 1000).toUTC();
-
-        // const approvalReached = this.isApprovalReached(proposal);
-        // const isSignalingProposal = proposal.actions.length === 0;
-
-        // const isExecutable = approvalReached && now <= endDate && !isSignalingProposal;
-
-        // if (proposal.executed.status) {
-        //     return ProposalStatus.EXECUTED;
-        // }
-
-        // if (startDate >= now) {
-        //     return ProposalStatus.PENDING;
-        // }
-
-        // if (isExecutable) {
-        //     return ProposalStatus.EXECUTABLE;
-        // }
-
-        // if (now <= endDate) {
-        //     return ProposalStatus.ACTIVE;
-        // }
-
-        // if (approvalReached) {
-        //     return isSignalingProposal ? ProposalStatus.ACCEPTED : ProposalStatus.EXPIRED;
-        // }
-
-        // return ProposalStatus.REJECTED;
     };
 
     isApprovalReached = (proposal: IMultisigProposal): boolean => {
