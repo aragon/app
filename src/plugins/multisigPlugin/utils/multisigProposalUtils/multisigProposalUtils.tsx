86d19f7d4d8a291637c940aa44e26f0898a6ecc0
shan8851 <asamshan456@gmail.com>
2025-04-25 13:59:17 +0100

    chore: small refactor

--- src/plugins/multisigPlugin/utils/multisigProposalUtils/multisigProposalUtils.tsx
+++ src/plugins/multisigPlugin/utils/multisigProposalUtils/multisigProposalUtils.tsx
@@ -6,15 +6,17 @@ class MultisigProposalUtils {
     getProposalStatus = (proposal: IMultisigProposal): ProposalStatus => {
         const { startDate, endDate, executed, actions } = proposal;
 
+        const paramsMet = this.isApprovalReached(proposal);
+
         const status = proposalStatusUtils.getProposalStatus({
             isExecuted: executed.status,
             isVetoed: false,
             startDate,
             endDate,
-            paramsMet: this.isApprovalReached(proposal),
+            paramsMet,
             hasActions: actions.length > 0,
             executionExpiryDate: endDate,
-            canExecuteEarly: this.isApprovalReached(proposal),
+            canExecuteEarly: paramsMet,
         });
 
         return status;
