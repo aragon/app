be90b4b788151e943ec31f0d4b03950c4185a8f1
Ruggero Cino <ruggero.cino@gmail.com>
2025-05-08 16:54:05 +0200

    Display OSx updates on dialog

--- src/modules/settings/dialogs/updateDaoContractsListDialog/updateDaoContractsListDialog.tsx
+++ src/modules/settings/dialogs/updateDaoContractsListDialog/updateDaoContractsListDialog.tsx
@@ -1,7 +1,10 @@
 import { useDao, type IDaoPlugin } from '@/shared/api/daoService';
 import { useDialogContext, type IDialogComponentProps } from '@/shared/components/dialogProvider';
 import { useTranslations } from '@/shared/components/translationsProvider';
+import { networkDefinitions } from '@/shared/constants/networkDefinitions';
+import type { IPluginInfo } from '@/shared/types';
 import { daoUtils } from '@/shared/utils/daoUtils';
+import { pluginRegistryUtils } from '@/shared/utils/pluginRegistryUtils';
 import { Dialog, invariant } from '@aragon/gov-ui-kit';
 import { SettingsDialogId } from '../../constants/settingsDialogId';
 import type { IPrepareDaoContractsUpdateDialogParams } from '../prepareDaoContractsUpdateDialog';
@@ -25,13 +28,16 @@ export const UpdateDaoContractsListDialog: React.FC<IUpdateDaoContractsListDialo
     const { location } = props;
 
     const { t } = useTranslations();
-    const { open } = useDialogContext();
+    const { open, close } = useDialogContext();
 
     invariant(location.params != null, 'UpdateDaoContractsListDialog: required parameters must be set.');
-
     const { plugin, daoId } = location.params;
 
     const { data: dao } = useDao({ urlParams: { id: daoId } });
+    invariant(dao != null, 'UpdateDaoContractsListDialog: DAO must be defined.');
+
+    const hasOsxUpdate = daoUtils.hasAvailableOsxUpdate(dao);
+    const newProtocolVersion = networkDefinitions[dao.network].protocolVersion;
     const pluginUpdates = daoUtils.getAvailablePluginUpdates(dao);
 
     const handleConfimClick = () => {
@@ -43,9 +49,30 @@ export const UpdateDaoContractsListDialog: React.FC<IUpdateDaoContractsListDialo
         <>
             <Dialog.Header onClose={close} title={t('app.settings.updateDaoContractsListDialog.title')} />
             <Dialog.Content description={t('app.settings.updateDaoContractsListDialog.description')}>
-                {pluginUpdates.map((plugin) => (
-                    <UpdateDaoContractsCard key={plugin.address} plugin={plugin} />
-                ))}
+                <div className="flex flex-col gap-3">
+                    {hasOsxUpdate && (
+                        <UpdateDaoContractsCard
+                            key="osx"
+                            name={t('app.settings.updateDaoContractsListDialog.osxUpdate.name')}
+                            smartContractName={t(
+                                'app.settings.updateDaoContractsListDialog.osxUpdate.smartContractName',
+                            )}
+                            address={dao.address}
+                            currentVersion={dao.version}
+                            newVersion={newProtocolVersion}
+                        />
+                    )}
+                    {pluginUpdates.map((plugin) => (
+                        <UpdateDaoContractsCard
+                            key={plugin.address}
+                            name={daoUtils.getPluginName(plugin)}
+                            smartContractName={daoUtils.parsePluginSubdomain(plugin.subdomain)}
+                            address={plugin.address}
+                            currentVersion={plugin}
+                            newVersion={(pluginRegistryUtils.getPlugin(plugin.subdomain) as IPluginInfo).installVersion}
+                        />
+                    ))}
+                </div>
             </Dialog.Content>
             <Dialog.Footer
                 primaryAction={{
