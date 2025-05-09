d4c97367efcd6d6f8c625972a15c80da2f12b6c3
Evan Aronson <93671071+evanaronson@users.noreply.github.com>
2025-05-09 16:56:26 +0200

    Update sppPlugin.ts

--- src/plugins/sppPlugin/constants/sppPlugin.ts
+++ src/plugins/sppPlugin/constants/sppPlugin.ts
@@ -4,7 +4,7 @@ import type { IPluginInfo } from '@/shared/types';
 export const sppPlugin: IPluginInfo = {
     id: 'spp',
     name: 'Staged Proposal Processor',
-    installVersion: { release: 1, build: 1 },
+    installVersion: { release: 1, build: 2 },
     repositoryAddresses: {
         [Network.ARBITRUM_MAINNET]: '0xe3B00403Cd8cBee7af01961c25220289a4Cc5753',
         [Network.BASE_MAINNET]: '0x3C13098D4e2FE9aCb2fCEb3EE4fBBe33405eD39D',
