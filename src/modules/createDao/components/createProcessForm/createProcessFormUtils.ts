40644bd135e068a1f5ea54ffae7b0f864807ad82
Evan Aronson <93671071+evanaronson@users.noreply.github.com>
2025-05-20 16:00:31 +0200

    feat: Set early-advance to true on governance stages by default (#587)

--- src/modules/createDao/components/createProcessForm/createProcessFormUtils.ts
+++ src/modules/createDao/components/createProcessForm/createProcessFormUtils.ts
@@ -3,7 +3,7 @@ import { type ICreateProcessFormDataAdvanced, ProcessStageType } from './createP
 class CreateProcessFormUtils {
     private defaultVotingPeriod = { days: 7, minutes: 0, hours: 0 };
 
-    private defaultTimingSettings = { votingPeriod: this.defaultVotingPeriod, earlyStageAdvance: false };
+    private defaultTimingSettings = { votingPeriod: this.defaultVotingPeriod, earlyStageAdvance: true };
 
     private defaultType = ProcessStageType.NORMAL;
 
