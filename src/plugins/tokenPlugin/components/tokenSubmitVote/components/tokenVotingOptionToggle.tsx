bbf07790f166685a2556e36f1b8adb3ad311feb3
Milos Dzepina <milos@aragon.org>
2025-05-27 19:04:32 +0200

    Simplify voting option toggle and update styling
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/plugins/tokenPlugin/components/tokenSubmitVote/components/tokenVotingOptionToggle.tsx
+++ src/plugins/tokenPlugin/components/tokenSubmitVote/components/tokenVotingOptionToggle.tsx
@@ -3,6 +3,8 @@ import { ToggleGroupItem as RadixToggle } from '@radix-ui/react-toggle-group';
 import classNames from 'classnames';
 import type { ComponentProps } from 'react';
 
+export type TokenVotingOptionToggleVariant = 'success' | 'neutral' | 'critical';
+
 export interface ITokenVotingOptionToggleProps extends Omit<ComponentProps<'button'>, 'ref'> {
     /**
      * Value of the toggle.
@@ -20,42 +22,53 @@ export interface ITokenVotingOptionToggleProps extends Omit<ComponentProps<'butt
      * Variant of the toggle.
      * @default neutral
      */
-    variant?: 'success' | 'neutral' | 'critical';
+    variant?: TokenVotingOptionToggleVariant;
     /**
      * Is the toggle selected.
      */
     isSelected: boolean;
 }
 
+const variantToClassNames: Record<TokenVotingOptionToggleVariant, string[]> = {
+    success: [
+        'active:border-success-500',
+        'data-[state=on]:border-success-300 data-[state=on]:text-success-800 data-[state=on]:shadow-success-sm',
+    ],
+    neutral: [
+        'active:border-primary-400',
+        'data-[state=on]:border-primary-300 data-[state=on]:text-neutral-800 data-[state=on]:shadow-primary-sm',
+    ],
+    critical: [
+        'active:border-critical-500',
+        'data-[state=on]:border-critical-300 data-[state=on]:text-critical-800 data-[state=on]:shadow-critical-sm',
+    ],
+};
+
+const variantToIcon: Record<TokenVotingOptionToggleVariant, IconType> = {
+    success: IconType.CHECKMARK,
+    neutral: IconType.RADIO,
+    critical: IconType.CLOSE,
+};
+
+const variantToIconVariant: Record<TokenVotingOptionToggleVariant, 'success' | 'primary' | 'critical'> = {
+    success: 'success',
+    neutral: 'primary',
+    critical: 'critical',
+};
+
 /**
- *
  * **NOTE**: The component must be used inside a `<ToggleGroup />` component in order to work properly.
  */
 export const TokenVotingOptionToggle: React.FC<ITokenVotingOptionToggleProps> = (props) => {
     const { className, label, description, value, disabled, variant = 'neutral', isSelected, ...otherProps } = props;
 
-    const successClasses = classNames(
-        'active:border-success-500 active:shadow-none',
-        'data-[state=on]:border-success-300 data-[state=on]:text-success-800 data-[state=on]:shadow-success-sm',
-    );
-    const neutralClasses = classNames(
-        'active:border-primary-400 active:shadow-none',
-        'data-[state=on]:border-primary-300 data-[state=on]:text-neutral-800 data-[state=on]:shadow-primary-sm',
-    );
-    const criticalClasses = classNames(
-        'active:border-critical-500 active:shadow-none',
-        'data-[state=on]:border-critical-300 data-[state=on]:text-critical-800 data-[state=on]:shadow-critical-sm',
-    );
-
     const toggleClasses = classNames(
-        'flex items-center justify-between gap-3 rounded-xl border py-2 pl-4 pr-3',
+        'flex items-center justify-between gap-3 rounded-xl border py-2 pl-4 pr-3 h-[50px]',
         'cursor-pointer bg-neutral-0 transition-all',
-        'focus:outline-none focus-visible:ring-3 focus-visible:ring-primary focus-visible:ring-offset-3',
-        'border-neutral-100 text-neutral-500 shadow-neutral-sm',
+        'border-neutral-100 text-neutral-500 shadow-neutral-sm focus-ring-primary',
+        'active:shadow-none',
         'hover:border-neutral-200 hover:shadow-neutral',
-        variant === 'success' && successClasses,
-        variant === 'neutral' && neutralClasses,
-        variant === 'critical' && criticalClasses,
+        variantToClassNames[variant],
         className,
     );
 
@@ -65,15 +78,11 @@ export const TokenVotingOptionToggle: React.FC<ITokenVotingOptionToggleProps> = 
                 {label}
                 {description && <span className="text-neutral-500">{description}</span>}
             </p>
-            {variant === 'success' && (
-                <AvatarIcon icon={IconType.CHECKMARK} size="sm" variant={isSelected ? 'success' : 'neutral'} />
-            )}
-            {variant === 'neutral' && (
-                <AvatarIcon icon={IconType.RADIO} size="sm" variant={isSelected ? 'primary' : 'neutral'} />
-            )}
-            {variant === 'critical' && (
-                <AvatarIcon icon={IconType.CLOSE} size="sm" variant={isSelected ? 'critical' : 'neutral'} />
-            )}
+            <AvatarIcon
+                icon={variantToIcon[variant]}
+                variant={isSelected ? variantToIconVariant[variant] : 'neutral'}
+                size="sm"
+            />
         </RadixToggle>
     );
 };
