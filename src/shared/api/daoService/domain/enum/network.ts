d236130a0a4441f183c7767125f52f205fec4cff
Milos Dzepina <milos@aragon.org>
2025-05-29 17:40:27 +0200

    Add Corn and Chiliz
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/shared/api/daoService/domain/enum/network.ts
+++ src/shared/api/daoService/domain/enum/network.ts
@@ -8,4 +8,6 @@ export enum Network {
     ZKSYNC_SEPOLIA = 'zksync-sepolia',
     PEAQ_MAINNET = 'peaq-mainnet',
     OPTIMISM_MAINNET = 'optimism-mainnet',
+    CORN_MAINNET = 'corn-mainnet',
+    CHILIZ_MAINNET = 'chiliz-mainnet',
 }
