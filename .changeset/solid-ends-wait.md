d80b7ad26421875fbce39d7762c6ae14abb4bcba
KD <whomst@kevind.xyz>
2025-07-08 15:19:27 +0200

    chore: fix build error, include changset

---
+++ .changeset/solid-ends-wait.md
@@ -0,0 +1,5 @@
+---
+'@aragon/app-next': minor
+---
+
+Update Collapsible component for collapsedLines prop change
--- src/shared/components/page/pageHeader/pageHeader.tsx
+++ src/shared/components/page/pageHeader/pageHeader.tsx
@@ -54,7 +54,6 @@ export const PageHeader: React.FC<IPageHeaderProps> = (props) => {
                                     collapsedLines={2}
                                     buttonLabelClosed={t('app.shared.page.header.readMore')}
                                     buttonLabelOpened={t('app.shared.page.header.readLess')}
-                                    customCollapsedHeight={48}
                                     className="text-base text-neutral-500 md:text-lg"
                                 >
                                     {description}
