6eb025717fe966d1a234470fcd22a05f4e62bbb7
Evan Aronson <93671071+evanaronson@users.noreply.github.com>
2025-07-08 15:29:24 +0200

    fix(APP-4194): Apply minor UI changes to the transfer action, fix validation of AssetInput component (#659)
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update en.json
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update transferAssetForm.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Create all-tokens-dump.md
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * Update assetInput.tsx
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    * cleanup
    
    * cleanup
    
    * prettier
    
    * Code cleanup
    
    * Remove tha gap when asset is selected
    
    Signed-off-by: Milos Dzepina <milos@aragon.org>
    
    ---------
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    Signed-off-by: Milos Dzepina <milos@aragon.org>
    Co-authored-by: Ruggero Cino <ruggero.cino@gmail.com>
    Co-authored-by: Milos Dzepina <milos@aragon.org>

--- src/modules/finance/components/transferAssetForm/transferAssetForm.tsx
+++ src/modules/finance/components/transferAssetForm/transferAssetForm.tsx
@@ -1,9 +1,11 @@
-import { AssetInput } from '@/modules/finance/components/assetInput';
+import { AssetInput, type IAssetInputFormData } from '@/modules/finance/components/assetInput';
 import type { Network } from '@/shared/api/daoService';
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { useFormField } from '@/shared/hooks/useFormField';
 import { AddressInput, addressUtils } from '@aragon/gov-ui-kit';
+import classNames from 'classnames';
 import { useState } from 'react';
+import { useWatch } from 'react-hook-form';
 import type { ITransferAssetFormData } from './transferAssetFormDefinitions';
 
 export interface ITransferAssetFormProps {
@@ -26,6 +28,8 @@ export const TransferAssetForm: React.FC<ITransferAssetFormProps> = (props) => {
 
     const { t } = useTranslations();
 
+    const assetValue = useWatch<Record<string, IAssetInputFormData['asset']>>({ name: `${fieldPrefix!}.asset` });
+
     const {
         onChange: onReceiverChange,
         value,
@@ -41,7 +45,8 @@ export const TransferAssetForm: React.FC<ITransferAssetFormProps> = (props) => {
     const fetchAssetsParams = { queryParams: { address: sender, network } };
 
     return (
-        <div className="flex w-full flex-col gap-6">
+        <div className={classNames('flex w-full flex-col', { 'gap-6': !assetValue })}>
+            <AssetInput fetchAssetsParams={fetchAssetsParams} fieldPrefix={fieldPrefix} />
             <AddressInput
                 helpText={t('app.finance.transferAssetForm.receiver.helpText')}
                 placeholder={t('app.finance.transferAssetForm.receiver.placeholder')}
@@ -50,7 +55,6 @@ export const TransferAssetForm: React.FC<ITransferAssetFormProps> = (props) => {
                 onAccept={onReceiverChange}
                 {...receiverField}
             />
-            <AssetInput fetchAssetsParams={fetchAssetsParams} fieldPrefix={fieldPrefix} />
         </div>
     );
 };
