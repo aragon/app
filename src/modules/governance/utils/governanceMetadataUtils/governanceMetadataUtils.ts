f8e10221ecbdb5c1388e6cb16fb644831270a4f4
Milos Dzepina <milos@aragon.org>
2025-07-18 14:22:17 +0200

    Wrap generateProposalMetadata in try-catch
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/governance/utils/governanceMetadataUtils/governanceMetadataUtils.ts
+++ src/modules/governance/utils/governanceMetadataUtils/governanceMetadataUtils.ts
@@ -6,6 +6,7 @@ import { metadataUtils } from '@/shared/utils/metadataUtils';
 
 import { daoUtils } from '@/shared/utils/daoUtils';
 import type { Metadata } from 'next';
+import { monitoringUtils } from '../../../../shared/utils/monitoringUtils';
 
 export interface IGenerateProposalMetadataParams {
     /**
@@ -16,20 +17,38 @@ export interface IGenerateProposalMetadataParams {
 
 class GovernanceMetadataUtils {
     generateProposalMetadata = async ({ params }: IGenerateProposalMetadataParams): Promise<Metadata> => {
-        const { proposalSlug, addressOrEns, network } = await params;
-        const daoId = await daoUtils.resolveDaoId({ addressOrEns, network });
-        const proposal = await governanceService.getProposalBySlug({
-            urlParams: { slug: proposalSlug },
-            queryParams: { daoId },
-        });
+        try {
+            const { proposalSlug, addressOrEns, network } = await params;
+            const daoId = await daoUtils.resolveDaoId({
+                addressOrEns,
+                network,
+            });
+            const proposal = await governanceService.getProposalBySlug({
+                urlParams: { slug: proposalSlug },
+                queryParams: { daoId },
+            });
 
-        const title = `${proposalSlug}: ${proposal.title}`;
-        const description = proposal.summary;
-        const dao = await daoService.getDao({ urlParams: { id: daoId } });
-        const siteName = `${dao.name} | Governed on Aragon`;
-        const image = ipfsUtils.cidToSrc(dao.avatar);
+            const title = `${proposalSlug}: ${proposal.title}`;
+            const description = proposal.summary;
+            const dao = await daoService.getDao({ urlParams: { id: daoId } });
+            const siteName = `${dao.name} | Governed on Aragon`;
+            const image = ipfsUtils.cidToSrc(dao.avatar);
 
-        return metadataUtils.buildMetadata({ title, description, siteName, image, type: 'article' });
+            return metadataUtils.buildMetadata({
+                title,
+                description,
+                siteName,
+                image,
+                type: 'article',
+            });
+        } catch (error: unknown) {
+            monitoringUtils.logError(error);
+
+            return {
+                title: 'Proposal not found',
+                description: 'The requested proposal could not be found.',
+            };
+        }
     };
 }
 
