18b17e3d42cb0f6ef66fa0cd4e1d5d4bfa83f0e1
Milos Dzepina <milos@aragon.org>
2025-07-18 16:32:00 +0200

    Fix tests
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.test.tsx
+++ src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.test.tsx
@@ -52,7 +52,7 @@ describe('<CreateProposalFormActions /> component', () => {
 
     it('renders a button to add an action', () => {
         render(createTestComponent());
-        const actionButton = screen.getByRole('button', { name: /shared.actionComposer.addAction.default/ });
+        const actionButton = screen.getByRole('button', { name: /governance.actionComposer.addAction.default/ });
         expect(actionButton).toBeInTheDocument();
     });
 });
