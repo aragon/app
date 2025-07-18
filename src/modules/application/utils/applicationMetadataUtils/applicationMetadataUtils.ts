82c5569f7bd99b2289b28deaf781638ae94fd335
Milos Dzepina <milos@aragon.org>
2025-07-18 13:01:53 +0200

    Migrate to try-catch
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/application/utils/applicationMetadataUtils/applicationMetadataUtils.ts
+++ src/modules/application/utils/applicationMetadataUtils/applicationMetadataUtils.ts
@@ -3,8 +3,8 @@ import type { IDaoPageParams } from '@/shared/types';
 import { daoUtils } from '@/shared/utils/daoUtils';
 import { ipfsUtils } from '@/shared/utils/ipfsUtils';
 import { metadataUtils } from '@/shared/utils/metadataUtils';
-import { monitoringUtils } from '@/shared/utils/monitoringUtils';
 import type { Metadata } from 'next';
+import { monitoringUtils } from '../../../../shared/utils/monitoringUtils';
 
 export interface IGenerateDaoMetadataParams {
     /**
@@ -14,8 +14,8 @@ export interface IGenerateDaoMetadataParams {
 }
 
 class ApplicationMetadataUtils {
-    generateDaoMetadata = async ({ params }: IGenerateDaoMetadataParams): Promise<Metadata> =>
-        monitoringUtils.serverActionWrapper('generateDaoMetadata', async () => {
+    generateDaoMetadata = async ({ params }: IGenerateDaoMetadataParams): Promise<Metadata> => {
+        try {
             const daoPageParams = await params;
             const daoId = await daoUtils.resolveDaoId(daoPageParams);
             const dao = await daoService.getDao({ urlParams: { id: daoId } });
@@ -26,7 +26,15 @@ class ApplicationMetadataUtils {
             const siteName = `${dao.name} | Governed on Aragon`;
 
             return metadataUtils.buildMetadata({ title, description, siteName, image });
-        });
+        } catch (error: unknown) {
+            monitoringUtils.logError(error);
+
+            return {
+                title: 'DAO not found',
+                description: 'The requested DAO could not be found.',
+            };
+        }
+    };
 }
 
 export const applicationMetadataUtils = new ApplicationMetadataUtils();
