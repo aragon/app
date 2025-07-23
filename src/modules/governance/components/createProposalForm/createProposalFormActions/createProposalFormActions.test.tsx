6c4c438c0d56f616bf4887065d164555130d7b6e
Milos Dzepina <milos@aragon.org>
2025-07-23 09:50:05 +0200

    Fix tests
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.test.tsx
+++ src/modules/governance/components/createProposalForm/createProposalFormActions/createProposalFormActions.test.tsx
@@ -1,6 +1,13 @@
 import * as daoService from '@/shared/api/daoService';
 import * as DialogProvider from '@/shared/components/dialogProvider';
-import { FormWrapper, generateDao, generateDialogContext, generateReactQueryResultSuccess } from '@/shared/testUtils';
+import {
+    FormWrapper,
+    generateDao,
+    generateDaoPlugin,
+    generateDialogContext,
+    generateReactQueryResultSuccess,
+} from '@/shared/testUtils';
+import { daoUtils } from '@/shared/utils/daoUtils';
 import { GukModulesProvider } from '@aragon/gov-ui-kit';
 import { render, screen } from '@testing-library/react';
 import * as CreateProposalProvider from '../createProposalFormProvider';
@@ -10,6 +17,7 @@ describe('<CreateProposalFormActions /> component', () => {
     const useDaoSpy = jest.spyOn(daoService, 'useDao');
     const useDialogContextSpy = jest.spyOn(DialogProvider, 'useDialogContext');
     const useCreateProposalFormContextSpy = jest.spyOn(CreateProposalProvider, 'useCreateProposalFormContext');
+    const getDaoPluginsSpy = jest.spyOn(daoUtils, 'getDaoPlugins');
 
     beforeEach(() => {
         useDaoSpy.mockReturnValue(generateReactQueryResultSuccess({ data: generateDao() }));
@@ -18,12 +26,14 @@ describe('<CreateProposalFormActions /> component', () => {
             prepareActions: {},
             addPrepareAction: jest.fn(),
         });
+        getDaoPluginsSpy.mockReturnValue([generateDaoPlugin()]);
     });
 
     afterEach(() => {
         useDaoSpy.mockReset();
         useDialogContextSpy.mockReset();
         useCreateProposalFormContextSpy.mockReset();
+        getDaoPluginsSpy.mockReset();
     });
 
     const createTestComponent = (props?: Partial<ICreateProposalFormActionsProps>) => {
