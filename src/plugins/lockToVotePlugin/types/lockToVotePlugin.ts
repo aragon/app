abb29e4464e070afc7f3d0f0674a5a0cddefd3eb
KD <whomst@kevind.xyz>
2025-07-29 10:16:27 +0200

    chore: update ILockToVotePlugin for spec

--- src/plugins/lockToVotePlugin/types/lockToVotePlugin.ts
+++ src/plugins/lockToVotePlugin/types/lockToVotePlugin.ts
@@ -1,2 +1,9 @@
-// TODO: Define the shape of lockToVotePlugin and export your types here
-// export interface ILockToVote {}
+import type { ITokenPluginSettings } from '@/plugins/tokenPlugin/types';
+import type { IDaoPlugin } from '@/shared/api/daoService';
+
+export interface ILockToVotePlugin extends IDaoPlugin<ITokenPluginSettings> {
+    /**
+     * The address of the lock manager contract.
+     */
+    lockManagerAddress: string;
+}
