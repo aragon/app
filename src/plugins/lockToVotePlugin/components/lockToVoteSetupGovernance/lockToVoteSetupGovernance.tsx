d36db65dfb6e4e5759c3d76b0a569e4618a11e14
Milos Dzepina <milos@aragon.org>
2025-08-12 12:06:55 +0200

    Remove early execute from LockToVoteSetupGovernance
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/plugins/lockToVotePlugin/components/lockToVoteSetupGovernance/lockToVoteSetupGovernance.tsx
+++ src/plugins/lockToVotePlugin/components/lockToVoteSetupGovernance/lockToVoteSetupGovernance.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { DaoTokenVotingMode } from '@/plugins/tokenPlugin/types';
 import { AdvancedDateInputDuration } from '@/shared/components/forms/advancedDateInput/advancedDateInputDuration';
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { useFormField } from '@/shared/hooks/useFormField';
@@ -9,6 +8,7 @@ import { Card, InputContainer, Switch } from '@aragon/gov-ui-kit';
 import { MinParticipationField } from '@/plugins/tokenPlugin/components/tokenSetupGovernance/fields/minParticipationField';
 import { ProposalCreationEligibilityField } from '@/plugins/tokenPlugin/components/tokenSetupGovernance/fields/proposalCreationEligibilityField';
 import { SupportThresholdField } from '@/plugins/tokenPlugin/components/tokenSetupGovernance/fields/supportThresholdField';
+import { DaoLockToVoteVotingMode } from '../../types';
 import type { ILockToVoteSetupGovernanceForm, ILockToVoteSetupGovernanceProps } from './lockToVoteSetupGovernance.api';
 
 const voteDurationMin = { days: 0, hours: 1, minutes: 0 };
@@ -21,14 +21,11 @@ export const LockToVoteSetupGovernance: React.FC<ILockToVoteSetupGovernanceProps
 
     const votingModeField = useFormField<ILockToVoteSetupGovernanceForm, 'votingMode'>('votingMode', {
         fieldPrefix: formPrefix,
-        defaultValue: DaoTokenVotingMode.STANDARD,
+        defaultValue: DaoLockToVoteVotingMode.STANDARD,
     });
 
-    const handleEarlyExecutionToggle = (checked: boolean) =>
-        votingModeField.onChange(checked ? DaoTokenVotingMode.EARLY_EXECUTION : DaoTokenVotingMode.STANDARD);
-
     const handleVoteChangeToggle = (checked: boolean) =>
-        votingModeField.onChange(checked ? DaoTokenVotingMode.VOTE_REPLACEMENT : DaoTokenVotingMode.STANDARD);
+        votingModeField.onChange(checked ? DaoLockToVoteVotingMode.VOTE_REPLACEMENT : DaoLockToVoteVotingMode.STANDARD);
 
     return (
         <div className="flex w-full flex-col gap-y-6">
@@ -56,23 +53,12 @@ export const LockToVoteSetupGovernance: React.FC<ILockToVoteSetupGovernanceProps
                     </Card>
                 </InputContainer>
             )}
-            {!isSubPlugin && (
-                <Switch
-                    label={t('app.plugins.lockToVote.lockToVoteSetupGovernance.earlyExecution.label')}
-                    helpText={t('app.plugins.lockToVote.lockToVoteSetupGovernance.earlyExecution.helpText')}
-                    inlineLabel={t('app.plugins.lockToVote.lockToVoteSetupGovernance.earlyExecution.switch.label')}
-                    onCheckedChanged={handleEarlyExecutionToggle}
-                    checked={votingModeField.value === DaoTokenVotingMode.EARLY_EXECUTION}
-                    disabled={votingModeField.value === DaoTokenVotingMode.VOTE_REPLACEMENT}
-                />
-            )}
             <Switch
                 helpText={t('app.plugins.lockToVote.lockToVoteSetupGovernance.voteChange.helpText')}
                 inlineLabel={t('app.plugins.lockToVote.lockToVoteSetupGovernance.voteChange.switch.label')}
                 label={t('app.plugins.lockToVote.lockToVoteSetupGovernance.voteChange.label')}
                 onCheckedChanged={handleVoteChangeToggle}
-                checked={votingModeField.value === DaoTokenVotingMode.VOTE_REPLACEMENT}
-                disabled={votingModeField.value === DaoTokenVotingMode.EARLY_EXECUTION}
+                checked={votingModeField.value === DaoLockToVoteVotingMode.VOTE_REPLACEMENT}
             />
             {showProposalCreationSettings && (
                 <ProposalCreationEligibilityField formPrefix={formPrefix} token={membershipSettings.token} />
