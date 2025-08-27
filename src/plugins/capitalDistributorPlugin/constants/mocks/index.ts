1ccade361008e3dea47f447d49a6711fb2cb2093
Ruggero Cino <ruggero.cino@gmail.com>
2025-08-27 15:27:45 +0200

    Remove mocks for capital distributor plugin

--- src/plugins/capitalDistributorPlugin/constants/mocks/index.ts
+++
@@ -1,5 +0,0 @@
-import { claimableCampaignsMock } from './claimableCampaignsMock';
-import { claimedCampaignsMock } from './claimedCampaignsMock';
-import { injectPluginMock } from './injectPluginMock';
-
-export const capitalDistributorPluginMocks = [injectPluginMock, ...claimableCampaignsMock, ...claimedCampaignsMock];
