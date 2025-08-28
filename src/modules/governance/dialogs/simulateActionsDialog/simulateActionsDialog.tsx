363544aed548fa29184309404b4ea607893b0944
Milos Dzepina <milos@aragon.org>
2025-08-28 16:53:18 +0200

    Add hook to dialog
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/governance/dialogs/simulateActionsDialog/simulateActionsDialog.tsx
+++ src/modules/governance/dialogs/simulateActionsDialog/simulateActionsDialog.tsx
@@ -2,9 +2,20 @@ import { useDialogContext, type IDialogComponentProps } from '@/shared/component
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { wizardFormId } from '@/shared/components/wizards/wizard/wizardForm/wizardForm';
 import { ActionSimulation, Dialog, invariant } from '@aragon/gov-ui-kit';
+import { useEffect } from 'react';
+import type { Network } from '../../../../shared/api/daoService';
+import { useSimulateActions } from '../../api/actionSimulationService';
 import type { IProposalCreateAction } from '../publishProposalDialog';
 
 export interface ISimulateActionsDialogParams {
+    /**
+     * Network of the DAO.
+     */
+    network: Network;
+    /**
+     * Address of the plugin of the proposal.
+     */
+    pluginAddress: string;
     /**
      * List of actions to simulate.
      */
@@ -17,20 +28,32 @@ export const SimulateActionsDialog: React.FC<ISimulateActionsDialogProps> = (pro
     const { location } = props;
 
     invariant(location.params != null, 'SimulateActionsDialog: params must be set for the dialog to work correctly');
-    const { actions } = location.params;
+    const { actions, network, pluginAddress } = location.params;
 
     const { t } = useTranslations();
     const { close } = useDialogContext();
 
-    const { isSimulating, status } = { isSimulating: true, status: 'unknown' as const }; // Placeholder for actual simulation state
+    const { mutate: triggerSimulation, isError, isPending } = useSimulateActions();
 
-    const hasFailed = status === 'failure';
+    useEffect(() => {
+        triggerSimulation({
+            urlParams: { network },
+            body: actions.map(({ to, data, value }) => ({ from: pluginAddress, to, data, value: value as string })),
+        });
+    }, []);
+
+    const hasFailed = isError || status === 'failure';
     console.log('actionsactionsactions', actions);
     return (
         <>
             <Dialog.Header title={t(`app.governance.simulateActionsDialog.title`)} onClose={close} />
             <Dialog.Content className="pt-2 pb-3">
-                <ActionSimulation isEnabled={false} isLoading={isSimulating} totalActions={actions.length} />
+                <ActionSimulation
+                    isEnabled={false}
+                    isLoading={isPending}
+                    totalActions={actions.length}
+                    error={isError ? t('app.governance.simulateActionsDialog.error') : undefined}
+                />
             </Dialog.Content>
             <Dialog.Footer
                 hasError={hasFailed}
@@ -40,7 +63,7 @@ export const SimulateActionsDialog: React.FC<ISimulateActionsDialogProps> = (pro
                     label: t(
                         `app.governance.simulateActionsDialog.action.${hasFailed ? 'continueAnyway' : 'continue'}`,
                     ),
-                    disabled: isSimulating,
+                    disabled: isPending,
                     onClick: () => close(),
                 }}
                 secondaryAction={{
