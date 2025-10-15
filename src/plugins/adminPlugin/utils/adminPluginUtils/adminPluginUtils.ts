dafa927bad39c987492abf427af080079dc80782
Milos Dzepina <milos@aragon.org>
2025-10-15 10:31:08 +0200

    feat(APP-4616): Implement proposal execute guard (#799)
    Signed-off-by: Milos Dzepina <milos@aragon.org>

---
+++ src/plugins/adminPlugin/utils/adminPluginUtils/adminPluginUtils.ts
@@ -0,0 +1,23 @@
+import type { IDaoPlugin } from '@/shared/api/daoService';
+import { pluginMetaUtils } from '@/shared/utils/pluginMetaUtils';
+
+export interface IAdminPluginHasExecutePermissionParams {
+    /**
+     * Token plugin to check if version has EXECUTE_PROPOSAL_PERMISSION auth modifier set in execute() smart contract function.
+     */
+    plugin: IDaoPlugin;
+}
+
+class AdminPluginUtils {
+    hasExecuteProposalPermissionModifier = (params: IAdminPluginHasExecutePermissionParams): boolean => {
+        const { plugin } = params;
+        const hasExecuteProposalPermissionGuard = pluginMetaUtils.isVersionGreaterOrEqualTo(plugin, {
+            release: 1,
+            build: 2,
+        });
+
+        return hasExecuteProposalPermissionGuard;
+    };
+}
+
+export const adminPluginUtils = new AdminPluginUtils();
