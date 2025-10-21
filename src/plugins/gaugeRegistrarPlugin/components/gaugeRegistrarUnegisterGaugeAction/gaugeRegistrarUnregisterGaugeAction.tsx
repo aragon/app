82d9b0de211ac43e936f44af377731f7bc0e54d6
Milos Dzepina <milos@aragon.org>
2025-10-21 10:06:40 +0200

    unregister: implement empty card
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/plugins/gaugeRegistrarPlugin/components/gaugeRegistrarUnegisterGaugeAction/gaugeRegistrarUnregisterGaugeAction.tsx
+++ src/plugins/gaugeRegistrarPlugin/components/gaugeRegistrarUnegisterGaugeAction/gaugeRegistrarUnregisterGaugeAction.tsx
@@ -1,7 +1,8 @@
 import { type IProposalAction } from '@/modules/governance/api/governanceService';
 import type { IProposalActionData } from '@/modules/governance/components/createProposalForm';
 import type { IDaoPlugin } from '@/shared/api/daoService';
-import { type IProposalActionComponentProps } from '@aragon/gov-ui-kit';
+import { type IProposalActionComponentProps, CardEmptyState, IconType } from '@aragon/gov-ui-kit';
+import { useTranslations } from '../../../../shared/components/translationsProvider';
 
 export interface IGaugeRegistrarUnregisterGaugeActionProps
     extends IProposalActionComponentProps<IProposalActionData<IProposalAction, IDaoPlugin>> {}
@@ -19,5 +20,20 @@ const unregisterGaugeAbi = {
 } as const;
 
 export const GaugeRegistrarUnregisterGaugeAction: React.FC<IGaugeRegistrarUnregisterGaugeActionProps> = (props) => {
-    return <h1>Unregister Gauge!</h1>;
+    const { t } = useTranslations();
+
+    return (
+        <CardEmptyState
+            heading={t('app.plugins.gaugeRegistrar.gaugeRegistrarUnregisterGaugeAction.emptyCard.heading')}
+            description={t('app.plugins.gaugeRegistrar.gaugeRegistrarUnregisterGaugeAction.emptyCard.description')}
+            objectIllustration={{ object: 'SETTINGS' }}
+            secondaryButton={{
+                label: t('app.plugins.gaugeRegistrar.gaugeRegistrarUnregisterGaugeAction.emptyCard.action'),
+                onClick: () => {},
+                iconLeft: IconType.PLUS,
+            }}
+            isStacked={false}
+            className="border border-neutral-100"
+        />
+    );
 };
