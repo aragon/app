b746b4f6fb3259158bd8e04aa94654155615b3a2
KD <whomst@kevind.xyz>
2025-10-28 20:46:23 +0100

    chore: fix sandbox for preview

--- src/backendApiMocks.ts
+++ src/backendApiMocks.ts
@@ -1,4 +1,3 @@
-import { gaugeVoterPluginMocks } from './plugins/gaugeVoterPlugin/mocks';
 import type { IBackendApiMock } from './shared/types';
 
-export const backendApiMocks: IBackendApiMock[] = [...gaugeVoterPluginMocks];
+export const backendApiMocks: IBackendApiMock[] = [];
