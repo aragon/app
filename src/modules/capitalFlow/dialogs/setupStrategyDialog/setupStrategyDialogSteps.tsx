704efe1cf12417b0ccdce239bfd000c2f11e1814
Milos Dzepina <milos@aragon.org>
2025-11-20 16:09:19 +0100

    Add router type step
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/capitalFlow/dialogs/setupStrategyDialog/setupStrategyDialogSteps.tsx
+++ src/modules/capitalFlow/dialogs/setupStrategyDialog/setupStrategyDialogSteps.tsx
@@ -1,6 +1,7 @@
 import { WizardDialog } from '@/shared/components/wizards/wizardDialog';
 import type { ISetupStrategyForm } from './setupStrategyDialogDefinitions';
 import { SetupStrategyDialogDistribution } from './setupStrategyDialogDistribution';
+import { SetupStrategyDialogRouterType } from './setupStrategyDialogRouterType';
 import { SetupStrategyDialogSelect } from './setupStrategyDialogSelect';
 import { SetupStrategyDialogSourceVault } from './setupStrategyDialogSourceVault';
 
@@ -18,13 +19,14 @@ export interface ISetupStrategyDialogStepsProps {
 const setupStrategySteps = [
     { id: 'select', order: 1, meta: { name: '' } },
     { id: 'sourceVault', order: 2, meta: { name: '' } },
-    { id: 'distribution', order: 3, meta: { name: '' } },
+    { id: 'routerType', order: 3, meta: { name: '' } },
+    { id: 'distribution', order: 4, meta: { name: '' } },
 ];
 
 export const SetupStrategyDialogSteps: React.FC<ISetupStrategyDialogStepsProps> = (props) => {
     const { initialValues, daoId } = props;
 
-    const [selectStep, sourceVaultStep, distributionStep] = setupStrategySteps;
+    const [selectStep, sourceVaultStep, routerTypeStep, distributionStep] = setupStrategySteps;
 
     return (
         <>
@@ -33,6 +35,9 @@ export const SetupStrategyDialogSteps: React.FC<ISetupStrategyDialogStepsProps> 
             </WizardDialog.Step>
             <WizardDialog.Step {...sourceVaultStep}>
                 <SetupStrategyDialogSourceVault daoId={daoId} />
+            </WizardDialog.Step>
+            <WizardDialog.Step {...routerTypeStep}>
+                <SetupStrategyDialogRouterType />
             </WizardDialog.Step>
             <WizardDialog.Step {...distributionStep}>
                 <SetupStrategyDialogDistribution />
