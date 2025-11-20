01210b3da57477289d0634b1e3950089596e62b5
Milos Dzepina <milos@aragon.org>
2025-11-20 11:47:56 +0100

    Fix tests
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/shared/testUtils/generators/dialogContext.ts
+++ src/shared/testUtils/generators/dialogContext.ts
@@ -1,18 +1,13 @@
 import type { IDialogContext } from '@/shared/components/dialogProvider';
 
 export const generateDialogContext = (values?: Partial<IDialogContext>): IDialogContext => {
-    const { location, locations, ...rest } = values ?? {};
-    
-    // If location is provided but not locations, create locations array from location
-    const finalLocations = locations ?? (location ? [location] : []);
-    const finalLocation = finalLocations.length > 0 ? finalLocations[finalLocations.length - 1] : undefined;
-    
+    const { locations, ...rest } = values ?? {};
+
     return {
         open: jest.fn(),
         close: jest.fn(),
         updateOptions: jest.fn(),
-        locations: finalLocations,
-        location: finalLocation,
+        locations: locations ?? [],
         ...rest,
     };
 };
