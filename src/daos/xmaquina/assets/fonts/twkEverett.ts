636e622a98df4411d8a31518635d2d421247760e
KD <whomst@kevind.xyz>
2025-11-25 14:12:14 +0100

    chore: update font paths

--- src/daos/xmaquina/assets/fonts/twkEverett.ts
+++ src/daos/xmaquina/assets/fonts/twkEverett.ts
@@ -3,22 +3,22 @@ import localFont from 'next/font/local';
 export const twkEverett = localFont({
     src: [
         {
-            path: './TWK Everett Regular/Web/TWKEverett-Regular.woff2',
+            path: './TWK Everett Regular/TWKEverett-Regular.woff2',
             weight: '400',
             style: 'normal',
         },
         {
-            path: './TWK Everett Regular/Web/TWKEverett-Regular.woff',
+            path: './TWK Everett Regular/TWKEverett-Regular.woff',
             weight: '400',
             style: 'normal',
         },
         {
-            path: './TWK Everett Medium/Web/TWKEverett-Medium.woff2',
+            path: './TWK Everett Medium/TWKEverett-Medium.woff2',
             weight: '500',
             style: 'normal',
         },
         {
-            path: './TWK Everett Medium/Web/TWKEverett-Medium.woff',
+            path: './TWK Everett Medium/TWKEverett-Medium.woff',
             weight: '500',
             style: 'normal',
         },
