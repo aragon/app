cb2aced329491d1307e5aa5f15da7d4cc0dde898
Milos Dzepina <milos@aragon.org>
2025-12-02 17:46:16 +0100

    Add katana network
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/capitalFlow/constants/capitalFlowAddresses.ts
+++ src/modules/capitalFlow/constants/capitalFlowAddresses.ts
@@ -158,4 +158,17 @@ export const capitalFlowAddresses: Record<Network, Record<string, Hex>> = {
         claimerPluginRepo: '0x0000000000000000000000000000000000000000',
         burnRouterPluginRepo: '0x0000000000000000000000000000000000000000',
     },
+    [Network.KATANA_MAINNET]: {
+        // model factories
+        routerModelFactory: '0x0000000000000000000000000000000000000000',
+        omniModelFactory: '0x0000000000000000000000000000000000000000',
+        // source factories
+        routerSourceFactory: '0x0000000000000000000000000000000000000000',
+        claimerSourceFactory: '0x0000000000000000000000000000000000000000',
+        omniSourceFactory: '0x0000000000000000000000000000000000000000',
+        // plugin repos
+        routerPluginRepo: '0x0000000000000000000000000000000000000000',
+        claimerPluginRepo: '0x0000000000000000000000000000000000000000',
+        burnRouterPluginRepo: '0x0000000000000000000000000000000000000000',
+    },
 } as const;
