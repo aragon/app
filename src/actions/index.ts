509dfe2eeceff430a44301ea99054217200255ea
Milos Dzepina <milos@aragon.org>
2025-12-09 17:33:29 +0100

    Add initGaugeVoterActionViews
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,11 @@
 import { initGaugeRegistrarActionViews } from './gaugeRegistrar';
 import { gaugeRegistrarDialogsDefinitions } from './gaugeRegistrar/constants/gaugeRegistrarDialogsDefinitions';
+import { initGaugeVoterActionViews } from './gaugeVoter';
 import { gaugeVoterDialogsDefinitions } from './gaugeVoter/constants/gaugeVoterDialogsDefinitions';
 
 export const initActionViewRegistry = () => {
     initGaugeRegistrarActionViews();
+    initGaugeVoterActionViews();
 };
 
 export const actionsDialogsDefinitions = {
