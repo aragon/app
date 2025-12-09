0c94276c7a0a8adb404a98b74863dfdd10930578
Milos Dzepina <milos@aragon.org>
2025-12-09 16:04:46 +0100

    Add plugin metadata
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/capitalFlow/dialogs/preparePolicyDialog/routerPluginSetupAbi.ts
+++ src/modules/capitalFlow/dialogs/preparePolicyDialog/routerPluginSetupAbi.ts
@@ -2,11 +2,13 @@ export const routerPluginSetupAbi = [
     { name: '_routerSource', type: 'address' },
     { name: '_isStreamingSource', type: 'bool' },
     { name: '_routerModel', type: 'address' },
+    { name: '_pluginMetadata', type: 'bytes' },
 ] as const;
 
 export const burnRouterPluginSetupAbi = [
     { name: '_routerSource', type: 'address' },
     { name: '_isStreamingSource', type: 'bool' },
+    { name: '_pluginMetadata', type: 'bytes' },
 ] as const;
 
 export const cowSwapRouterPluginSetupAbi = [
@@ -14,6 +16,10 @@ export const cowSwapRouterPluginSetupAbi = [
     { name: '_isStreamingSource', type: 'bool' },
     { name: '_targetToken', type: 'address' },
     { name: '_cowSwapSettlement', type: 'address' },
+    { name: '_pluginMetadata', type: 'bytes' },
 ] as const;
 
-export const multiDispatchPluginSetupAbi = [{ name: '_subrouters', type: 'address[]' }] as const;
+export const multiDispatchPluginSetupAbi = [
+    { name: '_subrouters', type: 'address[]' },
+    { name: '_pluginMetadata', type: 'bytes' },
+] as const;
