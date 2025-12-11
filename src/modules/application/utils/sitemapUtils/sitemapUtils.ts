c3764b0af07b201a8acfd6c293b014c9baabb5cb
KD <whomst@kevind.xyz>
2025-12-11 12:47:55 +0100

    fix: update sitemap generator for new required params

--- src/modules/application/utils/sitemapUtils/sitemapUtils.ts
+++ src/modules/application/utils/sitemapUtils/sitemapUtils.ts
@@ -1,5 +1,5 @@
 import { daoExplorerService } from '@/modules/explore/api/daoExplorerService';
-import type { IDao } from '@/shared/api/daoService';
+import { Network, type IDao } from '@/shared/api/daoService';
 import { daoUtils } from '@/shared/utils/daoUtils';
 import { metadataUtils } from '@/shared/utils/metadataUtils';
 import type { MetadataRoute } from 'next';
@@ -7,26 +7,42 @@ import type { MetadataRoute } from 'next';
 class SitemapUtils {
     private daoPageRoutes = ['assets', 'dashboard', 'members', 'proposals', 'settings', 'transactions'];
 
-    private staticPages: MetadataRoute.Sitemap = [
-        { url: '', lastModified: new Date(), changeFrequency: 'monthly', priority: 1 },
-        { url: '/create', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.5 },
-    ];
-
     public generateSitemap = async (): Promise<MetadataRoute.Sitemap> => {
-        const daos = await daoExplorerService.getDaoList({ queryParams: { pageSize: 50 } });
+        const now = new Date();
+        const networks = Object.values(Network);
 
-        const daoPages = daos.data.flatMap((dao) => this.buildDaoPages(dao));
+        const daos = await daoExplorerService.getDaoList({
+            queryParams: { pageSize: 50, networks },
+        });
 
-        return this.prependBaseUrl([...this.staticPages, ...daoPages]);
+        const daoPages = daos.data.flatMap((dao) => this.buildDaoPages(dao, now));
+
+        return this.prependBaseUrl([...this.buildStaticPages(now), ...daoPages]);
     };
 
-    private buildDaoPages = (dao: IDao): MetadataRoute.Sitemap => {
+    private buildDaoPages = (dao: IDao, lastModified: Date): MetadataRoute.Sitemap => {
         return this.daoPageRoutes.map((daoPageRoute) => ({
             url: daoUtils.getDaoUrl(dao, daoPageRoute)!,
             changeFrequency: 'daily',
             priority: 0.8,
+            lastModified,
         }));
     };
+
+    private buildStaticPages = (lastModified: Date): MetadataRoute.Sitemap => [
+        {
+            url: '/',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: '/create',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        },
+    ];
 
     private prependBaseUrl = (sitemap: MetadataRoute.Sitemap): MetadataRoute.Sitemap =>
         sitemap.map((site) => ({ ...site, url: `${metadataUtils.baseUrl}${site.url}` }));
