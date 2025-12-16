6f86bc4ca6628fa65880edae8cc9f80f346ba29c
Milos Dzepina <milos@aragon.org>
2025-12-16 14:30:51 +0100

    Update details view
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/actions/gaugeVoter/components/gaugeVoterUpdateGaugeMetadataActionDetails/gaugeVoterUpdateGaugeMetadataActionDetails.tsx
+++ src/actions/gaugeVoter/components/gaugeVoterUpdateGaugeMetadataActionDetails/gaugeVoterUpdateGaugeMetadataActionDetails.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { type IProposalActionData } from '@/modules/governance/components/createProposalForm';
-import { useAllGauges } from '@/plugins/gaugeVoterPlugin/api/gaugeVoterService';
 import { useDao } from '@/shared/api/daoService';
 import { useTranslations } from '@/shared/components/translationsProvider';
 import { useDaoChain } from '@/shared/hooks/useDaoChain';
@@ -10,17 +9,13 @@ import {
     addressUtils,
     Avatar,
     ChainEntityType,
-    DataList,
     DefinitionList,
-    EmptyState,
     type IProposalAction,
     type IProposalActionComponentProps,
     type IProposalActionInputDataParameter,
     Link,
 } from '@aragon/gov-ui-kit';
-import type { Hex } from 'viem';
 import type { IGaugeVoterActionUpdateGaugeMetadata } from '../../types/gaugeVoterActionUpdateGaugeMetadata';
-import { GaugeVoterGaugeListItem, GaugeVoterGaugeListItemSkeleton } from '../gaugeVoterGaugeListItem';
 
 export interface IGaugeVoterUpdateGaugeMetadataActionDetailsProps
     extends IProposalActionComponentProps<IProposalActionData<IProposalAction>> {}
@@ -37,7 +32,6 @@ export const GaugeVoterUpdateGaugeMetadataActionDetails: React.FC<IGaugeVoterUpd
     props,
 ) => {
     const { action } = props;
-    const pluginAddress = action.to;
 
     const { gaugeMetadata, daoId } = action as unknown as IGaugeVoterActionUpdateGaugeMetadata;
     const { gaugeAddress } = parseUpdateGaugeMetadataInputData(action.inputData?.parameters ?? []);
@@ -48,44 +42,10 @@ export const GaugeVoterUpdateGaugeMetadataActionDetails: React.FC<IGaugeVoterUpd
     const { data: dao } = useDao({ urlParams: { id: daoId } });
     const { buildEntityUrl } = useDaoChain({ network: dao?.network });
 
-    const { data: allGauges, isLoading: isAllGaugesLoading } = useAllGauges({
-        gaugeListParams: {
-            urlParams: {
-                pluginAddress: pluginAddress as Hex,
-                network: dao!.network,
-            },
-            queryParams: {},
-        },
-    });
-
-    if (isAllGaugesLoading) {
-        return <GaugeVoterGaugeListItemSkeleton />;
-    }
-
-    const gaugeToUpdate = gaugeAddress
-        ? allGauges.find((gauge) => addressUtils.isAddressEqual(gauge.address, gaugeAddress))
-        : undefined;
-
-    if (!gaugeToUpdate) {
-        return (
-            <DataList.Item>
-                <EmptyState
-                    heading={t('app.actions.gaugeVoter.gaugeVoterUpdateGaugeMetadataActionDetails.notFound.title')}
-                    description={t(
-                        'app.actions.gaugeVoter.gaugeVoterUpdateGaugeMetadataActionDetails.notFound.description',
-                    )}
-                    objectIllustration={{ object: 'MAGNIFYING_GLASS' }}
-                    isStacked={false}
-                />
-            </DataList.Item>
-        );
-    }
-
     const gaugeAddressLink = buildEntityUrl({ type: ChainEntityType.ADDRESS, id: gaugeAddress });
 
     return (
         <>
-            <GaugeVoterGaugeListItem gauge={gaugeToUpdate} />
             <DefinitionList.Container>
                 <DefinitionList.Item
                     term={t('app.actions.gaugeVoter.gaugeVoterUpdateGaugeMetadataActionDetails.gaugeAddressTerm')}
