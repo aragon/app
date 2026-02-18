911a4933d022e6e0c3a90aa8710099d35d990cce
Arabot <60001447+arabot-1@users.noreply.github.com>
2026-02-18 12:01:56 +0100

    Release v1.21.0 (#985)
    * Release v1.21.0
    
    * chore: Update API version in production and staging environment files to v3
    
    * feat: Introduce Capital Flow Automation feature flag
    
    - Added 'capitalFlowAutomation' feature flag to enable automation section in settings and dispatch panel in transactions.
    
    * feat: Refactor DAO Plugin Info and Process Details components
    
    - Updated the DAO Plugin Info and Process Details components to enhance the display of plugin information, including the addition of target DAO addresses and improved handling of launched status.
    - Refactored the structure of settings to better separate custom settings and plugin definitions.
    
    * feat: Add support for subDAOs in DAO proposal components
    
    - Introduced  parameter in the governance API to filter proposals from SubDAOs.
    - Updated  and  components to utilize the new feature flag for including SubDAO proposals.
    
    * Refine multi-dispatch and account labels
    
    Updated descriptions and labels for multi-dispatch and account settings.
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>
    Co-authored-by: Andrew <ilin.andrii@gmail.com>
    Co-authored-by: Evan Aronson <93671071+evanaronson@users.noreply.github.com>

--- src/modules/governance/pages/daoProposalsPage/daoProposalsPage.test.tsx
+++ src/modules/governance/pages/daoProposalsPage/daoProposalsPage.test.tsx
@@ -91,6 +91,7 @@ describe('<DaoProposalsPage /> component', () => {
             sort: daoProposalsSort,
             isSubProposal: false,
             onlyActive: false,
+            includeSubDaos: false,
         };
         expect(prefetchInfiniteQuerySpy.mock.calls[0][0].queryKey).toEqual(
             proposalListOptions({ queryParams: memberListParams }).queryKey,
