c801d6635f35b2922b29ed9d85a7ebd75ec4649f
Milos Dzepina <milos@aragon.org>
2026-02-25 15:01:53 +0100

    Add demo katana dao
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/daos/index.ts
+++ src/daos/index.ts
@@ -2,6 +2,7 @@ import { initialiseAragonDemo } from './aragonDemo';
 import { initialiseAragonDemoPolicies } from './aragonDemoPolicies';
 import { initialiseBoundless } from './boundless';
 import { initialiseCapitalDistributorTest } from './capitalDistributorTest';
+import { initialiseKatanaCDDemo } from './katanaCDDemo';
 import { initialiseXmaquina } from './xmaquina';
 
 export const initialiseDaos = () => {
@@ -10,4 +11,5 @@ export const initialiseDaos = () => {
     initialiseXmaquina();
     initialiseAragonDemoPolicies();
     initialiseCapitalDistributorTest();
+    initialiseKatanaCDDemo();
 };
