e6a1b3420f68f631d234639571ce181500981618
Milos Dzepina <milos@aragon.org>
2026-03-02 14:03:54 +0100

    Add katanaEmissionsTest DAO
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/daos/index.ts
+++ src/daos/index.ts
@@ -3,6 +3,7 @@ import { initialiseAragonDemoPolicies } from './aragonDemoPolicies';
 import { initialiseBoundless } from './boundless';
 import { initialiseCapitalDistributorTest } from './capitalDistributorTest';
 import { initialiseKatanaCDDemo } from './katanaCDDemo';
+import { initialiseKatanaEmissionsTest } from './katanaEmissionsTest';
 import { initialiseXmaquina } from './xmaquina';
 
 export const initialiseDaos = () => {
@@ -12,4 +13,5 @@ export const initialiseDaos = () => {
     initialiseAragonDemoPolicies();
     initialiseCapitalDistributorTest();
     initialiseKatanaCDDemo();
+    initialiseKatanaEmissionsTest();
 };
