65adb6f681f2cba6291e243aba5f38f60b26f85f
Milos Dzepina <milos@aragon.org>
2026-03-03 16:56:15 +0100

    Fix type check
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/plugins/gaugeVoterPlugin/api/gaugeVoterService/domain/index.ts
+++ src/plugins/gaugeVoterPlugin/api/gaugeVoterService/domain/index.ts
@@ -2,6 +2,5 @@ export type { IEpochMetrics } from './epochStats';
 export type { IGauge } from './gauge';
 export type {
     IRewardDistribution,
-    IRewardDistributionInvariant,
     IRewardDistributionOwner,
 } from './rewardDistribution';
