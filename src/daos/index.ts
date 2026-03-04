6d6de5fc0eaf5ca00fd103802ee6a164bd605346
Milos Dzepina <milos@aragon.org>
2026-03-04 18:10:54 +0100

    Add missing file
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/daos/index.ts
+++ src/daos/index.ts
@@ -2,9 +2,9 @@ import { initialiseAragonDemo } from './aragonDemo';
 import { initialiseAragonDemoPolicies } from './aragonDemoPolicies';
 import { initialiseBoundless } from './boundless';
 import { initialiseCapitalDistributorTest } from './capitalDistributorTest';
+import { initialiseCryptex } from './cryptex';
 import { initialiseKatanaCDDemo } from './katanaCDDemo';
 import { initialiseKatanaEmissionsTest } from './katanaEmissionsTest';
-import { initialiseCryptex } from './cryptex';
 import { initialiseXmaquina } from './xmaquina';
 
 export const initialiseDaos = () => {
