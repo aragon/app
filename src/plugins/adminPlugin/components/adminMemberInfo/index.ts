fddd5735a7332af3956c43e7183675697fdfdd39
Milos Dzepina <milos@aragon.org>
2026-03-24 10:38:43 +0100

    chore(APP-575): Remove unused SETTINGS_MEMBERS_INFO and SETTINGS_PANEL plugin slots (#1046)
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/plugins/adminPlugin/components/adminMemberInfo/index.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-
-export const AdminMemberInfo = dynamic(() =>
-    import('./adminMemberInfo').then((mod) => mod.AdminMemberInfo),
-);
-export type { IAdminMemberInfoProps } from './adminMemberInfo';
