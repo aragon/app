a270445cd1ff54b938756defb4156ded5378149f
Milos Dzepina <milos@aragon.org>
2026-06-11 15:04:47 +0200

    perf(APP-809): Improve image caching, ISR revalidation and dialog code splitting (#1169)
    Signed-off-by: Milos Dzepina <milos@aragon.org>

---
+++ .changeset/ipfs-optimisations-and-code-chunks.md
@@ -0,0 +1,5 @@
+---
+"@aragon/app": patch
+---
+
+Improve performance with image cache TTL, ISR revalidation, dynamic dialog imports and font cleanup
--- src/app/dao/[network]/[addressOrEns]/(daoPage)/layout.tsx
+++ src/app/dao/[network]/[addressOrEns]/(daoPage)/layout.tsx
@@ -2,4 +2,5 @@ import { LayoutDao } from '@/modules/application/components/layouts/layoutDao';
 import { applicationMetadataUtils } from '@/modules/application/utils/applicationMetadataUtils';
 
 export const generateMetadata = applicationMetadataUtils.generateDaoMetadata;
+
 export default LayoutDao;
