71711d44a8911c72524d2789e56be4b7ef2724b3
Milos Dzepina <milos@aragon.org>
2026-07-31 11:06:02 +0200

    chore: disable basic action
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- apps/app/src/plugins/crossChainControllerPlugin/index.ts
+++ apps/app/src/plugins/crossChainControllerPlugin/index.ts
@@ -1,17 +1,15 @@
-import { GovernanceSlotId } from '@/modules/governance/constants/moduleSlots';
 import { pluginRegistryUtils } from '@/shared/utils/pluginRegistryUtils';
 import { crossChainControllerPlugin } from './constants/crossChainControllerPlugin';
-import { useCrossChainControllerActions } from './hooks/useCrossChainControllerActions';
 
 export const initialiseCrossChainControllerPlugin = () => {
     pluginRegistryUtils
         // Plugin definitions
-        .registerPlugin(crossChainControllerPlugin)
+        .registerPlugin(crossChainControllerPlugin);
 
-        // Governance module slots
-        .registerSlotFunction({
-            slotId: GovernanceSlotId.GOVERNANCE_PLUGIN_ACTIONS,
-            pluginId: crossChainControllerPlugin.id,
-            function: useCrossChainControllerActions,
-        });
+    // Governance module slots
+    // .registerSlotFunction({
+    //     slotId: GovernanceSlotId.GOVERNANCE_PLUGIN_ACTIONS,
+    //     pluginId: crossChainControllerPlugin.id,
+    //     function: useCrossChainControllerActions,
+    // });
 };
