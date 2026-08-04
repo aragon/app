108f824f837022ca3a3788e9468328b7ca33860a
Milos Dzepina <milos@aragon.org>
2026-08-04 09:25:03 +0200

    fix: Handle allowed actions without decoded data
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- apps/app/src/modules/governance/api/executeSelectorsService/domain/allowedActionDecoded.ts
+++ apps/app/src/modules/governance/api/executeSelectorsService/domain/allowedActionDecoded.ts
@@ -4,13 +4,13 @@ export interface IAllowedActionDecoded {
     /**
      * Name of the contract where the action is executed.
      */
-    contractName: string;
+    contractName?: string;
     /**
      * Name of the contract function being called in the action.
      */
-    functionName: string;
+    functionName?: string;
     /**
      * Parameters of the function.
      */
-    inputs: ISmartContractAbiFunctionParameter[];
+    inputs?: ISmartContractAbiFunctionParameter[];
 }
