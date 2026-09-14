1cc62a027b673db740b46f549ea32498c69afcee
Fabrice Francois <fabricevladimir@gmail.com>
2026-09-14 10:12:02 -0400

    fix: failing test

--- apps/app/src/modules/application/components/navigations/navigationWorkspace/navigationWorkspaceUtils.test.ts
+++ apps/app/src/modules/application/components/navigations/navigationWorkspace/navigationWorkspaceUtils.test.ts
@@ -133,8 +133,8 @@ describe('navigationWorkspace utils', () => {
 
             expect(links.map((link) => link.link)).toEqual([
                 '/workspace/demo',
-                '/workspace/demo/transactions',
                 '/workspace/demo/assets',
+                '/workspace/demo/transactions',
             ]);
         });
 
